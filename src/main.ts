0dd6302a722a8bc7e7396ab62d81232cff236b30
Adam Beswick <adam.beswick@bbc.co.uk>
2018-01-12 15:28:12 +0000

    adds GMI

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,6 @@ console.log(GenieCore.Maths.doubleNumber(3));
 const game = new Phaser.Game(640, 480, Phaser.AUTO, "local-game-holder");
 game.state.add("Startup", new GenieCore.States.Startup(), true);
 game.state.start("Startup");
+
+const gmi: Gmi = (window as any).getGMI({});
+console.log(gmi);
