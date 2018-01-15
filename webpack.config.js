7c5d96495ed7765d12c59e2b42f9b6d7215fa3c4
Adam Beswick <adam.beswick@bbc.co.uk>
2018-01-15 13:42:26 +0000

    adds source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
 var path = require("path");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js'),
-  pixi = path.join(phaserModule, 'build/custom/pixi.js'),
-  p2 = path.join(phaserModule, 'build/custom/p2.js');
+var phaserModule = path.join(__dirname, "/node_modules/phaser-ce/");
+var phaser = path.join(phaserModule, "build/custom/phaser-split.js"),
+    pixi = path.join(phaserModule, "build/custom/pixi.js"),
+    p2 = path.join(phaserModule, "build/custom/p2.js");
 
 module.exports = {
-    entry: "./src/main.js",
+    devtool: "source-map",
+    entry: "./src/main.ts",
     output: {
+        devtoolModuleFilenameTemplate: "[absolute-resource-path]",
         filename: "bundle.js"
     },
     module: {
-      rules: [
-        {
-          test: /(phaser-split|p2|pixi).js$/,
-          use: 'script-loader'
-        }
-      ]
+        rules: [
+            {
+                test: /(phaser-split|p2|pixi).js$/,
+                use: "script-loader"
+            },
+            {
+                test: /\.tsx?$/,
+                use: "awesome-typescript-loader"
+            }
+        ]
     },
     resolve: {
-      alias: {
-          'phaser-ce': phaser,
-          'pixi.js': pixi,
-          'p2': p2,
-      }
+        alias: {
+            "phaser-ce": phaser,
+            "pixi.js": pixi,
+            p2: p2
+        },
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".jsx"]
     }
-}
+};
