7837ba449a32ccdfdc03ad8ef67961151ad0e5ef
Adam Beswick <adam.beswick@bbc.co.uk>
2018-01-16 09:38:45 +0000

    library example tidy up

--- src/lib/examples/core.ts
+++ src/lib/examples/core.ts
@@ -1,12 +1,12 @@
-import { Startup as StartupState } from "./core-state";
-import { doubleNumber as doubleNumberFunction } from "./math";
+import { Startup as _Startup } from "./core-state";
+import { doubleNumber as _doubleNumber } from "./math";
 
-export module GenieCore {
+export namespace GenieCore {
     export namespace States {
-        export const Startup = StartupState;
-        export type Startup = StartupState;
+        export const Startup = _Startup;
+        export type Startup = _Startup;
     }
     export namespace Maths {
-        export const doubleNumber = doubleNumberFunction;
+        export const doubleNumber = _doubleNumber;
     }
 }
