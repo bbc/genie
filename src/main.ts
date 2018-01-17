2b2c86ef6e25a23b8811a5a6ee7c68e9c956822d
martin-stone <martin.stone@bbc.co.uk>
2018-01-17 11:14:46 +0000

    GMI-aware startup.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
-import "./lib/phaser";
-import * as _ from "lodash";
-import { GenieCore } from "./lib/examples/core";
+import { startup } from "./core/startup";
 
-// library function test
-console.log(GenieCore.Maths.doubleNumber(3));
-
-// Phaser + library class test
-const game = new Phaser.Game(640, 480, Phaser.AUTO, "local-game-holder");
-game.state.add("Startup", new GenieCore.States.Startup(), true);
-game.state.start("Startup");
-
-// GMI test
-const gmi: Gmi = (window as any).getGMI({});
-console.log(gmi);
-
-// lodash test
-const array = [1, 2, 3];
-_.fill(array, 6);
-console.log(array);
+startup();
