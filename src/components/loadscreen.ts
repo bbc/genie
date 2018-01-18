4bb7f14048fbb7d6317a39d2eb55858fa17438a1
Adam Beswick <adam.beswick@bbc.co.uk>
2018-01-18 13:53:13 +0000

    allows loading of asset packs into a game

--- src/components/loadscreen.ts
+++ src/components/loadscreen.ts
@@ -1,13 +1,15 @@
-import { createAssetLoader, PackList } from "../core/asset-loader";
+import { createAssetLoader, Pack, PackList } from "../core/asset-loader";
 
-const LOADSCREEN_PACK_KEY: string = "LoadscreenAssetPack";
 const MASTER_PACK_KEY: string = "MasterAssetPack";
 const GEL_PACK_KEY: string = "GelAssetPack";
 
-const packsToLoad: PackList = {
-    [LOADSCREEN_PACK_KEY]: { url: "loadscreen-pack.json" },
+const gamePacksToLoad: PackList = {
     [MASTER_PACK_KEY]: { url: "asset-master-pack.json" },
     [GEL_PACK_KEY]: { url: "gel/gel-pack.json" },
+};
+const loadscreenPack: Pack = {
+    key: "loadscreen",
+    url: "loader/loadscreen-pack.json",
 };
 
 export class Loadscreen extends Phaser.State {
@@ -21,10 +23,11 @@ export class Loadscreen extends Phaser.State {
     public preload() {
         console.log("entered Loadscreen preload()");
 
-        createAssetLoader(this.game, packsToLoad, this.create);
+        createAssetLoader(this.game, gamePacksToLoad, loadscreenPack);
     }
 
     public create() {
         console.log("entered Loadscreen create()");
+        this.game.add.image(0, 0, "logo");
     }
 }
--- src/core/asset-loader.ts
+++ src/core/asset-loader.ts
@@ -1,34 +1,51 @@
-export interface PackList {
-    [Key: string]: { url: string; data?: string };
+import * as _ from "lodash";
+
+export interface Pack {
+    key: string;
+    url: string;
 }
 
-export function createAssetLoader(game: Phaser.Game, packs: PackList, drawCallback: Function, context?: any) {
+export interface PackList {
+    [key: string]: { url: string; data?: string };
+}
+
+interface AssetPack {
+    [key: string]: Array<{ [Key: string]: any }>;
+}
+
+export function createAssetLoader(game: Phaser.Game, gamePacks: PackList, loadscreenPack: Pack) {
+    let gameAssetPack: AssetPack = {};
     let nextQueue: number = 1;
+    game.load.pack(loadscreenPack.key, loadscreenPack.url);
     game.load.onLoadComplete.add(startNextLoadQueue);
-    startNextLoadQueue();
 
     function startNextLoadQueue() {
         let nextQueueIsDefined: boolean = true;
         switch (nextQueue) {
             case 1:
-                loadAssetPackJSON();
+                loadAssetPackJSON(gamePacks);
                 break;
             case 2:
-                processAssetPackJSON();
+                gameAssetPack = processAssetPackJSON(gamePacks);
+                loadAssetPack(gameAssetPack);
                 break;
             default:
                 nextQueueIsDefined = false;
         }
         nextQueue += 1;
         if (nextQueueIsDefined) {
-            console.log("Loading queue", nextQueue - 1);
             game.time.events.add(0, game.load.start, game.load);
         } else {
+            console.log(gameAssetPack);
             game.load.onLoadComplete.removeAll();
         }
     }
 
-    function loadAssetPackJSON() {
+    /**
+     * Loads each AssetPack in the PackList as JSON using the Phaser.Loader
+     * @param packs The list of AssetPacks to load.
+     */
+    function loadAssetPackJSON(packs: PackList) {
         for (let key in packs) {
             if (packs.hasOwnProperty(key)) {
                 game.load.json(key, packs[key].url);
@@ -36,11 +53,48 @@ export function createAssetLoader(game: Phaser.Game, packs: PackList, drawCallba
         }
     }
 
-    function processAssetPackJSON() {
+    /**
+     * Gets each asset pack as a JSON object from the Phaser.Cache
+     * and merges it into one asset pack.
+     * @param packs The list of asset packs which have already been loaded,
+     * which to get fetch from the cache.
+     * @returns An asset pack which contains data from all the given asset packs.
+     */
+    function processAssetPackJSON(packs: PackList): AssetPack {
         for (let key in packs) {
             if (packs.hasOwnProperty(key)) {
                 packs[key].data = game.cache.getJSON(key);
             }
         }
+        return convertPackListToAssetPack(packs);
     }
+
+    /**
+     * Loads all of the entries in an AssetPack into the game.
+     * @param packs The AssetPack to load.
+     */
+    function loadAssetPack(pack: AssetPack) {
+        for (let screen in pack) {
+            if (pack.hasOwnProperty(screen)) {
+                game.load.pack(screen, undefined, pack);
+            }
+        }
+    }
+}
+
+/**
+ * A helper function for processAssetPackJSON which converts a PackList
+ * that contains data, into a AssetPack.
+ * @param packs The list of asset packs which already have data fetched
+ * from the cache.
+ * @returns An asset pack which contains all the data from the given PackList.
+ */
+function convertPackListToAssetPack(packs: PackList): AssetPack {
+    const assetPack: AssetPack = {};
+    for (let pack in packs) {
+        if (packs.hasOwnProperty(pack)) {
+            _.assign(assetPack, packs[pack].data);
+        }
+    }
+    return assetPack;
 }
