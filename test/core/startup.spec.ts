44ecf7429e8f11e3dbd40fa1948f68330df85994
martin-stone <martin.stone@bbc.co.uk>
2018-01-30 11:03:09 +0000

    Added startup tests. Ignored and removed .vscode files.

--- test/core/startup.spec.ts
+++ test/core/startup.spec.ts
@@ -1,17 +1,48 @@
 import { expect } from "chai";
 import { PromiseTrigger } from "src/core/promise-utils";
 import { startup } from "src/core/startup";
-import { installMockGetGmi, uninstallMockGetGmi, getElementOrThrow } from "test/helpers/mock";
-
-const TEST_DIV_ID = "test-div";
+import * as mock from "test/helpers/mock";
+import { fail } from "assert";
 
 describe("Startup", () => {
-    beforeEach(installMockGetGmi);
-    afterEach(uninstallMockGetGmi);
+    beforeEach(mock.installMockGetGmi);
+    afterEach(mock.uninstallMockGetGmi);
 
-    it("should create a canvas element", () => {
-        return startup([]).then(() => {
-            expect(getElementOrThrow(TEST_DIV_ID).children.length).to.equal(1);
+    it("should resolve a promise when Phaser is fully initialised", () => {
+        return startup([mock.screenDef()])
+            .then(game => {
+                expect(game.isBooted).to.equal(true);
+                expect(game.stage).to.be.ok;
+            })
+    });
+    
+    it("should create a canvas element within the designated parent", () => {
+        return startup([mock.screenDef()]).then(() => {
+            expect(mock.getGameHolderDiv().children.length).to.equal(1);
+            expect(mock.getGameHolderDiv().children[0].tagName).to.equal("CANVAS");
         });
     });
+
+    it("should configure the Phaser base url to be the GMI gameDir", () => {
+        mock.installMockGetGmi({gameDir: "my/game/dir/"});
+        return startup([mock.screenDef()])
+            .then(game => {
+                expect(game.load.baseURL).to.equal("my/game/dir/");
+            });
+    });
+
+    it("should configure the Phaser loader path to be the config directory", () => {
+        mock.installMockGetGmi({embedVars: {configPath: "my/config/file.json"}});
+        return startup([mock.screenDef()])
+            .then(game => {
+                expect(game.load.path).to.equal("my/config/");
+            });
+    });
+
+    xit("should display error messages in the browser", () => {
+        // Manual test: Any generation of exceptions or even ErrorEvents causes
+        // the test to fail anyway :-(
+        const dummyMessage = "This message should be rendered in the game div";
+    });
+
 });
