6162a8a18345a5f4b2367e0f4172c5c917b2fa8a
Nick Tipping <nick.tipping@bbc.co.uk>
2018-01-31 16:38:38 +0000

    Fix for npm test failing to load types

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
         ],
         exclude: [],
         preprocessors: {
-            "**/*.ts": ["webpack"],
+            "../**/*.ts": ["webpack"],
         },
         webpack: {
             module: webpackConfig.module,
