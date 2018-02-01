a1b09ae6c0e4d7774f77f42a90870773c08d1795
alex-s2 <alex.sneddon@bbc.co.uk>
2018-02-01 15:06:07 +0000

    Initial scaler tests

---
+++ test/core/gelLayers.spec.ts
@@ -0,0 +1,34 @@
+/// <reference path="../../src/lib/gmi.d.ts" />
+
+import { expect } from "chai";
+import * as sinon from "sinon";
+import * as GelLayers from "src/core/gelLayers";
+import "src/lib/phaser";
+
+describe("Startup", () => {
+
+    let mockGame: any;
+    let mockScaler: any;
+
+    beforeEach(() => {
+        
+        mockGame = {
+          add: {group: sinon.spy(function(){return {addChild: sinon.spy()}; })},
+          renderer: {resolution: 800},
+        };
+
+        mockScaler = {
+          onSizeChange: { add: sinon.spy()},
+          onScaleChange: { add: sinon.spy()},
+        };
+
+    });
+
+    it("Should add three gel layer groups to the phaser game", () => {
+        GelLayers.create(mockGame, mockScaler);
+        sinon.assert.calledWith(mockGame.add.group, undefined, "gelGroup", true);
+        sinon.assert.calledWith(mockGame.add.group, undefined, "gelBackground");
+        sinon.assert.calledWith(mockGame.add.group, undefined, "gelForeground");
+        expect(mockGame.add.group.callCount).to.eql(3);
+    });
+});
--- test/core/scaler.spec.ts
+++ test/core/scaler.spec.ts
@@ -1,16 +1,33 @@
 /// <reference path="../../src/lib/gmi.d.ts" />
 
 import { expect } from "chai";
-
-const TEST_DIV_ID = "test-div";
+import * as sinon from "sinon";
+import * as Scaler from "src/core/scaler";
+import "src/lib/phaser";
 
 describe("Startup", () => {
+
+    let mockGame: any;
+
     beforeEach(() => {
+        mockGame = {
+            scale: {
+                setGameSize: sinon.spy(),
+                scaleMode: sinon.spy(),
+                onSizeChange: { add: sinon.spy()},
+            },
+        };
 
     });
 
-    afterEach(() => {
-
+    it("Should set the scalemode be an exact fit on create", () => {
+        Scaler.create(600, mockGame);
+        expect(mockGame.scale.scaleMode).to.eql(Phaser.ScaleManager.EXACT_FIT);
     });
-
+    
+    it("Should call the games onSizeChange add function once", () => {
+        Scaler.create(600, mockGame);
+        window.dispatchEvent(new Event("resize"));
+        expect(mockGame.scale.onSizeChange.add.callCount).to.eql(1);
+    });
 });
