3510020f9b02a96f8832881011d0cd1b79be2766
Nick Tipping <nick.tipping@bbc.co.uk>
2018-02-01 14:55:47 +0000

    Added soundOff to temp file to aid in testing. Made 2 functions pure in scaler.

--- src/core/drawsomething.ts
+++ src/core/drawsomething.ts
@@ -1,5 +1,5 @@
 export function drawSomething(game: Phaser.Game, layout: any) {
-    const tempLayout = layout.create(["exit", "howToPlay", "play", "settings"]);
+    const tempLayout = layout.create(["exit", "howToPlay", "play", "soundOff", "settings"]);
 
     const style = { font: "65px Arial", fill: "#FFCC66", align: "center" };
     const text = game.add.text(0, -160, "- Phaser -\nloading in the GMI", style);
