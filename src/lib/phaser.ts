ebba5368f4da8c4dc5ea231fd4b39fb4faa80ce5
Sean Holden <sean.holden@bbc.co.uk>
2018-02-12 12:20:34 +0000

    fixing quote inconsistency

--- src/lib/phaser.ts
+++ src/lib/phaser.ts
@@ -1,8 +1,8 @@
 // Phaser and its dependencies don't conform to the module conventions that
 // Webpack requires. This file makes them behave properly.
 
-import 'pixi.js';
-import 'p2';
+import "pixi.js";
+import "p2";
 import "phaser-ce";
 
 export const Phaser = (window as any).Phaser;
