ce998d78f6a9cdb91319550bf2390fa10f54b424
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-02-20 10:30:52 +0000

    Fixes sequencer tests, adds basic tests for home screen

--- test/core/sequencer.spec.ts
+++ test/core/sequencer.spec.ts
@@ -2,13 +2,16 @@ import { expect } from "chai";
 import * as sinon from "sinon";
 import "src/lib/gmi.d";
 
+import * as LayoutFactory from "src/core/layout/factory";
 import * as Sequencer from "src/core/sequencer";
 
-describe.only("Sequencer", () => {
+describe("Sequencer", () => {
     let sequencer: any;
     let mockGame: any;
+    let mockLayout: any;
     let next: NextScreenFunction;
 
+    const sandbox = sinon.sandbox.create();
     const mockContext: any = {
         inState: "inState",
     };
@@ -30,31 +33,32 @@ describe.only("Sequencer", () => {
     ];
 
     beforeEach(() => {
-        //sinon.spy(LayoutFactory, "create");
+        mockLayout = { removeAll: sandbox.spy() };
+        sandbox.stub(LayoutFactory, "create").returns(mockLayout);
         mockGame = {
-            state: { add: sinon.spy(), start: sinon.spy() },
+            state: { add: sandbox.spy(), start: sandbox.spy() },
             add: {
-                group: sinon.spy(() => ({
-                    addChild: sinon.spy(),
+                group: sandbox.spy(() => ({
+                    addChild: sandbox.spy(),
                 })),
             },
             scale: {
-                setGameSize: sinon.spy(),
-                scaleMode: sinon.spy(),
-                onSizeChange: { add: sinon.spy() },
-                getParentBounds: sinon.spy(),
+                setGameSize: sandbox.spy(),
+                scaleMode: sandbox.spy(),
+                onSizeChange: { add: sandbox.spy() },
+                getParentBounds: sandbox.spy(),
             },
         };
         sequencer = Sequencer.create(mockGame, mockContext, mockTransitions, document.createElement("div"));
         next = mockGame.state.start.getCall(0).args[4];
     });
 
+    afterEach(() => {
+        sandbox.restore();
+    });
+
     it("adds each transition to game state", () => {
-        console.log("calls count", mockGame.state.add.callCount);
         expect(mockGame.state.add.callCount).to.equal(3);
-        console.log("0", mockGame.state.add.getCall(0).args);
-        console.log("1", mockGame.state.add.getCall(1).args);
-        console.log("2", mockGame.state.add.getCall(2).args);
         expect(mockGame.state.add.getCall(0).args).to.eql([mockTransitions[0].name, mockTransitions[0].state]);
         expect(mockGame.state.add.getCall(1).args).to.eql([mockTransitions[1].name, mockTransitions[1].state]);
         expect(mockGame.state.add.getCall(2).args).to.eql([mockTransitions[2].name, mockTransitions[2].state]);
@@ -62,7 +66,14 @@ describe.only("Sequencer", () => {
 
     it("starts the current screen", () => {
         expect(mockGame.state.start.callCount).to.equal(1);
-        expect(mockGame.state.start.getCall(0).args).to.eql([mockTransitions[0].name, true, false, mockContext, next]);
+        expect(mockGame.state.start.getCall(0).args).to.eql([
+            mockTransitions[0].name,
+            true,
+            false,
+            mockContext,
+            next,
+            mockLayout,
+        ]);
     });
 
     describe("getTransitions Method", () => {
@@ -76,7 +87,19 @@ describe.only("Sequencer", () => {
             const expectedNextScreen = mockTransitions[0].nextScreenName();
             next({});
             expect(mockGame.state.start.callCount).to.equal(2);
-            expect(mockGame.state.start.getCall(1).args).to.eql([expectedNextScreen, true, false, mockContext]);
+            expect(mockGame.state.start.getCall(1).args).to.eql([
+                expectedNextScreen,
+                true,
+                false,
+                mockContext,
+                next,
+                mockLayout,
+            ]);
+        });
+
+        it("clears down items from the layout", () => {
+            next({});
+            expect(mockLayout.removeAll.calledOnce).to.equal(true);
         });
 
         // it("passes the state of the screen to the next screen", () => {
@@ -90,7 +113,14 @@ describe.only("Sequencer", () => {
             next({});
             next({});
             expect(mockGame.state.start.callCount).to.equal(3);
-            expect(mockGame.state.start.getCall(2).args).to.eql([expectedNextScreen, true, false, mockContext]);
+            expect(mockGame.state.start.getCall(2).args).to.eql([
+                expectedNextScreen,
+                true,
+                false,
+                mockContext,
+                next,
+                mockLayout,
+            ]);
         });
     });
 });
