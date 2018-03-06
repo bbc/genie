d1c22bbc318926408b3855be0638c4be6285d0c8
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-03-06 10:02:31 +0000

    Adds commented out test (needs work)

--- test/core/layout/factory.spec.ts
+++ test/core/layout/factory.spec.ts
@@ -1,12 +1,16 @@
-import { expect } from "chai";
+import { assert, expect } from "chai";
 import * as sinon from "sinon";
 
 import * as LayoutFactory from "../../../src/core/layout/factory";
+import * as Scaler from "../../../src/core/scaler";
 
-describe("Layout Factory", () => {
+describe("Layout - Factory", () => {
     let sandbox: sinon.SinonSandbox;
     let layoutFactory: any;
     let mockGame: any;
+    let scalerSpy: any;
+    let scalerGetSizeSpy: any;
+    let scalerScaleChangeSpy: any;
     let removeAllSpy: any;
 
     before(() => {
@@ -14,21 +18,32 @@ describe("Layout Factory", () => {
     });
 
     beforeEach(() => {
-        removeAllSpy = sinon.spy();
+        scalerGetSizeSpy = sandbox.spy();
+        scalerScaleChangeSpy = sandbox.spy();
+        scalerSpy = sandbox.stub(Scaler, "create").returns({
+            getSize: scalerGetSizeSpy,
+            onScaleChange: {
+                add: scalerScaleChangeSpy,
+            },
+        });
+        removeAllSpy = sandbox.spy();
         mockGame = {
-            start: sinon.spy(),
+            start: sandbox.spy(),
             add: {
-                group: sinon.spy(() => ({
-                    addChild: sinon.spy(),
+                group: sandbox.spy(() => ({
+                    addChild: sandbox.spy(),
                     removeAll: removeAllSpy,
+                    scale: { set: sandbox.spy() },
+                    position: { set: sandbox.spy() },
                 })),
             },
             renderer: { resolution: 800 },
             scale: {
-                setGameSize: sinon.spy(),
-                scaleMode: sinon.spy(),
-                onSizeChange: { add: sinon.spy() },
-                getParentBounds: sinon.spy(),
+                setGameSize: sandbox.spy(),
+                setGamePosition: sandbox.spy(),
+                scaleMode: sandbox.spy(),
+                onSizeChange: { add: sandbox.spy() },
+                getParentBounds: sandbox.spy(),
             },
         };
         layoutFactory = LayoutFactory.create(mockGame, document.createElement("div"));
@@ -38,6 +53,16 @@ describe("Layout Factory", () => {
         sandbox.restore();
     });
 
+    it("returns the correct methods", () => {
+        expect(layoutFactory.keyLookups).to.eql({});
+        assert.exists(layoutFactory.addToBackground);
+        assert.exists(layoutFactory.addToForeground);
+        assert.exists(layoutFactory.addLayout);
+        assert.exists(layoutFactory.removeAll);
+        assert.exists(layoutFactory.addLookups);
+        assert.exists(layoutFactory.getSize);
+    });
+
     it("Should add 'gelBackground', 'gelGroup' and 'foreground' layers to the phaser game", () => {
         expect(mockGame.add.group.calledWith(undefined, "gelGroup", true)).to.equal(true);
         expect(mockGame.add.group.calledWith(undefined, "gelBackground")).to.equal(true);
@@ -45,10 +70,14 @@ describe("Layout Factory", () => {
         expect(mockGame.add.group.callCount).to.equal(3);
     });
 
-    it("Should add a callback to the scaler.onSizechange method", () => {
-        //expect(mockGame.scale.setGameSize.calledWith(undefined, "gelBackground")).to.eql(true);
-        expect(mockGame.scale.setGameSize.callCount).to.equal(1);
+    it("creates a scaler with correct params", () => {
+        expect(scalerSpy.calledWith(600, mockGame)).to.equal(true);
     });
+
+    // it.only("scales the background", () => {
+    //     expect(mockGame.add.group.scale.set.called).to.equal(true);
+    //     // expect(mockGame.position.setGamePosition.callCount).to.equal(1);
+    // });
 
     describe("removeAll method", () => {
         it("removes everything from the background", () => {
