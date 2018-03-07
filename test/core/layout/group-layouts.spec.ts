84db108a350aabb122f0d664c87ae0f3fa3af558
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-03-07 11:00:41 +0000

    Add unit tests for group-layouts

---
+++ test/core/layout/group-layouts.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from "chai";
+import { isBoolean, isUndefined } from "util";
+import { groupLayouts } from "../../../src/core/layout/group-layouts";
+
+describe("Group Layouts", () => {
+
+    it("has only unique layout positions", () => {
+        expect(groupLayouts.filter(
+            (v, k, a) => a.indexOf(v) === k),
+        )
+        .to.have.members(groupLayouts);
+    });
+
+    it("has vertical positions of either top, middle or bottom", () => {
+        expect(groupLayouts.filter(
+            (v, k, a) => ["top", "middle", "bottom"].indexOf(v.vPos) >= 0),
+        )
+        .to.have.members(groupLayouts);
+    });
+
+    it("has horizontal positions of either left, center or right", () => {
+        expect(groupLayouts.filter(
+            (v, k, a) => ["left", "center", "right"].indexOf(v.hPos) >= 0),
+        )
+        .to.have.members(groupLayouts);
+    });
+
+    it("has an optional arrangeV property of either true or false", () => {
+        expect(groupLayouts.filter(
+            (v, k, a) => !isUndefined(v.arrangeV) && isBoolean(v.arrangeV)),
+        )
+        .to.have.members(groupLayouts.filter(
+            (v, k, a) => !isUndefined(v.arrangeV)),
+        );
+    });
+
+});
