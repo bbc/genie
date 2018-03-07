54956e5f4be9c750778d8397ae7a34be021db882
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2018-03-07 16:28:52 +0000

    [CGPROD-489] GEL defaults unit tests (#41)
    * [CGPROD-489] Added unit tests for gel-defaults.ts

---
+++ test/core/layout/gel-defaults.spec.ts
@@ -0,0 +1,22 @@
+import { assert } from "chai";
+import config from "../../../src/core/layout/gel-defaults";
+
+describe("Gel Defaults", () => {
+
+    it("has an exit button with title 'Exit'", () => {
+        assert(config.exit.title === "Exit");
+    });
+
+    it("has a play button with group 'middleCenterV'", () => {
+        assert(config.play.group === "middleCenterV");
+    });
+
+    it("has a settings button with aria label 'Game Settings'", () => {
+        assert(config.settings.ariaLabel === "Game Settings");
+    });
+
+    it("has a home button with key 'home'", () => {
+        assert(config.home.key === "home");
+    });
+
+});
