9acd366098ad41667089984ac989d3880f557693
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-03-07 17:42:10 +0000

    Switch to assert and make sure the tests pass

--- test/core/layout/group-layouts.spec.ts
+++ test/core/layout/group-layouts.spec.ts
@@ -1,9 +1,9 @@
-import { expect } from "chai";
+import { assert } from "chai";
 import * as fp from "lodash/fp";
 import { isBoolean, isUndefined } from "util";
 import { groupLayouts } from "../../../src/core/layout/group-layouts";
 
-describe("Group Layouts", () => {
+describe.only("Group Layouts", () => {
 
     it("has only unique layout positions", () => {
         expect(fp.uniqWith(fp.isEqual, groupLayouts))
@@ -11,28 +11,24 @@ describe("Group Layouts", () => {
     });
 
     it("has vertical positions of either top, middle or bottom", () => {
-        expect(fp.filter(
+        assert.isEmpty(fp.filter(
             fp.negate(fp.includes(fp, ["top", "middle", "bottom"])),
-            groupLayouts,
-        ))
-        .to.have.members(groupLayouts);
+            fp.map(g => g.vPos, groupLayouts),
+        ));
     });
 
     it("has horizontal positions of either left, center or right", () => {
-        expect(fp.filter(
+        assert.isEmpty(fp.filter(
             fp.negate(fp.includes(fp, ["left", "center", "right"])),
-            groupLayouts,
-        ))
-        .to.have.members(groupLayouts);
+            fp.map(g => g.hPos, groupLayouts),
+        ));
     });
 
     it("has an optional arrangeV property of either true or false", () => {
-        expect(groupLayouts.filter(
-            (v, k, a) => !isUndefined(v.arrangeV) && isBoolean(v.arrangeV)),
-        )
-        .to.have.members(groupLayouts.filter(
-            (v, k, a) => !isUndefined(v.arrangeV)),
-        );
+        assert.isEmpty(fp.filter(
+            fp.flow(isUndefined, fp.negate(isBoolean)),
+            groupLayouts,
+        ));
     });
 
 });
