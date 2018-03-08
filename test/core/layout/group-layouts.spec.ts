c6b5d341c7f36e5a32cf74e226fc16cf220b40c7
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-03-08 10:37:23 +0000

    Replace with simple comparison

--- test/core/layout/group-layouts.spec.ts
+++ test/core/layout/group-layouts.spec.ts
@@ -3,34 +3,21 @@ import * as fp from "lodash/fp";
 import { isBoolean, isUndefined } from "util";
 import { groupLayouts } from "../../../src/core/layout/group-layouts";
 
-describe("Group Layouts", () => {
+describe.only("Group Layouts", () => {
 
-    it("has only unique layout positions", () => {
-        assert.sameMembers(
-            fp.uniqWith(fp.isEqual, groupLayouts),
-            groupLayouts,
-        );
-    });
-
-    it("has vertical positions of either top, middle or bottom", () => {
-        assert.isEmpty(fp.filter(
-            fp.negate(fp.includes(fp, ["top", "middle", "bottom"])),
-            fp.map(g => g.vPos, groupLayouts),
-        ));
-    });
-
-    it("has horizontal positions of either left, center or right", () => {
-        assert.isEmpty(fp.filter(
-            fp.negate(fp.includes(fp, ["left", "center", "right"])),
-            fp.map(g => g.hPos, groupLayouts),
-        ));
-    });
-
-    it("has an optional arrangeV property of either true or false", () => {
-        assert.isEmpty(fp.filter(
-            fp.flow(isUndefined, fp.negate(isBoolean)),
-            groupLayouts,
-        ));
+    it("matches the test constant", () => {
+        const expectedGroupLayouts = [
+            { vPos: "top", hPos: "left" },
+            { vPos: "top", hPos: "right" },
+            { vPos: "bottom", hPos: "left" },
+            { vPos: "bottom", hPos: "right" },
+            { vPos: "middle", hPos: "left" },
+            { vPos: "middle", hPos: "right" },
+            { vPos: "bottom", hPos: "center" },
+            { vPos: "middle", hPos: "center" },
+            { vPos: "middle", hPos: "center", arrangeV: true },
+        ];
+        assert.isTrue(fp.isEqual(expectedGroupLayouts, groupLayouts));
     });
 
 });
