e94d15a5021cf80527e7c555beed8707e8e683ec
Thomas-James-Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2018-05-09 16:43:09 +0100

    Initial implementation of the visual load screen

--- src/components/loadscreen.js
+++ src/components/loadscreen.js
@@ -9,6 +9,7 @@ import _ from "../lib/lodash/lodash.js";
 import { loadAssets } from "../core/asset-loader.js";
 import { Screen } from "../core/screen.js";
 import { initGameAssets, GameAssets } from "../core/game-assets.js";
+import { calculateMetrics } from "../core/layout/calculate-metrics.js";
 
 const MASTER_PACK_KEY = "MasterAssetPack";
 const GEL_PACK_KEY = "GelAssetPack";
@@ -39,11 +40,37 @@ export class Loadscreen extends Screen {
             }
             initGameAssets(this.game);
             this.startMusic();
-            this.next();
+            setTimeout(() => {
+                this.next();
+            }, 7000);
         });
     }
 
+    create() {
+        this.layoutFactory.addToBackground(this.game.add.image(0, 0, "loadscreenBackground"));
+        this.layoutFactory.addToBackground(this.game.add.image(0, -150, "loadscreenTitle"));
+
+        this.loadingBar = new PreloadBar(this.game, "loadbarBackground", "loadbarFill");
+        this.loadingBar.position.set(0, 110);
+        this.layoutFactory.addToBackground(this.loadingBar);
+
+        // TODO: Find a way to make the brand logo play nice with the scaler and position it correctly
+        const size = this.layoutFactory.getSize();
+        const metrics = calculateMetrics(size.width, size.height, size.scale, size.stageHeightPx);
+        const padding = metrics.borderPad * size.scale;
+        const horizontal = metrics.horizontals["right"];
+        const vertical = metrics.verticals["bottom"];
+        const x = horizontal - this.game.width - padding;
+        const y = vertical - (this.game.height + padding);
+
+        this.brandLogo = this.layoutFactory.addToBackground(this.game.add.image(0, 0, "brandLogo"));
+        this.brandLogo.anchor.set(1, 1);
+        this.brandLogo.position.set(x, y);
+    }
+
     updateLoadProgress(progress) {
+        if (this.hasOwnProperty("loadingBar")) this.loadingBar.setFillPercent(progress);
+
         // use progress to update loading bar
         if (this.context.qaMode.active) {
             console.log("Loader progress:", progress); // eslint-disable-line no-console
@@ -64,4 +91,36 @@ function dumpToConsole(keyLookups) {
         ]),
     ]);
     console.log(_.join(lines, "\n")); // eslint-disable-line no-console
+}
+
+/** Prototype code below */
+
+class PreloadBar extends Phaser.Group {
+    constructor(game, barBgKey, barFillKey) {
+        super(game);
+        this._createBarBackground(barBgKey);
+        this._createProgressBar(barFillKey);
+    }
+
+    setFillPercent(percent) {
+        const cropRect = new Phaser.Rectangle(0, 0, this.barWidth * percent / 100, this.barFill.height);
+        this.barFill.crop(cropRect);
+    }
+
+    _createBarBackground(barBgKey) {
+        const barBg = this.game.add.image(0, 0, barBgKey);
+        barBg.anchor.add(0.5, 0.5);
+        this.addChild(barBg);
+    }
+
+    _createProgressBar(barFillKey) {
+        this.barFill = this.game.add.image(0, 0, barFillKey);
+        this.barWidth = this.barFill.width;
+        this.barFill.x = -this.barFill.width / 2;
+        this.barFill.anchor.add(0, 0.5);
+        const cropRect = new Phaser.Rectangle(0, 0, this.barWidth, this.barFill.height);
+        this.barFill.crop(cropRect, false);
+        this.setFillPercent(0);
+        this.addChild(this.barFill);
+    }
 }
