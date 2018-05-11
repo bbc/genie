2a267781dc7f2f48ef595c1709ac7c351067d05d
Thomas-James-Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2018-05-11 17:18:26 +0100

    Added TODO to unfinished test suite

--- test/components/loadbar.spec.js
+++ test/components/loadbar.spec.js
@@ -5,6 +5,7 @@ import * as LoadBar from "../../src/components/loadbar";
 import * as AssetLoader from "../../src/core/asset-loader";
 import { GameAssets } from "../../src/core/game-assets.js";
 
+// TODO: Unit test this
 describe.skip("Load Bar", () => {
     describe("createLoadBar", () => {
         it("creates and returns a load bar", () => {
