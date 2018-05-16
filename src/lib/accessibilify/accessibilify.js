c16171212bd567f8f9c9d6d2cf947e248e6f390f
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-05-16 16:08:12 +0100

    Fix music not starting on accessible element touch

--- src/lib/accessibilify/accessibilify.js
+++ src/lib/accessibilify/accessibilify.js
@@ -87,7 +87,10 @@ export function accessibilify(button, config) {
 
     function buttonAction() {
         if (game.sound.context.state === "suspended") {
-            game.sound.resumeWebAudio();
+            game.sound._resumeWebAudioOnClick();
+        }
+        if (game.sound.touchLocked) {
+            game.sound.unlock();
         }
         button.events.onInputUp.dispatch(button, game.input.activePointer, false);
     }
