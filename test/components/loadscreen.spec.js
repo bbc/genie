96d113de6ae682b1502c63b9d4f444bfb608a23e
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-05-29 11:38:07 +0100

    Update tests to support debug

--- test/components/loadscreen.spec.js
+++ test/components/loadscreen.spec.js
@@ -25,6 +25,8 @@ describe("Load Screen", () => {
         musicLoopStub = sandbox.stub();
         musicLoopStub.withArgs("shared/background-music").returns({
             loopFull: sandbox.spy(),
+            onMute: { add: () => {} },
+            onPause: { add: () => {} },
         });
         addImageStub = sandbox.stub();
         musicLoopStub.returns({});
