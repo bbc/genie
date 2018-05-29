5872eac205d2ec9e478db039e4e265ccd1396dd9
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-05-29 11:02:20 +0100

    Add debug for IE11 issues

--- src/core/startup.js
+++ src/core/startup.js
@@ -74,6 +74,9 @@ class Startup extends Phaser.State {
     }
 
     configureAudioSetting() {
+        this.game.sound.onMute.add(() => console.log("Muted"));
+        this.game.sound.onUnMute.add(() => console.log("UnMuted"));
+
         this.game.sound.mute = settings.getAllSettings().muted;
         this.game.onPause.add(() => {
             this.game.sound.mute = settings.getAllSettings().muted;
