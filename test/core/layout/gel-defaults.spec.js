d1f197a0f5ae1c4226fa07e015cfec6ae9c99883
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-05-30 17:14:04 +0100

    GEL defaults test overhaul

---
+++ test/core/layout/gel-defaults.spec.js
@@ -0,0 +1,58 @@
+import { assert } from "chai";
+import * as sinon from "sinon";
+
+import * as pause from "../../../src/components/overlays/pause.js";
+import * as gel from "../../../src/core/layout/gel-defaults";
+
+describe("Layout - Gel Defaults", () => {
+    const sandbox = sinon.sandbox.create();
+    let mockGame;
+
+    beforeEach(() => {
+        sandbox.stub(pause, "create");
+        mockGame = {
+            state: {
+                current: "current-screen",
+                states: {
+                    "current-screen": {
+                        navigation: {
+                            home: sandbox.spy(),
+                        },
+                    },
+                },
+            },
+        };
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe("Exit Button Callback", () => {
+        it("throws an error if the GMI has not been initialised", () => {
+            assert.throw(gel.config.exit.action, "gmi has not been initialised in gel-defaults");
+        });
+
+        it("exits the game using the GMI", () => {
+            const gmiSpy = sandbox.spy();
+            gel.setGmi({ exit: gmiSpy });
+            gel.config.exit.action();
+            sandbox.assert.calledOnce(gmiSpy);
+        });
+    });
+
+    describe("Home Button Callback", () => {
+        it("navigates to the home screen", () => {
+            const homeNavigationSpy = mockGame.state.states["current-screen"].navigation.home;
+            gel.config.home.action({ game: mockGame });
+            sandbox.assert.calledOnce(homeNavigationSpy);
+        });
+    });
+
+    describe("PauseNoReplay Button Callback", () => {
+        it("creates a pause screen", () => {
+            gel.config.pauseNoReplay.action({ game: mockGame });
+            sandbox.assert.calledOnce(pause.create.withArgs({ game: mockGame }, true));
+        });
+    });
+});
