c5df67cb5e65e941c4f17ddc3e0aa77d657a4295
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-06-05 14:40:44 +0100

    Reimplement gel-defaults tests

--- test/core/layout/gel-defaults.spec.js
+++ test/core/layout/gel-defaults.spec.js
@@ -1,7 +1,10 @@
+import { assert } from "chai";
 import * as sinon from "sinon";
+import * as howToPlay from "../../../src/components/overlays/how-to-play.js";
 import * as gel from "../../../src/core/layout/gel-defaults";
+import { settings } from "../../../src/core/settings.js";
 
-describe("Layout - Gel Defaults", () => {
+describe.only("Layout - Gel Defaults", () => {
     const sandbox = sinon.sandbox.create();
     let mockGame;
 
@@ -24,11 +27,41 @@ describe("Layout - Gel Defaults", () => {
         sandbox.restore();
     });
 
+    describe("Exit Button Callback", () => {
+        it("exits the game using the GMI", () => {
+            assert.strictEqual(gel.config.exit.action, settings.exit);
+        });
+    });
+
     describe("Home Button Callback", () => {
         it("navigates to the home screen", () => {
             const homeNavigationSpy = mockGame.state.states["current-screen"].navigation.home;
             gel.config.home.action({ game: mockGame });
             sandbox.assert.calledOnce(homeNavigationSpy);
+        });
+    });
+
+    describe("Settings Button Callback", () => {
+        it("shows the settings", () => {
+            assert.strictEqual(gel.config.settings.action, settings.show);
+        });
+    });
+
+    describe("Pause Button Callback", () => {
+        it("creates a pause screen", () => {
+            assert.strictEqual(gel.config.settings.action, pause.create(false));
+        });
+    });
+
+    describe("PauseNoReplay Button Callback", () => {
+        it("creates a pause screen with replay button hidden", () => {
+            assert.strictEqual(gel.config.settings.action, pause.create(true));
+        });
+    });
+
+    describe("HowToPlay Button Callback", () => {
+        it("creates a how to play screen", () => {
+            assert.strictEqual(gel.config.howToPlay.action, howToPlay.create);
         });
     });
 });
