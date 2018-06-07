ae5e32ffde1474a468b111a496beb19482cbfdd1
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-06-07 15:29:23 +0100

    Expose calculate metrics instead of getsize from scaler

--- src/core/scaler.js
+++ src/core/scaler.js
@@ -2,33 +2,34 @@
  *
  *  @module core/scaler
  */
-
-export const GEL_MIN_RATIO_WIDTH = 4;
-export const GEL_MIN_RATIO_HEIGHT = 3;
-export const GEL_SAFE_FRAME_RATIO = GEL_MIN_RATIO_WIDTH / GEL_MIN_RATIO_HEIGHT;
+import { GEL_MIN_ASPECT_RATIO, calculateMetrics } from "./layout/calculate-metrics.js";
 
 import fp from "../../lib/lodash/fp/fp.js";
 
 const getScale = fp.curry((scaleMethods, stageHeight, { width, height }) => {
-    const scale = scaleMethods[width / height >= GEL_SAFE_FRAME_RATIO ? "wide" : "narrow"](width, height);
+    const scale = scaleMethods[width / height >= GEL_MIN_ASPECT_RATIO ? "wide" : "narrow"](width, height);
     return { width, height, scale, stageHeight };
 });
 
 const getBounds = game => () => game.scale.getParentBounds();
 
 export function create(stageHeight, game) {
+
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
     const onScaleChange = new Phaser.Signal();
 
     const scaleMethods = {
         wide: (width, height) => height / stageHeight,
-        narrow: width => width / stageHeight / GEL_SAFE_FRAME_RATIO,
+        narrow: width => width / stageHeight / GEL_MIN_ASPECT_RATIO,
     };
 
     const getSize = fp.flow(getBounds(game), fp.pick(["width", "height"]), getScale(scaleMethods, stageHeight));
+    const _calculateMetrics = fp.flow(getSize, calculateMetrics);
+    let metrics = _calculateMetrics();
 
     const setSize = ({ width, height, scale, stageHeight }) => {
+        metrics = _calculateMetrics();
         onScaleChange.dispatch(width, height, scale, stageHeight);
     };
 
@@ -40,6 +41,6 @@ export function create(stageHeight, game) {
 
     return {
         onScaleChange,
-        getSize,
+        calculateMetrics: () => metrics,
     };
 }
