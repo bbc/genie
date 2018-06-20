19fba9ffaa13f3d8fe58f417e9ab8d40699f65d6
Sean Holden <sean.holden@bbc.co.uk>
2018-06-20 15:44:46 +0100

    Added screen visibleLayer prefix to GEL buttons
    Screen class now has a new function named `visibleLayer` which will return either the screen name or overlay name, depending on what the player is seeing.
    
    Using this, we are now able to prefix each Gel Button id with the correct "screen" name.

--- src/core/accessibilify/accessibilify.js
+++ src/core/accessibilify/accessibilify.js
@@ -13,6 +13,7 @@ export function accessibilify(button, config, gameButton = true) {
 
     let signal;
     const game = button.game;
+    const screen = game.state.states[game.state.current];
     const accessibleElement = newAccessibleElement();
     const resizeAndRepositionElement = fp.debounce(200, setElementSizeAndPosition);
 
@@ -30,7 +31,7 @@ export function accessibilify(button, config, gameButton = true) {
 
     function newAccessibleElement() {
         return accessibleDomElement({
-            id: config.id,
+            id: screen.visibleLayer + config.id,
             ariaLabel: config.ariaLabel,
             parent: game.canvas.parentElement,
             onClick: buttonAction,
