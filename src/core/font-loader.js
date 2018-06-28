7a5fa64c2fb94c64f0c14094e593cc2b021d4e4e
Thomas-James-Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2018-06-28 16:13:31 +0100

    Stopped using webfontloader and webpack for handling fonts and just directly linked the font-face stylesheet in the HTML

--- src/core/font-loader.js
+++ src/core/font-loader.js
@@ -1,29 +1,22 @@
-import "../../node_modules/webfontloader/webfontloader.js";
-
 export const loadFonts = (game, done) => {
-    WebFont.load({
-        active: () => {
-            // --- Hack start ---
-            /** Phaser has an issue when attempting to use bold and italic fonts. The first time these fonts are loaded
-             *  they will not be rendered and Phaser will then fall back on the default browser font. We can circumvent
-             *  this issue by adding some some small non-visible text to the game at startup to force our bold and italic
-             *  variants of ReithSans to load before the first screen.
-             */
-            const boldReithSans = { font: "bold 1px ReithSans" };
-            game.add.text(-10000, -10000, ".", boldReithSans);
+    // --- Hack start ---
+    /** Phaser has an issue when attempting to use additional fonts. The first time these fonts are loaded
+     *  they will not be rendered and Phaser will then fall back on the default browser font. We can circumvent
+     *  this issue by adding some some small non-visible text to the game at startup to force our bold and italic
+     *  variants of ReithSans to load before the first screen.
+     */
+    const regularReithSans = { font: "1px ReithSans" };
+    game.add.text(-10000, -10000, ".", regularReithSans);
 
-            const italicReithSans = { font: "italic 1px ReithSans" };
-            game.add.text(-10000, -10000, ".", italicReithSans);
+    const boldReithSans = { font: "bold 1px ReithSans" };
+    game.add.text(-10000, -10000, ".", boldReithSans);
 
-            const italicBoldReithSans = { font: "italic bold 1px ReithSans" };
-            game.add.text(-10000, -10000, ".", italicBoldReithSans);
-            // --- Hack end ---
+    const italicReithSans = { font: "italic 1px ReithSans" };
+    game.add.text(-10000, -10000, ".", italicReithSans);
 
-            done();
-        },
-        custom: {
-            families: ["ReithSans"],
-            urls: ["../../fonts/fonts.css"],
-        },
-    });
+    const italicBoldReithSans = { font: "italic bold 1px ReithSans" };
+    game.add.text(-10000, -10000, ".", italicBoldReithSans);
+    // --- Hack end ---
+
+    done();
 };
