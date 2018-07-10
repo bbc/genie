3d7257b2d3bc4cc5525b38c6b7096d4c9e00e1d3
Nick Tipping <nick.tipping@bbc.co.uk>
2018-07-10 08:34:21 +0100

    Update version and release noted

--- src/core/accessibility/accessible-carousel-elements.js
+++ src/core/accessibility/accessible-carousel-elements.js
@@ -16,21 +16,6 @@ export function create(pageName, carouselSprites, parentElement, choices) {
     const accessibleElements = [];
     const carousel = createCarouselElement(parentElement, pageName);
 
-
-
-    const controls = document.createElement("fieldset");
-
-    controls.setAttribute("aria-controls", "carousel");
-
-    const next = document.getElementById("character-select__next");
-    const previous = document.getElementById("character-select__previous");
-
-
-    controls.appendChild(previous);
-    controls.appendChild(next);
-    parentElement.appendChild(controls);
-
-
     carouselSprites.forEach((sprite, index) => {
         const accessibleElement = accessibleDomElement({
             id: "carousel-" + pageName + "__" + (index + 1),
@@ -40,7 +25,6 @@ export function create(pageName, carouselSprites, parentElement, choices) {
             notClickable: true,
         }).el;
 
-        accessibleElement.style.display = "none";
         accessibleElements.push(accessibleElement);
 
         if (index === 0) {
