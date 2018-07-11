3c0416adcf522f4de46daa0c36c1a3b129ad24ef
Nick Tipping <nick.tipping@bbc.co.uk>
2018-07-11 10:08:59 +0100

    Fix start state of carousel items.

--- src/core/accessibility/accessible-carousel-elements.js
+++ src/core/accessibility/accessible-carousel-elements.js
@@ -16,19 +16,6 @@ export function create(pageName, carouselSprites, parentElement, choices) {
     const accessibleElements = [];
     const carousel = createCarouselElement(parentElement, pageName);
 
-    //const controls = document.createElement("fieldset");
-    //
-    //controls.setAttribute("aria-controls", "carousel");
-    //
-    //const next = document.getElementById("character-select__next");
-    //const previous = document.getElementById("character-select__previous");
-    //
-    //
-    //controls.appendChild(previous);
-    //controls.appendChild(next);
-    //parentElement.appendChild(controls);
-
-
     carouselSprites.forEach((sprite, index) => {
         const accessibleElement = accessibleDomElement({
             id: "carousel-" + pageName + "__" + (index + 1),
@@ -37,6 +24,8 @@ export function create(pageName, carouselSprites, parentElement, choices) {
             text: getAccessibilityText(choices, index),
             notClickable: true,
         }).el;
+
+        accessibleElement.style.display = index ? "none" : "block";
 
         accessibleElements.push(accessibleElement);
 
