0965e39ff6a88de6ef2490c2c80ae55373d6b1ba
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-07-19 16:41:55 +0100

    Conditionally rewrite urls in webpack-dev-server

--- build-scripts/webpack.config.js
+++ build-scripts/webpack.config.js
@@ -8,37 +8,54 @@ var phaser = path.join(phaserModule, "build/custom/phaser-split.js");
 var pixi = path.join(phaserModule, "build/custom/pixi.js");
 var p2 = path.join(phaserModule, "build/custom/p2.js");
 
-module.exports = {
-    mode: "production",
-    performance: { hints: false },
-    plugins: [new HardSourceWebpackPlugin(), new Visualizer()],
-    entry: ["babel-polyfill", "pixi", "p2", "phaser", "webfontloader", path.resolve("src/main.js")],
-    output: {
-        path: path.resolve("output"),
-        publicPath: "output",
-        filename: "main.js",
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, use: ["babel-loader"], include: path.resolve("src") },
-            { test: /pixi\.js/, use: ["expose-loader?PIXI"] },
-            { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] },
-            { test: /p2\.js/, use: ["expose-loader?p2"] },
-            { test: /webfontloader\.js/, use: ["expose-loader?WebFont"] },
-        ],
-    },
-    resolve: {
-        alias: {
-            phaser: phaser,
-            pixi: pixi,
-            p2: p2,
+module.exports = env => {
+    var webPackConfig = {
+        mode: "production",
+        performance: { hints: false },
+        plugins: [new HardSourceWebpackPlugin(), new Visualizer()],
+        entry: ["babel-polyfill", "pixi", "p2", "phaser", "webfontloader", path.resolve("src/main.js")],
+        output: {
+            path: path.resolve("output"),
+            publicPath: "output",
+            filename: "main.js",
         },
-    },
-    devServer: {
-        useLocalIp: true,
-        host: "0.0.0.0",
-        historyApiFallback: {
-            index: "dev/index.main.html",
+        module: {
+            rules: [
+                { test: /\.js$/, use: ["babel-loader"], include: path.resolve("src") },
+                { test: /\.js$/, use: ["babel-loader"], include: path.resolve("node_modules/genie/src") },
+                { test: /pixi\.js/, use: ["expose-loader?PIXI"] },
+                { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] },
+                { test: /p2\.js/, use: ["expose-loader?p2"] },
+                { test: /webfontloader\.js/, use: ["expose-loader?WebFont"] },
+            ],
         },
-    },
+        resolve: {
+            alias: {
+                phaser: phaser,
+                pixi: pixi,
+                p2: p2,
+            },
+        },
+        devServer: {
+            useLocalIp: true,
+            host: "0.0.0.0",
+            historyApiFallback: {
+                index: "node_modules/genie/dev/index.main.html",
+            },
+        },
+    };
+
+    if (env && env.genieCore) {
+        webPackConfig.devServer.historyApiFallback.index = "dev/index.main.html";
+        webPackConfig.devServer.historyApiFallback.rewrites = [
+            {
+                from: /^\/node_modules\/genie/,
+                to: function(context) {
+                    return context.parsedUrl.pathname.replace(context.match[0], "");
+                },
+            },
+        ];
+    }
+
+    return webPackConfig;
 };
