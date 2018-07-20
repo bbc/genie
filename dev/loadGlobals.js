91cd82273a74f0dd94fad9320cd17b45eba320ff
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2018-07-20 12:42:50 +0100

    Add support for loading globals from a js file

--- dev/loadGlobals.js
+++ dev/loadGlobals.js
@@ -7,3 +7,18 @@ function dynamicallyLoadScript(url) {
 
 dynamicallyLoadScript("node_modules/phaser-ce/build/phaser.min.js");
 dynamicallyLoadScript("node_modules/webfontloader/webfontloader.js");
+
+import("/globals.js")
+    .then(globals => {
+        var globalLibraries = globals.globalLibraries;
+        for (var globalLibrary in globalLibraries) {
+            if (globalLibraries.hasOwnProperty(globalLibrary)) {
+                dynamicallyLoadScript(globalLibraries[globalLibrary]);
+            }
+        }
+    })
+    .catch(e => {
+        if (e.message.indexOf("Failed to fetch dynamically imported module") === -1) {
+            throw e;
+        }
+    });
