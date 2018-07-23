71cfee1fd9275498f74b74c0a5907d109f6ea0a3
Sean Holden <sean.holden@bbc.co.uk>
2018-07-23 10:35:08 +0100

    Added specs for screen.js latest changes

--- test/core/screen.spec.js
+++ test/core/screen.spec.js
@@ -4,6 +4,7 @@ import * as GameSound from "../../src/core/game-sound";
 import * as Game from "../fake/game.js";
 import * as Scene from "../fake/scene.js";
 import * as sinon from "sinon";
+import * as a11y from "../../src/core/accessibility/accessibility-layer.js";
 
 describe("Screen", () => {
     let screen;
@@ -14,11 +15,16 @@ describe("Screen", () => {
     });
 
     describe("with context", () => {
-        let mockContext;
+        let mockContext, signalInstance;
 
         beforeEach(() => {
             sandbox.stub(GameSound, "setupScreenMusic");
+            sandbox.stub(a11y, "clearAccessibleButtons");
+            signalInstance = { add: sandbox.stub() };
+            sandbox.stub(Phaser, "Signal").returns(signalInstance);
             screen = new Screen();
+            sandbox.stub(screen, "onOverlayOpen");
+            sandbox.stub(screen, "onOverlayClosed");
             mockContext = {
                 popupScreens: ["pause"],
                 config: {
@@ -47,6 +53,22 @@ describe("Screen", () => {
             };
 
             expect(screen.context).to.eql(expectedContext);
+        });
+
+        it("clears accessible buttons", () => {
+            sandbox.assert.calledOnce(a11y.clearAccessibleButtons);
+        });
+
+        it("creates overlay open and close signals", () => {
+            sandbox.assert.calledTwice(Phaser.Signal);
+        });
+
+        it("adds a listener to overlayOpen signal", () => {
+            sandbox.assert.calledOnce(signalInstance.add.withArgs(screen.onOverlayOpen, screen));
+        });
+
+        it("adds a listener to overlayClosed signal", () => {
+            sandbox.assert.calledOnce(signalInstance.add.withArgs(screen.onOverlayClosed, screen));
         });
     });
 
@@ -139,6 +161,51 @@ describe("Screen", () => {
         it("sets the background music using the theme config", () => {
             sinon.assert.calledOnce(setupScreenMusicStub);
             sinon.assert.calledWith(setupScreenMusicStub, screen.game, themeScreenConfigMock);
+        });
+    });
+
+    describe("when overlayOpen signal is triggered", () => {
+        beforeEach(() => {
+            sandbox.stub(a11y, "resetElementsInDom");
+            screen = new Screen();
+            screen.onOverlayOpen();
+        });
+
+        it("resets accessible elements in DOM", () => {
+            sandbox.assert.calledOnce(a11y.resetElementsInDom.withArgs(screen));
+        });
+    });
+
+    describe("when overlayClosed signal is triggered", () => {
+        beforeEach(() => {
+            sandbox.stub(a11y, "clearElementsFromDom");
+            sandbox.stub(a11y, "clearAccessibleButtons");
+            sandbox.stub(a11y, "appendElementsToDom");
+            screen = new Screen();
+            screen.game = Game.Stub;
+            screen.game.canvas.focus = sandbox.stub();
+            screen.context = { popupScreens: ["how-to-play"] };
+            screen.onOverlayClosed();
+        });
+
+        it("focuses on game canvas", () => {
+            sandbox.assert.calledOnce(screen.game.canvas.focus);
+        });
+
+        it("clears accessible elements from DOM", () => {
+            sandbox.assert.calledOnce(a11y.clearElementsFromDom);
+        });
+
+        it("clears accessible buttons object", () => {
+            sandbox.assert.calledOnce(a11y.clearAccessibleButtons.withArgs(screen));
+        });
+
+        it("removes latest popup screen from popupScreens array", () => {
+            expect(screen.context.popupScreens).to.eql([]);
+        });
+
+        it("appends accessible elements to DOM", () => {
+            sandbox.assert.calledOnce(a11y.appendElementsToDom.withArgs(screen));
         });
     });
 });
