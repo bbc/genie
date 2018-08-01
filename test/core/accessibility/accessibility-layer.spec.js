8ca1534d5c972e499c931472b9384830dce92c2a
Thomas-James-Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2018-08-01 14:53:54 +0100

    [CGPROD-813] Tidy up and add test

--- test/core/accessibility/accessibility-layer.spec.js
+++ test/core/accessibility/accessibility-layer.spec.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import * as sinon from "sinon";
 import * as a11y from "../../../src/core/accessibility/accessibility-layer.js";
 
-describe("managing accessible buttons", () => {
+describe("Managing accessible buttons", () => {
     let sandbox;
 
     before(() => {
@@ -18,24 +18,35 @@ describe("managing accessible buttons", () => {
     });
 
     describe("#setup", () => {
-        it("creates and appends the parent DOM element", () => {
-            const gameParentElement = { appendChild: sandbox.stub() };
-            const el = { id: "" };
-            const createElement = sandbox.stub(document, "createElement").returns(el);
+        let createElement;
+        let gameParentElement;
+        let el;
+
+        beforeEach(() => {
+            gameParentElement = { appendChild: sandbox.spy() };
+            el = { id: "", role: "", setAttribute: sandbox.spy() };
+            createElement = sandbox.stub(document, "createElement").returns(el);
 
             a11y.setup(gameParentElement);
+        });
 
+        it("creates and appends the parent DOM element", () => {
             sandbox.assert.calledOnce(createElement.withArgs("div"));
             sandbox.assert.calledOnce(gameParentElement.appendChild.withArgs(el));
             expect(el.id).to.eq("accessibility");
         });
+
+        it("sets the role of the accessibility container div to 'application' (for NVDA/FF tabbing focus)", () => {
+            sandbox.assert.calledOnce(el.setAttribute);
+            sandbox.assert.calledWith(el.setAttribute, "role", "application");
+        });
     });
 
     describe("#appendElementsToDom", () => {
-        let el1, el2, el3, button1, button2, button3, screen, screen2, parentElement;
+        let el1, el2, el3, button1, button2, button3, screen1, screen2, parentElement;
 
-        beforeEach(() => {
-            screen = { visibleLayer: "home" };
+        before(() => {
+            screen1 = { visibleLayer: "home" };
             screen2 = { visibleLayer: "pause" };
             el1 = { id: "home__play" };
             el2 = { id: "home__pause" };
@@ -43,19 +54,19 @@ describe("managing accessible buttons", () => {
             button1 = { accessibleElement: el1 };
             button2 = { accessibleElement: el2 };
             button3 = { accessibleElement: el3 };
-            a11y.addToAccessibleButtons(screen, button1);
-            a11y.addToAccessibleButtons(screen, button2);
+            a11y.addToAccessibleButtons(screen1, button1);
+            a11y.addToAccessibleButtons(screen1, button2);
             a11y.addToAccessibleButtons(screen2, button3);
             parentElement = { appendChild: sandbox.stub() };
             sandbox
                 .stub(document, "getElementById")
                 .withArgs("accessibility")
                 .returns(parentElement);
+
+            a11y.appendElementsToDom(screen1);
         });
 
         it("appends correct elements to the DOM", () => {
-            a11y.appendElementsToDom(screen);
-
             sandbox.assert.calledOnce(parentElement.appendChild.withArgs(el1));
             sandbox.assert.calledOnce(parentElement.appendChild.withArgs(el2));
             sandbox.assert.notCalled(parentElement.appendChild.withArgs(el3));
@@ -65,16 +76,17 @@ describe("managing accessible buttons", () => {
     describe("#clearElementsFromDom", () => {
         let parentElement;
 
-        beforeEach(() => {
+        before(() => {
             parentElement = { innerHTML: "<div id='home__play'></div>" };
             sandbox
                 .stub(document, "getElementById")
                 .withArgs("accessibility")
                 .returns(parentElement);
+
+            a11y.clearElementsFromDom();
         });
 
         it("clears all accessible elements from the DOM", () => {
-            a11y.clearElementsFromDom();
             expect(parentElement.innerHTML).to.eq("");
         });
     });
