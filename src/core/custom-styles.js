be9750706958e167fd17ece67d2afd4a4a0f16e4
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-08-10 14:38:25 +0100

    Simplifies code, moves injected styles into <head>

--- src/core/custom-styles.js
+++ src/core/custom-styles.js
@@ -1,12 +1,6 @@
-export const addCustomStyles = parentElement => {
-    const customStyles = {
-        ".hide-focus-ring:focus": "{outline:none;}",
-        ".gel-button": "{ -webkit-user-select: none; }",
-    };
+export const addCustomStyles = () => {
+    const customStyles = [".hide-focus-ring:focus { outline:none; }", ".gel-button { -webkit-user-select: none; }"];
     const styleElement = document.createElement("style");
-    const styles = Object.keys(customStyles).map(style => {
-        return style + customStyles[style];
-    });
-    styleElement.innerHTML = styles.join(" ");
-    parentElement.appendChild(styleElement);
+    styleElement.innerHTML = customStyles.join(" ");
+    document.head.appendChild(styleElement);
 };
