cdccbbe470cf292144fa59df214c876b027e9927
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-09-04 15:59:02 +0100

    Amends dynamicScripts to use XMLHttpRequest instead of fetch for IE11

--- dev/scripts/dynamicScripts.js
+++ dev/scripts/dynamicScripts.js
@@ -1,20 +1,26 @@
+"use strict";
+
 function dynamicallyLoadScript(url) {
     var script = document.createElement("script");
     script.src = url;
-
     document.head.appendChild(script);
 }
 
 dynamicallyLoadScript("node_modules/phaser-ce/build/phaser.min.js");
 dynamicallyLoadScript("node_modules/webfontloader/webfontloader.js");
 
-fetch("/globals.json").then(response => {
-    if (response.status >= 400) return;
-    response.json().then(globals => {
-        for (var global in globals) {
-            if (globals.hasOwnProperty(global)) {
-                dynamicallyLoadScript(globals[global]);
-            }
+/* Cannot use fetch because of IE11 */
+var request = new XMLHttpRequest();
+request.open("GET", "/globals.json", true);
+request.onload = function() {
+    if (request.status >= 400) return;
+    const globals = JSON.parse(request.responseText);
+    let global;
+    for (global in globals) {
+        if (globals.hasOwnProperty(global)) {
+            // console.log("Add script:", globals[global]);
+            dynamicallyLoadScript(globals[global]);
         }
-    });
-});
+    }
+};
+request.send(null);
