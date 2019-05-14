f06635fe07ce33c06541df826947b5d396c56a95
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-14 13:31:44 +0100

    Adds jest config and setup

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/**
+ * @copyright BBC 2018
+ * @author BBC Children's D+E
+ * @license Apache-2.0
+ */
+
+module.exports = {
+    collectCoverageFrom: ["src/**/*.js"],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
+    testEnvironment: "jsdom",
+    modulePathIgnorePatterns: ["/.node_modules.+/"],
+    testPathIgnorePatterns: ["/node_modules/", "/.node_modules.+/", ".node_modules_production"],
+    setupTestFrameworkScriptFile: "./test/helpers/setup-jest.js",
+    transform: { "^.+\\.js$": "babel-jest" },
+};
