929247b8aee144ccb942a50940edad6c21dfaf94
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-14 13:31:18 +0100

    Updates tests for loadbar

--- test/components/loadbar.spec.js
+++ test/components/loadbar.spec.js
@@ -3,39 +3,99 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { assert } from "chai";
-import * as sinon from "sinon";
 import * as LoadBar from "../../src/components/loadbar";
 
 describe("Load Bar", () => {
-    const sandbox = sinon.createSandbox();
-
-    const mockGame = {
-        add: { image: () => mockBarFill },
-    };
-    const barBackgroundKey = "";
-    const barFillKey = "";
-
-    const mockBarFill = {
-        width: 100,
-        anchor: { add: () => {} },
-    };
+    let mockPhaserGroup;
+    let mockPhaserRectangle;
+    let mockBarBg;
+    let mockBarFill;
+    let mockGame;
 
     beforeEach(() => {
-        mockBarFill.crop = sinon.spy();
-    });
-    afterEach(() => {
-        sandbox.restore();
+        mockPhaserGroup = { addChild: jest.fn() };
+        mockPhaserRectangle = { crop: jest.fn() };
+        global.Phaser.Group = jest.fn().mockImplementation(() => mockPhaserGroup);
+        global.Phaser.Rectangle = jest.fn().mockImplementation(() => mockPhaserRectangle);
+
+        mockBarBg = {
+            anchor: { add: jest.fn() },
+            crop: jest.fn(),
+        };
+
+        mockBarFill = {
+            width: 300,
+            height: 20,
+            anchor: { add: jest.fn() },
+            crop: jest.fn(),
+        };
+
+        mockGame = {
+            add: { image: jest.fn().mockImplementation((x, y, imageName) => imageName) },
+        };
     });
 
-    describe("createLoadBar", () => {
-        it("sets crop size when fillPercent is updated", () => {
-            const progress = 50;
-            const loadBar = LoadBar.createLoadBar(mockGame, barBackgroundKey, barFillKey);
+    afterEach(() => jest.clearAllMocks());
 
-            loadBar.fillPercent = progress;
-            const cropRect = mockBarFill.crop.getCall(0).args[0];
-            assert.equal(cropRect.width, progress);
+    test("creates a new Phaser game with correct params", () => {
+        LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+        expect(Phaser.Group).toHaveBeenCalledWith(mockGame);
+    });
+
+    describe("Images", () => {
+        test("adds a loading bar background image with co-cordinates", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(mockGame.add.image).toHaveBeenCalledWith(0, 0, mockBarBg);
+            expect(mockBarBg.anchor.add).toHaveBeenCalledWith(0.5, 0.5);
+        });
+
+        test("adds the loading bar background to the phaser group", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(mockPhaserGroup.addChild).toHaveBeenCalledWith(mockBarBg);
+        });
+
+        test("adds a loading bar fill image with co-cordinates", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(mockGame.add.image).toHaveBeenCalledWith(0, 0, mockBarFill);
+            expect(mockBarFill.anchor.add).toHaveBeenCalledWith(0, 0.5);
+        });
+
+        test("adds width to the loading bar fill image", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(mockBarFill.x).toBe(-150);
+        });
+
+        test("makes a phaser rectangle for the fill image", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(global.Phaser.Rectangle).toHaveBeenCalledWith(0, 0, 0, mockBarFill.height);
+            expect(mockBarFill.crop).toHaveBeenCalledWith(mockPhaserRectangle, false);
+        });
+
+        test("adds the loading bar to the Phaser group", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(mockPhaserGroup.addChild).toHaveBeenCalledWith(mockBarFill);
+        });
+    });
+
+    describe("Fill percentage", () => {
+        test("can get the fill percentage", () => {
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            expect(mockPhaserGroup.fillPercent).toBe(0);
+        });
+
+        test("can set the fill percentage", () => {
+            const percentage = 25;
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            mockPhaserGroup.fillPercent = percentage;
+            expect(mockPhaserGroup.fillPercent).toBe(25);
+        });
+
+        test("setting the percentage updates the bar fill image", () => {
+            const percentage = 35;
+            LoadBar.createLoadBar(mockGame, mockBarBg, mockBarFill);
+            mockPhaserGroup.fillPercent = percentage;
+            expect(mockPhaserRectangle.width).toBe(105);
+            expect(mockBarFill.crop).toHaveBeenCalledWith(mockPhaserRectangle);
         });
     });
 });
