856388a7c70cd3ea1b8f41c5e4550537a87969e1
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-14 19:00:14 +0100

    Adds calculate metrics tests

--- test/core/layout/calculate-metrics.spec.js
+++ test/core/layout/calculate-metrics.spec.js
@@ -3,7 +3,7 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { expect } from "chai";
+
 import * as _ from "lodash";
 
 import { calculateMetrics } from "../../../src/core/layout/calculate-metrics";
@@ -20,96 +20,96 @@ const getMetrics = newValues => {
 };
 
 describe("Layout - Calculate Metrics", () => {
-    it("returns basic metrics", () => {
+    test("returns basic metrics", () => {
         const metrics = getMetrics({});
-        expect(metrics.width).to.equal(defaultValues.width);
-        expect(metrics.height).to.equal(defaultValues.height);
-        expect(metrics.scale).to.equal(1);
+        expect(metrics.width).toBe(defaultValues.width);
+        expect(metrics.height).toBe(defaultValues.height);
+        expect(metrics.scale).toBe(1);
     });
 
     describe("borderPad metric", () => {
-        it("sets a border padding of 2% of the longest edge", () => {
-            expect(getMetrics({ width: 600, height: 600 }).borderPad).to.equal(16);
-            expect(getMetrics({ width: 800, height: 600 }).borderPad).to.equal(16);
-            expect(getMetrics({ width: 1000, height: 600 }).borderPad).to.equal(20);
-            expect(getMetrics({ width: 1500, height: 600 }).borderPad).to.equal(28);
+        test("sets a border padding of 2% of the longest edge", () => {
+            expect(getMetrics({ width: 600, height: 600 }).borderPad).toBe(16);
+            expect(getMetrics({ width: 800, height: 600 }).borderPad).toBe(16);
+            expect(getMetrics({ width: 1000, height: 600 }).borderPad).toBe(20);
+            expect(getMetrics({ width: 1500, height: 600 }).borderPad).toBe(28);
 
-            expect(getMetrics({ width: 200, height: 600 }).borderPad).to.equal(16);
-            expect(getMetrics({ width: 200, height: 800 }).borderPad).to.equal(16);
-            expect(getMetrics({ width: 200, height: 1000 }).borderPad).to.equal(16);
+            expect(getMetrics({ width: 200, height: 600 }).borderPad).toBe(16);
+            expect(getMetrics({ width: 200, height: 800 }).borderPad).toBe(16);
+            expect(getMetrics({ width: 200, height: 1000 }).borderPad).toBe(16);
         });
     });
 
     describe("isMobile metric", () => {
-        it("reports as a mobile device if stage width is less than 770 Pixels", () => {
-            expect(getMetrics({ width: 769 }).isMobile).to.equal(true);
+        test("reports as a mobile device if stage width is less than 770 Pixels", () => {
+            expect(getMetrics({ width: 769 }).isMobile).toBe(true);
         });
 
-        it("reports as a tablet/desktop device if stage width is greater than or equal to 770 Pixels", () => {
-            expect(getMetrics({ width: 771 }).isMobile).to.equal(false);
+        test("reports as a tablet/desktop device if stage width is greater than or equal to 770 Pixels", () => {
+            expect(getMetrics({ width: 771 }).isMobile).toBe(false);
         });
     });
 
     describe("buttonPad metric", () => {
-        it("is 22 if mobile device", () => {
-            expect(getMetrics({ width: 500 }).buttonPad).to.equal(22);
+        test("is 22 if mobile device", () => {
+            expect(getMetrics({ width: 500 }).buttonPad).toBe(22);
         });
 
-        it("is 24 if tablet/desktop device", () => {
-            expect(getMetrics({ width: 800 }).buttonPad).to.equal(24);
+        test("is 24 if tablet/desktop device", () => {
+            expect(getMetrics({ width: 800 }).buttonPad).toBe(24);
         });
     });
 
     describe("buttonMin metric", () => {
-        it("is 42 if mobile device", () => {
-            expect(getMetrics({ width: 500 }).buttonMin).to.equal(42);
+        test("is 42 if mobile device", () => {
+            expect(getMetrics({ width: 500 }).buttonMin).toBe(42);
         });
 
-        it("is 64 if tablet/desktop device", () => {
-            expect(getMetrics({ width: 800 }).buttonMin).to.equal(64);
+        test("is 64 if tablet/desktop device", () => {
+            expect(getMetrics({ width: 800 }).buttonMin).toBe(64);
         });
     });
 
     describe("hitMin metric", () => {
-        it("is 64 if mobile device", () => {
-            expect(getMetrics({ width: 500 }).hitMin).to.equal(64);
+        test("is 64 if mobile device", () => {
+            expect(getMetrics({ width: 500 }).hitMin).toBe(64);
         });
 
-        it("is 70 if tablet/desktop device", () => {
-            expect(getMetrics({ width: 800 }).hitMin).to.equal(70);
+        test("is 70 if tablet/desktop device", () => {
+            expect(getMetrics({ width: 800 }).hitMin).toBe(70);
         });
     });
 
     describe("horizontals metric", () => {
-        it("returns horizontals in relation to the width and scale", () => {
+        test("returns horizontals in relation to the width and scale", () => {
             const expectedFor600 = { left: -400, center: 0, right: 400 };
             const expectedFor800 = { left: -400, center: 0, right: 400 };
             const expectedFor1000 = { left: -250, center: 0, right: 250 };
-            expect(getMetrics({ width: 600 }).horizontals).to.eql(expectedFor600);
-            expect(getMetrics({ width: 800 }).horizontals).to.eql(expectedFor800);
-            expect(getMetrics({ width: 1000, stageHeight: 300 }).horizontals).to.eql(expectedFor1000);
+            expect(getMetrics({ width: 600 }).horizontals).toEqual(expectedFor600);
+            expect(getMetrics({ width: 800 }).horizontals).toEqual(expectedFor800);
+            expect(getMetrics({ width: 1000, stageHeight: 300 }).horizontals).toEqual(expectedFor1000);
         });
     });
 
     describe("safeHorizontals metric", () => {
-        it("returns safe horizontals in relation to the stage height", () => {
+        test("returns safe horizontals in relation to the stage height", () => {
             const expectedFor600 = { left: -400, center: 0, right: 400 };
             const expectedFor768 = { left: -512, center: 0, right: 512 };
             const expectedFor1080 = { left: -720, center: 0, right: 720 };
-            expect(getMetrics({ stageHeight: 600 }).safeHorizontals).to.eql(expectedFor600);
-            expect(getMetrics({ stageHeight: 768 }).safeHorizontals).to.eql(expectedFor768);
-            expect(getMetrics({ stageHeight: 1080 }).safeHorizontals).to.eql(expectedFor1080);
+            expect(getMetrics({ stageHeight: 600 }).safeHorizontals).toEqual(expectedFor600);
+            expect(getMetrics({ stageHeight: 768 }).safeHorizontals).toEqual(expectedFor768);
+            expect(getMetrics({ stageHeight: 1080 }).safeHorizontals).toEqual(expectedFor1080);
         });
     });
 
     describe("verticals metric", () => {
-        it("returns verticals in relation to the stage height", () => {
+        test("returns verticals in relation to the stage height", () => {
             const expectedFor600 = { top: -300, middle: 0, bottom: 300 };
             const expectedFor800 = { top: -400, middle: 0, bottom: 400 };
             const expectedFor1000 = { top: -500, middle: 0, bottom: 500 };
-            expect(getMetrics({ stageHeight: 600 }).verticals).to.eql(expectedFor600);
-            expect(getMetrics({ stageHeight: 800 }).verticals).to.eql(expectedFor800);
-            expect(getMetrics({ stageHeight: 1000 }).verticals).to.eql(expectedFor1000);
+            expect(getMetrics({ stageHeight: 600 }).verticals).toEqual(expectedFor600);
+            expect(getMetrics({ stageHeight: 800 }).verticals).toEqual(expectedFor800);
+            expect(getMetrics({ stageHeight: 1000 }).verticals).toEqual(expectedFor1000);
         });
     });
 });
