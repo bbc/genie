91d0a93ff2c08bddd55d9ed90796694700ebdda9
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-14 17:25:56 +0100

    Adds working tests for overlay layout

--- test/components/overlays/overlay-layout.spec.js
+++ test/components/overlays/overlay-layout.spec.js
@@ -3,12 +3,9 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { assert } from "chai";
-import * as sinon from "sinon";
 import * as OverlayLayout from "../../../src/components/overlays/overlay-layout";
 
 describe("Overlay Layout", () => {
-    const sandbox = sinon.createSandbox();
     let mockLayouts;
     let mockScreen;
 
@@ -18,12 +15,12 @@ describe("Overlay Layout", () => {
                 buttons: {
                     audio: {
                         input: { enabled: true },
-                        update: sandbox.spy(),
+                        update: jest.fn(),
                         name: "audio",
                     },
                     settings: {
                         input: { enabled: false },
-                        update: sandbox.spy(),
+                        update: jest.fn(),
                         name: "audio",
                     },
                 },
@@ -33,61 +30,59 @@ describe("Overlay Layout", () => {
         mockScreen = {
             context: { popupScreens: ["pause", "how-to-play"] },
             scene: {
-                getLayouts: sandbox.stub().returns(mockLayouts),
-                addToBackground: sandbox.spy(),
+                getLayouts: jest.fn().mockImplementation(() => mockLayouts),
+                addToBackground: jest.fn(),
             },
         };
     });
 
-    afterEach(() => {
-        sandbox.restore();
-    });
+    afterEach(() => jest.clearAllMocks());
 
     describe("addBackground method", () => {
-        it("adds a background image and positions it in the correct order", () => {
+        test("adds a background image and positions it in the correct order", () => {
             const overlayLayout = OverlayLayout.create(mockScreen);
             const mockBackgroundImage = { inputEnabled: false, input: {} };
             overlayLayout.addBackground(mockBackgroundImage);
 
-            assert.isTrue(mockBackgroundImage.inputEnabled);
-            assert.equal(mockBackgroundImage.input.priorityID, 1002);
-            assert.deepEqual(mockScreen.scene.addToBackground.getCall(0).args[0], mockBackgroundImage);
+            expect(mockBackgroundImage.inputEnabled).toBe(true);
+            expect(mockBackgroundImage.input.priorityID).toBe(1002);
+            expect(mockScreen.scene.addToBackground.mock.calls[0][0]).toEqual(mockBackgroundImage);
         });
     });
 
     describe("moveGelButtonsToTop method", () => {
-        it("moves buttons to the top layer", () => {
+        test("moves buttons to the top layer", () => {
             const overlayLayout = OverlayLayout.create(mockScreen);
             const mockGelLayout = {
                 buttons: {
                     audio: {
-                        update: sandbox.spy(),
+                        update: jest.fn(),
                         input: { priorityID: 0 },
                         parent: {
-                            updateTransform: sandbox.spy(),
-                            parent: { updateTransform: sandbox.spy() },
+                            updateTransform: jest.fn(),
+                            parent: { updateTransform: jest.fn() },
                         },
                     },
                 },
             };
             overlayLayout.moveGelButtonsToTop(mockGelLayout);
-            assert.equal(mockGelLayout.buttons.audio.input.priorityID, 1003);
-            assert.equal(mockGelLayout.buttons.audio.parent.updateTransform.calledOnce, true);
-            assert.equal(mockGelLayout.buttons.audio.parent.parent.updateTransform.calledOnce, true);
-            assert.equal(mockGelLayout.buttons.audio.update.calledOnce, true);
+            expect(mockGelLayout.buttons.audio.input.priorityID).toEqual(1003);
+            expect(mockGelLayout.buttons.audio.parent.updateTransform).toHaveBeenCalledTimes(1);
+            expect(mockGelLayout.buttons.audio.parent.parent.updateTransform).toHaveBeenCalledTimes(1);
+            expect(mockGelLayout.buttons.audio.update).toHaveBeenCalledTimes(1);
         });
     });
 
     describe("moveToTop method", () => {
-        it("moves an item to the top layer", () => {
+        test("moves an item to the top layer", () => {
             const overlayLayout = OverlayLayout.create(mockScreen);
             const mockItem = {
                 inputEnabled: false,
                 input: { priorityID: 0 },
             };
             overlayLayout.moveToTop(mockItem);
-            assert.isTrue(mockItem.inputEnabled);
-            assert.equal(mockItem.input.priorityID, 1003);
+            expect(mockItem.inputEnabled).toBe(true);
+            expect(mockItem.input.priorityID).toBe(1003);
         });
     });
 });
