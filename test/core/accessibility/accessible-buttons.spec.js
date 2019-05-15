fb7afd50a19952fe160736bbd00383ecd5d67737
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-15 17:11:32 +0100

    Adds accessible buttons test

--- test/core/accessibility/accessible-buttons.spec.js
+++ test/core/accessibility/accessible-buttons.spec.js
@@ -6,12 +6,12 @@
 import { Buttons, findButtonByElementId } from "../../../src/core/accessibility/accessible-buttons.js";
 
 describe("#Buttons", () => {
-    it("returns an object", () => {
+    test("returns an object", () => {
         expect(typeof Buttons).toBe("object");
     });
 });
 
-describe("#findButtonByElementId", () => {
+describe("findButtonByElementId Method", () => {
     let button1;
 
     beforeEach(() => {
@@ -19,7 +19,7 @@ describe("#findButtonByElementId", () => {
         Buttons["home__something"] = button1;
     });
 
-    it("takes an id and returns a Phaser button", () => {
+    test("takes an id and returns a Phaser button", () => {
         const btn = findButtonByElementId("home__something");
         expect(btn).toEqual(button1);
     });
