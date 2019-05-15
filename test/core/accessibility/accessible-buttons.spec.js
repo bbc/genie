792849e5da868a497ca55e3df200c39ae3dfd112
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-15 11:40:55 +0100

    Updates accessible buttons

--- test/core/accessibility/accessible-buttons.spec.js
+++ test/core/accessibility/accessible-buttons.spec.js
@@ -3,25 +3,24 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { expect } from "chai";
 import { Buttons, findButtonByElementId } from "../../../src/core/accessibility/accessible-buttons.js";
 
 describe("#Buttons", () => {
     it("returns an object", () => {
-        expect(Buttons).to.be.an("object");
+        expect(typeof Buttons).toBe("object");
     });
 });
 
 describe("#findButtonByElementId", () => {
     let button1;
 
-    before(() => {
+    beforeEach(() => {
         button1 = { name: "play" };
         Buttons["home__something"] = button1;
     });
 
     it("takes an id and returns a Phaser button", () => {
         const btn = findButtonByElementId("home__something");
-        expect(btn).to.eql(button1);
+        expect(btn).toEqual(button1);
     });
 });
