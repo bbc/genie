3c37bd2be3dc57afd1ad9a9b00ee2d027ab4d8d0
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-18 18:35:27 +0100

    Adds asset loader tests

--- test/core/asset-loader.spec.js
+++ test/core/asset-loader.spec.js
@@ -3,14 +3,9 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { assert } from "chai";
-import * as sinon from "sinon";
-
 import * as AssetLoader from "../../src/core/asset-loader.js";
 
 describe("Asset Loader", () => {
-    const sandbox = sinon.createSandbox();
-
     let assetsLoaded;
     let fakeGame;
     let fakeGamePacksToLoad;
@@ -26,15 +21,15 @@ describe("Asset Loader", () => {
         fakeLoadscreenPack = { key: "loadscreen", url: "loader/loadscreen-pack.json" };
         fakeGame = {
             load: {
-                onFileComplete: { add: sandbox.stub(), removeAll: sandbox.stub() },
-                onLoadComplete: { add: sandbox.stub(), removeAll: sandbox.stub() },
-                json: sandbox.stub(),
-                pack: sandbox.stub(),
-                totalQueuedPacks: sandbox.stub().returns(0),
+                onFileComplete: { add: jest.fn(), removeAll: jest.fn() },
+                onLoadComplete: { add: jest.fn(), removeAll: jest.fn() },
+                json: jest.fn(),
+                pack: jest.fn(),
+                totalQueuedPacks: jest.fn().mockImplementation(() => 0),
                 start: "start",
             },
-            time: { events: { add: sandbox.stub() } },
-            cache: { getJSON: sandbox.stub() },
+            time: { events: { add: jest.fn() } },
+            cache: { getJSON: jest.fn() },
             state: {
                 states: {
                     __proto: {},
@@ -43,77 +38,81 @@ describe("Asset Loader", () => {
                 },
             },
         };
-        fakeCallback = sandbox.stub();
+        fakeCallback = jest.fn();
 
-        fakeGame.cache.getJSON.withArgs("MasterAssetPack").returns({
-            howToPlay: [{ type: "image", key: "background", url: "background.png", overwrite: false }],
-            pause: [{ type: "image", key: "title", url: "title.png", overwrite: false }],
+        fakeGame.cache.getJSON.mockImplementation(packType => {
+            if (packType === "MasterAssetPack") {
+                return {
+                    howToPlay: [{ type: "image", key: "background", url: "background.png", overwrite: false }],
+                    pause: [{ type: "image", key: "title", url: "title.png", overwrite: false }],
+                };
+            }
+            if (packType === "GelAssetPack") {
+                return { gelDesktop: [{ type: "spritesheet", key: "achievements", url: "achievements.png" }] };
+            }
+            if (packType === "character-select") {
+                return {
+                    "character-select": [
+                        { type: "image", key: "title", url: "title.png", overwrite: false },
+                        { type: "image", key: "background", url: "background.png", overwrite: false },
+                    ],
+                };
+            }
         });
-        fakeGame.cache.getJSON
-            .withArgs("GelAssetPack")
-            .returns({ gelDesktop: [{ type: "spritesheet", key: "achievements", url: "achievements.png" }] });
-        fakeGame.cache.getJSON.withArgs("character-select").returns({
-            "character-select": [
-                { type: "image", key: "title", url: "title.png", overwrite: false },
-                { type: "image", key: "background", url: "background.png", overwrite: false },
-            ],
-        });
-
         assetsLoaded = AssetLoader.loadAssets(fakeGame, fakeGamePacksToLoad, fakeLoadscreenPack, fakeCallback);
 
-        nextLoadInQueue = fakeGame.load.onLoadComplete.add.getCall(0).args[0];
+        nextLoadInQueue = fakeGame.load.onLoadComplete.add.mock.calls[0][0];
         nextLoadInQueue();
         nextLoadInQueue();
         nextLoadInQueue();
     });
 
-    afterEach(() => {
-        sandbox.restore();
-    });
+    afterEach(() => jest.clearAllMocks());
 
     it("loads all the game json files", () => {
         return assetsLoaded.then(() => {
-            sandbox.assert.calledOnce(fakeGame.load.json.withArgs("MasterAssetPack", "asset-master-pack.json"));
-            sandbox.assert.calledOnce(fakeGame.load.json.withArgs("GelAssetPack", "gel/gel-pack.json"));
+            expect(fakeGame.load.json).toHaveBeenCalledWith("MasterAssetPack", "asset-master-pack.json");
+            expect(fakeGame.load.json).toHaveBeenCalledWith("GelAssetPack", "gel/gel-pack.json");
         });
     });
 
     it("loads json files for missing screens", () => {
         return assetsLoaded.then(() => {
-            sandbox.assert.calledOnce(fakeGame.load.json.withArgs("character-select", "character-select.json"));
+            expect(fakeGame.load.json).toHaveBeenCalledWith("character-select", "character-select.json");
         });
     });
 
     it("does not loads json files for __proto and default screens", () => {
         return assetsLoaded.then(() => {
-            assert.isTrue(fakeGame.load.json.neverCalledWith("__proto"));
-            assert.isTrue(fakeGame.load.json.neverCalledWith("default"));
+            expect(fakeGame.load.json).not.toHaveBeenCalledWith("__proto");
+            expect(fakeGame.load.json).not.toHaveBeenCalledWith("default");
         });
     });
 
     it("saves the json files to the game cache", () => {
         return assetsLoaded.then(() => {
-            sandbox.assert.calledOnce(fakeGame.cache.getJSON.withArgs("MasterAssetPack"));
-            sandbox.assert.calledOnce(fakeGame.cache.getJSON.withArgs("GelAssetPack"));
-            sandbox.assert.calledOnce(fakeGame.cache.getJSON.withArgs("character-select"));
+            expect(fakeGame.cache.getJSON).toHaveBeenCalledWith("MasterAssetPack");
+            expect(fakeGame.cache.getJSON).toHaveBeenCalledWith("GelAssetPack");
+            expect(fakeGame.cache.getJSON).toHaveBeenCalledWith("character-select");
         });
     });
 
     it("loads the loadscreen json", () => {
         return assetsLoaded.then(() => {
-            sandbox.assert.calledOnce(fakeGame.load.pack.withArgs("loadscreen", "loader/loadscreen-pack.json"));
+            expect(fakeGame.load.pack).toHaveBeenCalledWith("loadscreen", "loader/loadscreen-pack.json");
         });
     });
 
     it("adds a game time event for each load iteration", () => {
         return assetsLoaded.then(() => {
-            sandbox.assert.calledThrice(fakeGame.time.events.add.withArgs(0, fakeGame.load.start, fakeGame.load));
+            expect(fakeGame.time.events.add).toHaveBeenCalledTimes(3);
+            expect(fakeGame.time.events.add).toHaveBeenCalledWith(0, fakeGame.load.start, fakeGame.load);
         });
     });
 
     it("updates the callback with the load progress so far", () => {
         return assetsLoaded.then(() => {
-            sandbox.assert.calledOnce(fakeCallback.withArgs(100));
+            expect(fakeCallback).toHaveBeenCalledWith(100);
         });
     });
 
@@ -135,15 +134,15 @@ describe("Asset Loader", () => {
         };
         nextLoadInQueue();
         return assetsLoaded.then(keyLookUps => {
-            assert.deepEqual(keyLookUps, expectedLookUps);
+            expect(keyLookUps).toEqual(expectedLookUps);
         });
     });
 
     it("clears all the files when loading is complete", () => {
         nextLoadInQueue();
         return assetsLoaded.then(() => {
-            sandbox.assert.calledOnce(fakeGame.load.onLoadComplete.removeAll);
-            sandbox.assert.calledOnce(fakeGame.load.onFileComplete.removeAll);
+            expect(fakeGame.load.onLoadComplete.removeAll).toHaveBeenCalled();
+            expect(fakeGame.load.onFileComplete.removeAll).toHaveBeenCalled();
         });
     });
 });
