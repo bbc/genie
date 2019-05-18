3ab0bf68145f0c788c51f7a00ce40b8954a7f38c
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-18 19:50:34 +0100

    Adds startup tests

--- test/core/startup.spec.js
+++ test/core/startup.spec.js
@@ -3,8 +3,9 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { assert } from "chai";
-import * as sinon from "sinon";
+import { createMockGmi } from "../fake/gmi";
+import { domElement } from "../fake/dom-element";
+
 import { startup } from "../../src/core/startup.js";
 import * as gmiModule from "../../src/core/gmi/gmi.js";
 import * as Game from "../fake/game.js";
@@ -16,49 +17,43 @@ import * as qaMode from "../../src/core/qa/qa-mode.js";
 
 describe("Startup", () => {
     let mockGmi;
-    const sandbox = sinon.createSandbox();
-    let PhaserGame, containerDiv;
+    let containerDiv;
 
     beforeEach(() => {
         mockGmi = {
+            gmi: jest.fn(),
+            setGmi: jest.fn(),
+            startStatsTracking: jest.fn(),
             gameContainerId: "some-id",
         };
-        containerDiv = sandbox.stub();
+        createMockGmi(mockGmi);
 
-        sandbox
-            .stub(document, "getElementById")
-            .withArgs(mockGmi.gameContainerId)
-            .returns(containerDiv);
+        containerDiv = domElement();
 
-        sandbox.stub(gmiModule, "setGmi");
-        sandbox.stub(gmiModule, "startStatsTracking");
-        sandbox.stub(styles, "addCustomStyles");
-        sandbox.replace(gmiModule, "gmi", mockGmi);
-        PhaserGame = sandbox.stub(Phaser, "Game").returns(Game.Stub);
-        window.getGMI = sandbox.stub().returns(mockGmi);
+        jest.spyOn(global.document, "getElementById").mockImplementation(argument => {
+            if (argument === mockGmi.gameContainerId) {
+                return containerDiv;
+            }
+        });
+        jest.spyOn(styles, "addCustomStyles");
+        jest.spyOn(Phaser, "Game").mockImplementation(() => Game.Stub);
+        global.window.getGMI = jest.fn().mockImplementation(() => mockGmi);
     });
 
-    afterEach(() => {
-        sandbox.restore();
-    });
+    afterEach(() => jest.clearAllMocks());
 
-    it("instantiates the GMI with correct params", () => {
+    test("instantiates the GMI with correct params", () => {
         const fakeSettings = { settings: "some settings" };
         startup(fakeSettings);
-        sandbox.assert.calledOnce(gmiModule.setGmi.withArgs(fakeSettings, window));
+        expect(gmiModule.setGmi).toHaveBeenCalledWith(fakeSettings, global.window);
     });
 
-    it("injects custom styles to the game container element", () => {
+    test("injects custom styles to the game container element", () => {
         startup();
-        sandbox.assert.calledOnce(styles.addCustomStyles);
+        expect(styles.addCustomStyles).toHaveBeenCalled();
     });
 
-    it("creates a new Phaser game", () => {
-        startup();
-        sandbox.assert.calledWithNew(PhaserGame);
-    });
-
-    it("creates a new Phaser game with correct config", () => {
+    test("creates a new Phaser game with correct config", () => {
         startup();
 
         const expectedConfig = {
@@ -71,71 +66,71 @@ describe("Startup", () => {
             transparent: true,
         };
 
-        const actualConfig = PhaserGame.getCall(0).args[0];
+        const actualConfig = Phaser.Game.mock.calls[0][0];
 
-        assert.equal(actualConfig.width, expectedConfig.width);
-        assert.equal(actualConfig.height, expectedConfig.height);
-        assert.equal(actualConfig.renderer, expectedConfig.renderer);
-        assert.equal(actualConfig.antialias, expectedConfig.antialias);
-        assert.equal(actualConfig.multiTexture, expectedConfig.multiTexture);
-        assert.equal(actualConfig.parent, expectedConfig.parent);
-        assert.equal(actualConfig.transparent, expectedConfig.transparent);
+        expect(actualConfig.width).toBe(expectedConfig.width);
+        expect(actualConfig.height).toBe(expectedConfig.height);
+        expect(actualConfig.renderer).toBe(expectedConfig.renderer);
+        expect(actualConfig.antialias).toBe(expectedConfig.antialias);
+        expect(actualConfig.multiTexture).toBe(expectedConfig.multiTexture);
+        expect(actualConfig.parent).toEqual(expectedConfig.parent);
+        expect(actualConfig.transparent).toBe(expectedConfig.transparent);
     });
 
-    it("throws an error if the game container element cannot be found", () => {
-        mockGmi.gameContainerId = "not-existing";
-        assert.throws(startup, 'Container element "#not-existing" not found'); // eslint-disable-line quotes
+    test("throws an error if the game container element cannot be found", () => {
+        document.getElementById.mockImplementation(() => false);
+        expect(startup).toThrow(); // eslint-disable-line quotes
     });
 
     describe("onStarted()", () => {
-        let sceneCreate;
-        let loadFonts;
-        let navigationCreate;
-
         beforeEach(() => {
-            sceneCreate = sandbox.stub(Scene, "create").returns("Scene");
-            loadFonts = sandbox.stub(LoadFonts, "loadFonts");
-            navigationCreate = sandbox.stub(Navigation, "create");
-            sandbox.stub(qaMode, "create");
+            jest.spyOn(Scene, "create").mockImplementation(() => "Scene");
+            jest.spyOn(LoadFonts, "loadFonts").mockImplementation(() => {});
+            jest.spyOn(Navigation, "create").mockImplementation(() => {});
+            jest.spyOn(qaMode, "create").mockImplementation(() => {});
 
             startup({}, "NavConfig");
-            const game = PhaserGame.getCall(0).args[0];
+            const game = Phaser.Game.mock.calls[0][0];
             game.state._onStarted();
         });
 
         afterEach(() => {
-            sandbox.restore();
-            delete window.__qaMode;
+            jest.clearAllMocks();
+            delete global.window.__qaMode;
         });
 
-        it("creates the scene", () => {
-            sandbox.assert.calledWith(sceneCreate, PhaserGame());
+        test("creates the scene", () => {
+            expect(Scene.create).toHaveBeenCalledWith(Game.Stub);
         });
 
-        it("loads the fonts", () => {
-            sandbox.assert.calledWith(loadFonts, PhaserGame(), sandbox.match.func);
+        test("loads the fonts", () => {
+            expect(LoadFonts.loadFonts).toHaveBeenCalledWith(Game.Stub, expect.any(Function));
         });
 
-        it("creates the game navigation", () => {
-            const onComplete = loadFonts.getCall(0).args[1];
+        test("creates the game navigation", () => {
+            const onComplete = LoadFonts.loadFonts.mock.calls[0][1];
             onComplete();
-            sandbox.assert.calledWith(navigationCreate, Game.Stub.state, sandbox.match.object, "Scene", "NavConfig");
+            expect(Navigation.create.mock.calls[0]).toEqual([
+                Game.Stub.state,
+                expect.any(Object),
+                "Scene",
+                "NavConfig",
+            ]);
         });
 
-        it("creates qaMode if the qaMode url parameter is set to true", () => {
-            const onComplete = loadFonts.getCall(0).args[1];
+        test("creates qaMode if the qaMode url parameter is set to true", () => {
+            const onComplete = LoadFonts.loadFonts.mock.calls[0][1];
             onComplete();
-
-            sandbox.assert.calledOnce(qaMode.create);
+            expect(qaMode.create).toHaveBeenCalled();
         });
-        it("starts the stats tracking through the GMI", () => {
+        test("starts the stats tracking through the GMI", () => {
             const expectedContext = {
                 popupScreens: [],
                 gameMuted: true,
             };
-            const statsParams = gmiModule.startStatsTracking.getCall(0).args;
-            assert.deepEqual(statsParams[0], Game.Stub);
-            assert.deepEqual(JSON.stringify(statsParams[1]), JSON.stringify(expectedContext));
+            const statsParams = gmiModule.startStatsTracking.mock.calls[0];
+            expect(statsParams[0]).toEqual(Game.Stub);
+            expect(JSON.stringify(statsParams[1])).toEqual(JSON.stringify(expectedContext));
         });
     });
 });
