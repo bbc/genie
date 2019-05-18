9ef18570b9811317db6c83979c6440f5879e02ab
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-18 17:04:01 +0100

    Adds fullscreen tests

--- test/core/fullscreen.spec.js
+++ test/core/fullscreen.spec.js
@@ -3,27 +3,20 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import * as sinon from "sinon";
-
 import * as fullscreen from "../../src/core/fullscreen.js";
 
-describe("fullscreen", () => {
-    const sandbox = sinon.createSandbox();
-
+describe("Fullscreen", () => {
     it("adds a touchend listener", () => {
-        const mockRoot = {
-            addEventListener: sandbox.spy(),
-        };
-
+        const mockRoot = { addEventListener: jest.fn() };
         fullscreen.listenForTap(mockRoot, { scale: { startFullScreen: () => {} } });
-        sandbox.assert.calledWith(mockRoot.addEventListener, "touchend");
+        expect(mockRoot.addEventListener.mock.calls[0][0]).toBe("touchend");
     });
 
     it("Removes the event listener on first touch and starts fullscreen", () => {
         const mockRoot = document.createElement("div");
         const mockGame = {
             scale: {
-                startFullScreen: sandbox.spy(),
+                startFullScreen: jest.fn(),
             },
         };
 
@@ -32,6 +25,6 @@ describe("fullscreen", () => {
         mockRoot.dispatchEvent(new Event("touchend"));
         mockRoot.dispatchEvent(new Event("touchend")); //second event should be ignored
 
-        sinon.assert.calledOnce(mockGame.scale.startFullScreen);
+        expect(mockGame.scale.startFullScreen).toHaveBeenCalledTimes(1);
     });
 });
