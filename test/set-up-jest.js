433be2a396ca5248475d1ef3c89c22d3cd71f64d
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-22 11:05:16 +0100

    Moves set up file

--- test/set-up-jest.js
+++ test/set-up-jest.js
@@ -6,11 +6,11 @@
 
 import "jest-canvas-mock";
 
-const p2 = require("../../node_modules/phaser-ce/build/custom/p2.js");
+const p2 = require("../node_modules/phaser-ce/build/custom/p2.js");
 global.p2 = p2;
-const PIXI = require("../../node_modules/phaser-ce/build/custom/pixi.js");
+const PIXI = require("../node_modules/phaser-ce/build/custom/pixi.js");
 global.PIXI = PIXI;
-const phaserSplit = require("../../node_modules/phaser-ce/build/custom/phaser-split.js");
+const phaserSplit = require("../node_modules/phaser-ce/build/custom/phaser-split.js");
 global.Phaser = phaserSplit;
 
 const jsdom = require("jsdom");
