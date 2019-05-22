dbedc7a35965a15c4310be857b8abd3a228f75e5
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-05-22 10:57:29 +0100

    Tidy up

--- jest.config.js
+++ jest.config.js
@@ -8,15 +8,15 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js", "!src/components/test-harness/**/*.js", "!src/output/**/*.js"],
     coverageThreshold: {
         global: {
-            statements: 87.5,
+            statements: 87.83,
             branches: 77.42,
-            lines: 88.6,
-            functions: 84.88,
+            lines: 88.95,
+            functions: 85.17,
         },
     },
     testEnvironment: "jsdom",
     modulePathIgnorePatterns: ["/.node_modules.+/"],
     testPathIgnorePatterns: ["/node_modules/", "/.node_modules.+/", ".node_modules_production"],
-    setupTestFrameworkScriptFile: "./test/set-up/set-up-jest.js",
+    setupTestFrameworkScriptFile: "./test/set-up-jest.js",
     transform: { "^.+\\.js$": "babel-jest" },
 };
--- test/set-up-jest.js
+++ test/set-up-jest.js
@@ -6,26 +6,6 @@
 
 import "jest-canvas-mock";
 
-// global.Phaser = {
-//     Sprite: function() {},
-//     Group: function() {},
-//     Button: function() {},
-//     Signal: function() {
-//         var listener;
-//         // NOTE: this is different from actual implementation
-//         // supports only one handler
-//         this.addOnce = this.add = function(cb) {
-//             listener = cb;
-//         };
-//         this.dispatch = function() {
-//             if (listener) {
-//                 listener();
-//             }
-//         };
-//     },
-// };
-
-// import phaser from "phaser-ce";
 const p2 = require("../../node_modules/phaser-ce/build/custom/p2.js");
 global.p2 = p2;
 const PIXI = require("../../node_modules/phaser-ce/build/custom/pixi.js");
