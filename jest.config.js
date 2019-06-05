6aeb679ce53838d2338cf53e08d2c37a57dbf902
alex-s2 <alexandra.sneddon@bbc.co.uk>
2019-06-05 16:03:33 +0100

    Changelog

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js", "!src/components/test-harness/**/*.js", "!src/output/**/*.js"],
     coverageThreshold: {
         global: {
-            statements: 90.01,
-            branches: 80.84,
+            statements: 90.95,
+            branches: 83.33,
             functions: 86.1,
-            lines: 91.17,
+            lines: 92.01,
         },
     },
     testEnvironment: "jsdom",
