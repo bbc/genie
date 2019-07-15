5b4363081e923d32f275f6a933c1d06c3e7137b6
Asa Dawson <asa.dawson@bbc.co.uk>
2019-07-15 11:12:34 +0100

    Handle script exit

--- build-scripts/schemavalidator/validator.js
+++ build-scripts/schemavalidator/validator.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const ajv = new Ajv();
 
-var hasBeenInvalid = false;
-
+const rootThemeFolder = './themes/';
 const schema = {
     "$schema": "http://json-schema.org/draft-07/schema#",
     "title": "Achievements",
@@ -57,29 +56,47 @@ const schema = {
     }
 };
 
-const rootThemeFolder = './themes/';
 const validate = ajv.compile(schema);
+var hasBeenInvalid = false;
 
 const validateSchema = (filepath) => {
     return (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        
         const jsonData = JSON.parse(data);
         const valid = validate(jsonData);
-    
+
+        console.log(`======== ${filepath}`)
         if (!valid) {
-            console.log(`✖ Invalid JSON:\t${filepath}`);
+            console.log(`✖ Invalid JSON`);
             validate.errors.forEach((item) => {
                 console.log(`\tJSON SCHEMA:\t${item.schemaPath}`);
                 console.log(`\tDATAPATH:\t${item.dataPath}`);
-                console.log(`\tMESSAGE:\t${item.message}\n`);
+                console.log(`\tMESSAGE:\t${item.message}\n\n`);
             });
             hasBeenInvalid = true;
             return;
         }
-        console.log(`✓ Valid JSON: \t${filepath}`);
+        console.log(`✓ Valid JSON\n\n`);
     }
 }
 
+// Read[dir/file] are async so it's hard to pinpoint when to throw an exit
+// handle the exit instead and throw a non-zero exit if any JSON was invalid
+const exitHandle = () => {
+    process.exit(hasBeenInvalid ? 1 : 0);
+}
+
+process.on('exit', exitHandle.bind(null));
+
 fs.readdir(rootThemeFolder, { withFileTypes: true }, (err, files) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
     files.forEach(file => {
         if(file.isDirectory) {
             const filePath = path.join(rootThemeFolder, file.name, "achievements", "config.json");
@@ -87,10 +104,3 @@ fs.readdir(rootThemeFolder, { withFileTypes: true }, (err, files) => {
         }
     });
 });
-
-console.log(hasBeenInvalid);
-
-if (hasBeenInvalid) {
-    console.log("ERROR");
-    process.exit(1);
-}
