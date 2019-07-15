a2339bd29c3b53c50c04ec5ff31e724959d2debc
Asa Dawson <asa.dawson@bbc.co.uk>
2019-07-15 15:38:44 +0100

    Add validate:themes shortcut

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -4,6 +4,6 @@ set -e
 npm install --force
 python build-scripts/licensechecker/licensechecker.py
 npm run test
-#npm run validate -- build-scripts/schemavalidator/schemas/achievement.json ./themes/*/achievements/config.json # Validates themes
+#npm run validate:themes --  ./themes/*/achievements/config.json # Validates themes
 npm run build
 cp -r themes output/themes
