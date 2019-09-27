cfa851faa86f3f48ae2982d5e8e17f8c35f758f9
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-09-27 15:52:22 +0100

    Update game to use Phaser 3

--- src/components/test-harness/test-screens/game.js
+++ src/components/test-harness/test-screens/game.js
@@ -5,26 +5,27 @@
  */
 import { gmi } from "../../../core/gmi/gmi.js";
 import { Screen } from "../../../core/screen.js";
-import { accessibilify } from "../../../core/accessibility/accessibilify.js";
+// import { accessibilify } from "../../../core/accessibility/accessibilify.js";
 
 export class GameTest extends Screen {
     constructor() {
-        super();
+        super({ key: "game" });
     }
 
     create() {
-        this.layoutManager.addToBackground(this.game.add.image(0, 0, "home.background"));
+        this.add.image(0, 0, "home.background");
+        const title = this.add.text(0, -190, "Game goes here", {
+            font: "65px ReithSans",
+            fill: "#f6931e",
+            align: "center",
+        });
+        title.setOrigin(0.5);
+        this.addLayout(["pause"]);
 
-        const titleStyle = { font: "65px ReithSans", fill: "#f6931e", align: "center" };
-        const titleText = this.game.add.text(0, -190, "Game goes here", titleStyle);
-        titleText.anchor.set(0.5, 0.5);
-        this.layoutManager.addToBackground(titleText);
-        this.layoutManager.addLayout(["pause"]);
-
-        gmi.setGameData("characterSelected", this.transientData.characterSelected);
+        gmi.setGameData("characterSelected", this.data.characterSelected);
         console.log("Data saved to GMI:", gmi.getAllSettings().gameData); // eslint-disable-line no-console
 
-        const buttonKey = this.getAsset("basicButton");
+        const buttonKey = this.scene.key + ".basicButton";
         const buttonTextStyle = {
             font: "40px ReithSans",
             fill: "#fff",
@@ -35,42 +36,44 @@ export class GameTest extends Screen {
 
         [-70, 20, 110].forEach((buttonYPosition, index) => {
             const buttonNumber = index + 1;
-            const buttonText = new Phaser.Text(this.game, 0, 5, "Button " + buttonNumber, buttonTextStyle);
-            const button = this.game.add.button(
-                0,
-                buttonYPosition,
-                buttonKey,
-                () => onGameComplete(buttonNumber),
-                this,
-            );
-            const config = {
-                id: buttonNumber,
-                ariaLabel: buttonText,
-            };
-            accessibilify(button, config);
-            button.anchor.set(0.5, 0.5);
-            button.addChild(buttonText);
-            buttonText.anchor.set(0.5, 0.5);
-            this.layoutManager.addToBackground(button);
+            this.add
+                .image(0, buttonYPosition, buttonKey)
+                .setOrigin(0.5)
+                .setInteractive()
+                .on("pointerdown", () => onGameComplete(buttonNumber));
+            this.add
+                .text(0, buttonYPosition, "Button " + buttonNumber, buttonTextStyle)
+                .setOrigin(0.5)
+                .setInteractive()
+                .on("pointerdown", () => onGameComplete(buttonNumber));
+
+            // const button = this.add.button(0, buttonYPosition, buttonKey, () => onGameComplete(buttonNumber), this);
+            // const config = {
+            //     id: buttonNumber,
+            //     ariaLabel: buttonText,
+            // };
+            // TODO P3
+            // accessibilify(button, config);
+            // button.addChild(buttonText);
+            // this.add(button);
         }, this);
 
-        const onGameComplete = buttonNumber => {
-            const results = {
-                results: "You pressed button " + buttonNumber,
-                characterSelected: this.transientData.characterSelected,
-            };
-            gmi.setGameData("buttonPressed", buttonNumber);
-            console.log("Data saved to GMI:", gmi.getAllSettings().gameData); // eslint-disable-line no-console
-            this.navigation.next(results);
-        };
-
-        const characterSelectedText = this.game.add.text(
-            0,
-            200,
-            "Character Selected: " + this.transientData.characterSelected,
-            { font: "italic 32px ReithSans", fill: "#f6931e", align: "center" },
-        );
-
-        this.layoutManager.addToBackground(characterSelectedText);
+        // const onGameComplete = buttonNumber => {
+        //     const results = {
+        //         results: "You pressed button " + buttonNumber,
+        //         characterSelected: this.transientData.characterSelected,
+        //     };
+        //     gmi.setGameData("buttonPressed", buttonNumber);
+        //     console.log("Data saved to GMI:", gmi.getAllSettings().gameData); // eslint-disable-line no-console
+        //     this.navigation.next(results);
+        // };
+        //
+        // this.add
+        //     .text(0, 200, "Character Selected: " + this.context.transientData.characterSelected, {
+        //         font: "italic 32px ReithSans",
+        //         fill: "#f6931e",
+        //         align: "center",
+        //     })
+        //     .setOrigin(0.5);
     }
 }
