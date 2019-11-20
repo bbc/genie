c184d88060c59866ae2ded7dcfa63949f2ce9edf
Jacob Bracken <jacob.bracken@bbc.co.uk>
2019-11-20 14:04:49 +0000

    fix incorrect filenames

--- test/main.test.js
+++ test/main.test.js
@@ -6,7 +6,7 @@
 
 import { Home } from "../src/components/home";
 import { Results } from "../src/components/results";
-import { Select } from "../src/components/select-new";
+import { Select } from "../src/components/select";
 import { GameTest } from "../src/components/test-screens/game";
 import { Pause } from "../src/components/overlays/pause";
 import { settingsChannel } from "../src/core/settings";
