3db96a6998ee9d42fd227d0aada7250e2dc06cc1
Adam Beswick <adam.beswick@bbc.co.uk>
2019-11-25 09:35:21 +0000

    Chance depth of indicator to 1.

--- src/core/layout/gel-button.js
+++ src/core/layout/gel-button.js
@@ -12,6 +12,7 @@ class Indicator extends Phaser.GameObjects.Sprite {
     constructor(gelButton) {
         super(gelButton.scene, 0, 0, assetPath({ key: "notification", isMobile: gelButton._isMobile }));
         this.scene.add.existing(this);
+        this.setDepth(1);
         this.gelButton = gelButton;
         this.scale = 0;
         this.scene.add.tween({ targets: this, ease: "Bounce", delay: 500, duration: 500, scale: 1 });
