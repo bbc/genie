92f797d5d9c3ef380701ca3a30ed7cf73fe74c87
AnthonyGoodchild <anthony.goodchild@bbc.co.uk>
2019-12-05 12:28:30 +0000

    Items from theme

--- src/core/layout/gel-grid.js
+++ src/core/layout/gel-grid.js
@@ -8,56 +8,6 @@ import * as a11y from "../accessibility/accessibility-layer.js";
 import * as ButtonFactory from "./button-factory.js";
 import { GelButton } from "./gel-button.js";
 
-const horizontal = {
-    left: (metrics, group, horizontalsType) => {
-        let hitAreaOffset = 0;
-        fp.forEach(child => {
-            if (!child.input.hitArea) return;
-            hitAreaOffset = fp.max([hitAreaOffset, -(child.x - child.input.hitArea.width / 2) / metrics.scale]);
-        }, group.list);
-        group.x = metrics[horizontalsType].left + metrics.borderPad + hitAreaOffset;
-    },
-    center: (metrics, group, horizontalsType) => {
-        group.x = metrics[horizontalsType].center - group.getBounds().width / 2;
-    },
-    right: (metrics, group, horizontalsType) => {
-        let hitAreaOffset = 0;
-        fp.forEach(child => {
-            if (!child.input.hitArea) return;
-            hitAreaOffset = fp.max([
-                hitAreaOffset,
-                (child.x + child.input.hitArea.width / 2) / metrics.scale - group.getBounds().width,
-            ]);
-        }, group.list);
-        group.x = metrics[horizontalsType].right - metrics.borderPad - hitAreaOffset - group.getBounds().width;
-    },
-};
-
-const vertical = {
-    top: (metrics, group) => {
-        let hitAreaOffset = 0;
-        fp.forEach(child => {
-            if (!child.input.hitArea) return;
-            hitAreaOffset = fp.max([hitAreaOffset, -(child.y - child.input.hitArea.height / 2) / metrics.scale]);
-        }, group.list);
-        group.y = metrics.verticals.top + metrics.borderPad + hitAreaOffset;
-    },
-    middle: (metrics, group) => {
-        group.y = metrics.verticals.middle;
-    },
-    bottom: (metrics, group) => {
-        let hitAreaOffset = 0;
-        fp.forEach(child => {
-            if (!child.input.hitArea) return;
-            hitAreaOffset = fp.max([
-                hitAreaOffset,
-                (child.y + child.input.hitArea.height / 2) / metrics.scale - group.getBounds().height,
-            ]);
-        }, group.list);
-        group.y = metrics.verticals.bottom - metrics.borderPad - hitAreaOffset - group.getBounds().height;
-    },
-};
-
 export class GelGrid extends Phaser.GameObjects.Container {
     constructor(scene, parent, vPos, hPos, metrics, isSafe, isVertical) {
         super(scene, 0, 0);
@@ -70,15 +20,8 @@ export class GelGrid extends Phaser.GameObjects.Container {
         this._isVertical = isVertical;
         this._cells = [];
         this._buttonFactory = ButtonFactory.create(scene);
-        this._setGroupPosition = metrics => {
-            return; //TODO
-            horizontal[hPos](metrics, this, isSafe ? "safeHorizontals" : "horizontals");
-            vertical[vPos](metrics, this);
-        };
-
-        this.x = -400;
+        this.x = 0;
         this.xOffset = 0; //TODO DELETE ME!!!
-
         window.check = this.scene;
     }
 
@@ -92,15 +35,15 @@ export class GelGrid extends Phaser.GameObjects.Container {
             id: "__exit",
             channel: "gel-buttons-" + this.scene.scene.key,
         };
-
-        [1].forEach(idx => {
-            this.addCell(Object.assign({}, config, { forceKey: "char" + idx }));
-        }, this);
+        this.scene.theme.choices.map((choice, i) => {
+            this.addCell(Object.assign({}, config, { forceKey: choice.asset }));
+            !i ? (this._cells[i].visible = false) : (this._cells[i].visible = true);
+        });
 
         this.makeAccessible();
     }
 
-    // TODO This should return the list of cell keys (obviously) - keys need to be added to the button objects
+    // TODO This should return the list of cell keys (obviously) - keys need to be added to the button ob
     //
     // cellKeys() {
     //     return this._cells.map(cell => {
@@ -109,17 +52,14 @@ export class GelGrid extends Phaser.GameObjects.Container {
     // }
 
     addCell(config, position = this._cells.length) {
-        const newCell = new GelButton(this.scene, this.xOffset, 0, this._metrics, config);
+        const xOffset = 0;
+        const yOffset = 0;
+        const newCell = new GelButton(this.scene, xOffset, yOffset, this._metrics, config);
 
-        this.xOffset = this.xOffset + 200;
-
-        newCell.setScale(0.2);
-        newCell.setSize();
+        newCell.setSize(400);
 
         this.addAt(newCell, position);
         this._cells.push(newCell);
-
-        //this.alignChildren();
 
         return newCell;
     }
@@ -154,6 +94,17 @@ export class GelGrid extends Phaser.GameObjects.Container {
                 pos.x += child.width + this._metrics.buttonPad;
             }
         }, this);
+    }
+
+    reset(metrics) {
+        metrics = metrics || this._metrics;
+        if (this._metrics.isMobile !== metrics.isMobile) {
+            this.resetButtons(metrics);
+        }
+    }
+
+    resetButtons(metrics) {
+        // TODO This should resize the buttons for mobile/desktop breakpoints?
     }
 
     makeAccessible() {
