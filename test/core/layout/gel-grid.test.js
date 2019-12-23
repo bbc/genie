b53d9ef78178ff2aeebd09ae28963be82426dc0e
AnthonyGoodchild <anthony.goodchild@bbc.co.uk>
2019-12-23 09:24:09 +0000

    Initial gel-grid test file

---
+++ test/core/layout/gel-grid.test.js
@@ -0,0 +1,14 @@
+/**
+ * @copyright BBC 2019
+ * @author BBC Children's D+E
+ * @license Apache-2.0
+ */
+import { accessibilify } from "../../../src/core/accessibility/accessibilify.js";
+import { GelGrid } from "../../../src/core/layout/gel-grid.js";
+import { GelButton } from "../../../src/core/layout/gel-button.js";
+
+describe("Grid", () => {
+
+    
+
+});
