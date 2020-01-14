b830e1cf1817f0c6abbbe9eff803d2780e4746d9
Nick Tipping <nick@bigredmonster.com>
2020-01-14 17:22:34 +0000

    Fix button.sprite added to update list instead of button

--- src/core/layout/gel-game-objects.js
+++ src/core/layout/gel-game-objects.js
@@ -8,6 +8,6 @@ import { GelButton } from "./gel-button.js";
 export function addGelButton(x, y, metrics, config) {
     const gelButton = new GelButton(this.scene, x, y, metrics, config);
     this.displayList.add(gelButton);
-    this.updateList.add(gelButton);
+    this.updateList.add(gelButton.sprite);
     return gelButton;
 }
