dc567ea0d05fe0de554f974a0a32051b88734546
Nick Tipping <nick@bigredmonster.com>
2020-01-30 14:13:46 +0000

    * Set character select back to max rows and columns

--- src/components/results/results-screen.js
+++ src/components/results/results-screen.js
@@ -11,8 +11,6 @@ import * as Rows from "../../core/layout/rows.js";
 import { getMetrics, onScaleChange } from "../../core/scaler.js";
 import fp from "../../../lib/lodash/fp/fp.js";
 
-const PADDING_RATIO = 0.05;
-
 const getScoreMetaData = result => {
     if (typeof result === "number") {
         return { metadata: `SCO=[${result}]` };
@@ -40,12 +38,7 @@ export class Results extends Screen {
     }
 
     resultsArea() {
-        const metrics = getMetrics();
-        const safeArea = this.layout.getSafeArea(metrics, { top: false });
-        const bottomPadding = metrics.width * PADDING_RATIO;
-        safeArea.y += bottomPadding / 2;
-        safeArea.height -= bottomPadding;
-        return safeArea;
+        return this.layout.getSafeArea(getMetrics(), { top: false, left: "topLeft" });
     }
 
     createLayout() {
