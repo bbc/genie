6f83c22b1e33d94c02f8b80a32ef14d630c72535
Jacob Bracken <jacob.bracken@bbc.co.uk>
2020-01-30 14:05:58 +0000

    fix spelling errors

--- src/components/results/results-row.js
+++ src/components/results/results-row.js
@@ -16,7 +16,7 @@ export class ResultsRow extends Phaser.GameObjects.Container {
         this.drawRow();
         this.setContainerPosition();
         this.align();
-        this.alpha = 0; // hides the row until the transition tween is appliedg
+        this.alpha = 0; // hides the row until the transition tween is applied
     }
 
     align() {
