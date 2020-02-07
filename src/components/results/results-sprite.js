252c4a732ed209e9dd78d15f476997fbcd96efee
Jacob Bracken <jacob.bracken@bbc.co.uk>
2020-02-07 08:46:08 +0000

    improve readability

--- src/components/results/results-sprite.js
+++ src/components/results/results-sprite.js
@@ -1,10 +1,10 @@
-import { gmi } from "../../core/gmi/gmi.js";
-
 /**
  * @copyright BBC 2020
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
+
+import { gmi } from "../../core/gmi/gmi.js";
 
 export class ResultsSprite extends Phaser.GameObjects.Sprite {
     constructor(scene, config) {
