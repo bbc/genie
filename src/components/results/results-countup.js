e7ec63d8b8a9ac7a24c174e81fcfedd346c3c54a
Adam Beswick <adam.beswick@bbc.co.uk>
2020-02-10 11:02:34 +0000

    Refactor into update loop

--- src/components/results/results-countup.js
+++ src/components/results/results-countup.js
@@ -6,15 +6,28 @@
 
 import fp from "../../../lib/lodash/fp/fp.js";
 
+const COUNTUP_STATE = {
+    DELAYED: 0,
+    COUNTING: 1,
+    ENDED: 2,
+};
+
 export class ResultsCountup extends Phaser.GameObjects.Text {
     constructor(scene, config) {
         super(scene, 0, 0, undefined, config.textStyle);
         this.config = config;
         this.startCount = this.textFromTemplate(config.startCount, scene.transientData);
         this.endCount = this.textFromTemplate(config.endCount, scene.transientData);
+
+        this.delayProgress = 0;
+        this.currentValue = parseInt(this.startCount);
+        this.numberOfTicks = this.endCount - this.startCount;
+
         this.text = this.startCount;
         this.setFixedSize(this.getFinalWidth(this.endCount), 0);
-        this.startCountWithDelay(config.startDelay);
+
+        this.countupState = COUNTUP_STATE.DELAYED;
+        scene.events.on(Phaser.Scenes.Events.UPDATE, this.update.bind(this));
     }
 
     getFinalWidth(finalText) {
@@ -30,39 +43,40 @@ export class ResultsCountup extends Phaser.GameObjects.Text {
         return template(transientData[this.scene.scene.key]);
     }
 
-    canPlaySound(value) {
-        const fireRate = this.config.audio.fireRate >= 1 ? this.config.audio.fireRate : 1;
-        return value % fireRate === 0;
+    update(_, dt) {
+        if (this.countupState === COUNTUP_STATE.DELAYED) {
+            this.incrementDelayCount(dt, this.config.startDelay);
+        }
+        if (this.countupState === COUNTUP_STATE.COUNTING) {
+            this.incrementCount(dt, this.config.countupDuration);
+        }
     }
 
-    incrementCountByOne() {
-        const currentValue = parseInt(this.text);
-        this.text = currentValue + 1;
-        if (this.config.audio && this.canPlaySound(currentValue + 1)) {
+    incrementDelayCount(dt, delay) {
+        this.delayProgress += dt;
+        if (this.delayProgress > delay) {
+            this.countupState = COUNTUP_STATE.COUNTING;
+        }
+    }
+
+    canPlaySound(oldValue, newValue) {
+        //const fireRate = this.config.audio.fireRate >= 1 ? this.config.audio.fireRate : 1;
+        return newValue !== oldValue ? true : false; //value % fireRate === 0;
+    }
+
+    incrementCount(dt, countupDuration) {
+        this.currentValue += (dt / countupDuration) * this.numberOfTicks;
+        this.previousText = this.text;
+        this.text = parseInt(this.currentValue);
+        if (this.text === this.endCount) {
+            this.countupState = COUNTUP_STATE.ENDED;
+        }
+        if (this.config.audio && this.canPlaySound(this.previousText, this.text)) {
             const startRate = this.config.audio.startPlayRate || 1;
             const endRate = this.config.audio.endPlayRate || 1;
-            const progress = (currentValue + 1) / (this.endCount - this.startCount);
+            const progress = this.currentValue / (this.endCount - this.startCount);
             const currentRate = startRate + progress * (endRate - startRate);
             this.scene.sound.play(this.config.audio.key, { rate: currentRate });
         }
-    }
-
-    startCountWithDelay(startDelay) {
-        this.scene.time.delayedCall(startDelay, this.startCountingUp, undefined, this);
-    }
-
-    startCountingUp() {
-        if (this.startCount === this.endCount) {
-            return;
-        }
-        const repeat = this.endCount - this.startCount - 1;
-        const delay = repeat ? this.config.countupDuration / repeat : undefined;
-        this.scene.time.addEvent({
-            delay,
-            callback: this.incrementCountByOne,
-            callbackScope: this,
-            repeat,
-            startAt: delay,
-        });
     }
 }
