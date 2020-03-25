86a2db0f2d0a1287ad1246e142ef3863786063da
Nick Tipping <nick@bigredmonster.com>
2020-03-25 22:28:16 +0000

    * Split debug files into own asset pack and selectively load in debug mode. * Add debug button to home screen.

--- src/core/debug/debug-screens.js
+++ src/core/debug/debug-screens.js
@@ -12,7 +12,7 @@ import fp from "../../../lib/lodash/fp/fp.js";
 TODO
 - test in starter pack (may need routing in local web server)
 - Finalise dev docs. Make notes about what scene name tags / files should match...
-- Split debug asset files into own manifest and load only if debug mode + move out of themes
+- Move debug asset pack out of themes
 - doc or auto strip from build example theme files (may need a tidy here)
 - check if build can strip un-needed files when it copies the themes directory
 
