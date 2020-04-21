9b3f02c078c144ab3a14a60b4ce53a6d5432988d
Adam Beswick <adam.beswick@bbc.co.uk>
2020-04-21 14:09:06 +0100

    Remove extra setDepth

--- src/components/select/select-screen.js
+++ src/components/select/select-screen.js
@@ -42,7 +42,6 @@ export class Select extends Screen {
         this.resize();
         this._cells = this.grid.addGridCells(this.context.theme);
         this.layout.addCustomGroup("grid", this.grid, gridDefaults.tabIndex);
-        this.layout.root.setDepth(1);
 
         this._scaleEvent = onScaleChange.add(this.resize.bind(this));
         this.scene.scene.events.on("shutdown", this._scaleEvent.unsubscribe, this);
