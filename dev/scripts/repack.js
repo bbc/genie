f2928eae443ad18435a4c3608bb32d246d585adc
Nick Tipping <nick@bigredmonster.com>
2020-06-17 10:42:36 +0100

    Repack Tool for converting Phaser 2 asset packs to 3.

---
+++ dev/scripts/repack.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+/**
+ * Covert Phaser 2 asset packs to Phaser 3
+ *
+ * Usage: node repack path-to-file
+ */
+// constants
+const brightRed = "\x1b[1m\x1b[31m";
+const brightGreen = "\x1b[1m\x1b[32m";
+const resetCli = "\x1b[0m";
+const fs = require("fs");
+const path = require("path");
+
+// functions
+const getNewPath = ({ dir, ext, name }) => ({ dir, ext, name: name + "_new" });
+const isP3Format = json => Object.keys(json).some(key => Boolean(json[key].prefix) || Boolean(json[key].files));
+const exitWithMessage = (color, text, code) => {
+    console.log(color, text);
+    console.log(resetCli);
+    process.exit(code);
+};
+
+const audio = entry => {
+    entry.url = entry.urls.find(value => /\.(mp3|mp4)$/i.test(value));
+    delete entry.urls;
+    delete entry.autoDecode;
+    return entry;
+};
+
+const isDefined = value => value !== undefined
+const spriteParams = ["frameWidth", "frameHeight", "margin", "spacing", "startFrame", "endFrame"];
+
+const spritesheet = entry => {
+    const entries = spriteParams.map(param => [param, entry[param]]).filter(a => isDefined(a[1]))
+    entry.frameConfig = Object.fromEntries(entries);
+    [...spriteParams, "frameMax" ].forEach(param => delete entry[param])
+    return entry;
+};
+
+const defaultFile = entry => entry;
+
+const dispatcher = {
+    audio,
+    spritesheet,
+    defaultFile,
+};
+
+const updateFiles = entry => dispatcher?.[entry.type]?.(entry) || dispatcher.defaultFile(entry);
+
+// Open and process file
+const file = process.argv[2];
+if (!fs.existsSync(file)) exitWithMessage(brightRed, `ERROR: File ${file} not found!`, 1);
+
+let oldManifest = JSON.parse(fs.readFileSync(file));
+
+if (isP3Format(oldManifest)) exitWithMessage(brightRed, `ERROR: File ${file} is already Phaser 3 format!`, 1);
+
+const keys = Object.keys(oldManifest);
+const values = keys.map(key => ({ prefix: `${key}.`, files: oldManifest[key].map(updateFiles) }));
+const newManifest = keys.reduce((obj, k, i) => ({ ...obj, [k]: values[i] }), {});
+//const newFile = path.format(getNewPath(path.parse(file)));
+
+fs.writeFileSync(file, JSON.stringify(newManifest, null, 4));
+
+//console.log(JSON.stringify(newManifest, null, 4))
+
+exitWithMessage(brightGreen, `File written to ${file}`, 0);
