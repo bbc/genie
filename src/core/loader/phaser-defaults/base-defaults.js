20cb42eccd321d5dc7a82ab3be51c4592f79abf9
Nick Tipping <nick@bigredmonster.com>
2020-06-18 20:58:08 +0100

    add __GENIE__ after module resolution

--- src/core/loader/phaser-defaults/base-defaults.js
+++ src/core/loader/phaser-defaults/base-defaults.js
@@ -5,17 +5,19 @@
  */
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from "../../layout/metrics.js";
 
-const genie = __GENIE__
+export const getBaseDefaults = () => {
+    const genie = __GENIE__;
 
-export const getBaseDefaults = () => ({
-    width: CANVAS_WIDTH,
-    height: CANVAS_HEIGHT,
-    antialias: true,
-    multiTexture: true,
-    banner: true,
-    title: "BBC Games Genie",
-    version: `${genie.version} / Build: ${genie.build}`, //TODO add JEnkins JOB_NAME ?
-    clearBeforeRender: false,
-    scale: { mode: Phaser.Scale.NONE },
-    input: { windowEvents: false },
-});
+    return {
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT,
+        antialias: true,
+        multiTexture: true,
+        banner: true,
+        title: "BBC Games Genie",
+        version: `${genie.version} / Build: ${genie.build}`, //TODO add Jenkins JOB_NAME ?
+        clearBeforeRender: false,
+        scale: { mode: Phaser.Scale.NONE },
+        input: { windowEvents: false },
+    };
+};
