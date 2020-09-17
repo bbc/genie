871fc2cf8534e9d7926dece82a098721570247cb
Nick Tipping <nick@bigredmonster.com>
2020-09-17 10:44:25 +0100

    add itemRegistry map.

--- src/components/shop/item-registry.js
+++ src/components/shop/item-registry.js
@@ -3,42 +3,34 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
+export let itemsRegistry = new Map();
 
-import fp from "../../../lib/lodash/fp/fp.js";
+export const initRegistry = (key, items) => {
+    if (window.__debug) {
+        window.__debug.items = itemsRegistry;
+    }
 
-export const initRegistry = itemsArray => {
-    const getResult = result => (result ? Object.assign({}, result) : undefined);
+    const get = id => items.find(item => id === item.id);
 
-    const get = (id, categories = []) => {
-        if (id === undefined) return fp.clone(itemsArray);
-        let result;
-        if (!categories.length) {
-            result = itemsArray.find(item => id === item.id);
-            return getResult(result);
-        }
-        categories.forEach(category => {
-            result = itemsArray.find(item => id === item.id && item.category.includes(category));
-        });
-        return getResult(result);
-    };
-
-    const getCategory = category => itemsArray.filter(item => item.category.includes(category));
+    const getCategory = category => items.filter(item => item.category.includes(category));
 
     const set = item => {
-        const itemIndex = itemsArray.findIndex(storedItem => item.id === storedItem.id);
+        const itemIndex = items.findIndex(storedItem => item.id === storedItem.id);
         if (itemIndex !== -1) {
-            itemsArray[itemIndex] = item;
+            items[itemIndex] = item;
             return true;
         }
         return false;
     };
 
-    const registry = {
-        itemsArray,
+    const itemList = {
+        itemsArray: items,
         get,
         getCategory,
         set,
     };
 
-    return registry;
+    itemsRegistry.set(key, itemList);
+
+    return itemList;
 };
