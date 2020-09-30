3c4ce87a31afad53b05e3caf2cb7a20e92303484
Bee Godwin <bee.godwin@bbc.co.uk>
2020-09-30 16:38:55 +0100

    button unit test stubs

--- test/core/layout/scrollable-list/scrollable-list-buttons.test.js
+++ test/core/layout/scrollable-list/scrollable-list-buttons.test.js
@@ -1,5 +1,55 @@
 describe("Scrollable List Buttons", () => {
-    test("fails", () => {
-        expect(false).toBe(true);
+
+    afterEach(() => jest.clearAllMocks());
+
+    describe("createGelButton", () => {
+        test("adds a gel button", () => {
+            expect(false).toBe(true);
+        });
+
+        test("provides correct config", () => {
+            expect(false).toBe(true);
+        });
+
+        test("subscribes to the event bus", () => {
+            expect(false).toBe(true);
+        });
+
+        test("scales the button", () => {
+            expect(false).toBe(true);
+        });
+
+        test("applies correct overlays", () => {
+            expect(false).toBe(true);
+        });
+    });
+
+    describe("overlays1Wide", () => {
+        
+        test("sets background, icon, and currency icon overlays", () => {
+            expect(false).toBe(true);
+        });
+
+        test("sets currencyAmount, itemName, and itemDescription overlays", () => {
+            expect(false).toBe(true);
+        });
+
+        test("offsets the item name less if there is no item description", () => {
+            expect(false).toBe(true);
+        });
+
+        test("applies font styling from config", () => {
+            expect(false).toBe(true);
+        });
+    });
+
+    describe("scaleButton", () => {
+        test("scales the button to leave config.space pixels each side", () => {
+            expect(false).toBe(true);
+        });
+
+        test("preserves the aspect ratio", () => {
+            expect(false).toBe(true);
+        });
     });
 });
