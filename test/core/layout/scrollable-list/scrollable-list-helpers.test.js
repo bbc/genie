1221237197f941002f37bf976462f287866b749e
Bee Godwin <bee.godwin@bbc.co.uk>
2020-09-30 16:30:46 +0100

    helper tests

--- test/core/layout/scrollable-list/scrollable-list-helpers.test.js
+++ test/core/layout/scrollable-list/scrollable-list-helpers.test.js
@@ -1,5 +1,22 @@
+import * as helpers from "../../../../src/core/layout/scrollable-list/scrollable-list-helpers.js";
+
 describe("Scrollable List helpers", () => {
-    test("fails", () => {
-        expect(false).toBe(true);
+
+    afterEach(() => jest.clearAllMocks());
+
+    describe("onClick", () => {
+        test("calls console.log", () => {
+            console.log = jest.fn();
+            const mockGelButton = { config: { id: "foo"}};
+            helpers.onClick(mockGelButton);
+            expect(console.log).toHaveBeenCalledWith("Clicked foo");
+        });
+    });
+    describe("assetKey", () => {
+        test("concatenates the asset prefix with the asset key", () => {
+            const assetKeys = { prefix: "foo" };
+            const concatenatedKey = helpers.assetKey("bar", assetKeys)
+            expect(concatenatedKey).toEqual("foo.bar");
+        });
     });
 });
