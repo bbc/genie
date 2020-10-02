1724f781a6e2440b1e3e5a647d7ee828740b930b
Nick Tipping <nick@bigredmonster.com>
2020-10-02 23:16:51 +0100

    Working getAll and get.
    Not handling zero tag situations at this point.

--- src/components/home.js
+++ src/components/home.js
@@ -16,18 +16,25 @@ import { initList, itemLists } from "../core/item-list.js";
 
 export class Home extends Screen {
     preload() {
+        //catalogue
         this.load.json5({ key: "game-items", url: "themes/default/items/game-items.json5" });
+
+        //armoury collection
+        this.load.json5({ key: "armoury", url: "themes/default/items/armoury.json5" });
     }
 
     create() {
-        const gameItemsConfig = this.cache.json.get("game-items");
+        const armouryConfig = this.cache.json.get("armoury");
+        const itemConfig = this.cache.json.get("game-items");
 
-        initList("game-items", gameItemsConfig);
+        initList("armoury", armouryConfig, itemConfig);
 
-        const gameItems = itemLists.get("game-items");
+        const armoury = itemLists.get("armoury");
+
+        debugger;
+
 
         gameItems.set("sword", {state: null, qty: 5})
-        debugger;
 
         const achievements = gmi.achievements.get().length ? ["achievements"] : [];
         const debug = isDebug() ? ["debug"] : [];
--- src/core/item-list.js
+++ src/core/item-list.js
@@ -5,25 +5,40 @@
  */
 import { gmi } from "./gmi/gmi.js";
 import fp from "../../lib/lodash/fp/fp.js";
+import { catalogue } from "../components/shop/item-catalogue.js";
 
 const getGenieStore = () => gmi.getAllSettings().gameData.genie || {};
 
 export let itemLists = new Map();
-export const initList = (key, config) => {
+
+const addDefaults = itemDefaults => item => ({ ...item, ...itemDefaults.find(def => def.id === item.id) });
+const filterTags = filters => item => item.tags.some(tag => !filters.includes(tag));
+const addQty = qty => item => ({ ...item, qty });
+const addStored = stored => item => ({ ...item, ...stored[item.id] });
+const getStored = key => getGenieStore()[key] || {};
+
+
+//TODO need to handle no includes being set
+const getTags = catalogue => fp.uniq(catalogue.flatMap(entry => entry.tags))
+
+export const initList = (key, config, catalogue) => {
     window.__debug && (window.__debug.itemLists = itemLists);
 
-    const getMerged = stored => config.map(item => Object.assign(item, stored[item.id]));
-    const get = key =>
-        Object.assign(
-            {},
-            config.find(conf => conf.id === key),
-            fp.get(`${key}.${key}`, getGenieStore()),
-        );
+    const inverseTags = fp.difference(getTags(catalogue), config.include)
+
+    const getAll = () => catalogue
+        .filter(filterTags(inverseTags))
+        .map(addQty(config.defaultQty ?? 1))
+        .map(addDefaults(config.defaults))
+        .map(addStored(getStored(key)));
 
 
-    const getStored = () => getGenieStore()[key] || {};
 
-    const getAll = fp.flow(getStored, getMerged);
+    //TODO the inverse idea didn't work.
+
+    debugger
+
+    const get = getAll().find(item => item.id === key)
 
     const set = (id, itemList = null) => {
         gmi.setGameData("genie", fp.setWith(Object, [key, id], itemList, getGenieStore()));
