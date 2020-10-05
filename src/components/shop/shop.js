eba31220a0b13118377f58999f64e676a09b2dbe
Bee Godwin <bee.godwin@bbc.co.uk>
2020-10-05 15:46:32 +0100

    comments

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -8,7 +8,7 @@
  */
 import { Screen } from "../../core/screen.js";
 import { scrollableList } from "../../core/layout/scrollable-list/scrollable-list.js";
-import "../../../lib/rexuiplugin.min.js"; // CAN BE REMOVED IF NOT USING SHOP
+import "../../../lib/rexuiplugin.min.js";
 
 export class Shop extends Screen {
     create() {
