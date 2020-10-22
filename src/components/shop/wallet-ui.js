3c3649a2509aa3b53c7800a9adaccf2d4e1516f3
Bee Godwin <bee.godwin@bbc.co.uk>
2020-10-22 12:07:21 +0100

    refactor wallet-ui

--- src/components/shop/wallet-ui.js
+++ src/components/shop/wallet-ui.js
@@ -8,26 +8,41 @@
 
 import { getSafeArea, getXPos, getYPos } from "./shop.js";
 
+const styleDefaults = {
+    fontFamily: "ReithSans",
+    fontSize: "24px",
+    resolution: 4,
+};
+
+const makeElements = makerFns => conf => makerFns[conf.type](conf).setOrigin(0.5);
+
 export const createWallet = (scene, metrics) => {
-    const { wallet } = scene.config;
+    
+    const image = conf => scene.add.image(0, 0, `${scene.assetPrefix}.${conf.key}`);
+    const text = conf => {
+        const textStyle = { ...styleDefaults, ...conf.styles };
+        return scene.add.text(0, 0, conf.value, textStyle);
+    };
+    
+    const configs = scene.config.wallet || [];
+    const padding = scene.config.walletPadding;
+    const elems = configs.map(makeElements({ image, text }));
 
-    const walletBackground = scene.add.image(0, 0, wallet.background);
-    const walletIcon = scene.add.image(0, 0, wallet.icon);
-    const walletBalance = scene.add.text(0, 0, wallet.defaultBalance, wallet.font).setOrigin(1, 0.5);
-    const walletContainer = scene.add.container();
+    const container = scene.add.container();
 
-    const walletWidth = walletBalance.getBounds().width + walletIcon.getBounds().width + wallet.iconPadding * 3;
-    walletBalance.setPosition(walletWidth / 4 + wallet.iconPadding, 0);
-    walletIcon.setPosition(-walletWidth / 4, 0);
-    walletBackground.setScale(walletWidth / walletBackground.getBounds().width);
-    walletContainer.add([walletBackground, walletIcon, walletBalance]);
+    const width = elems[2].getBounds().width + elems[1].getBounds().width + padding * 3;
+    elems[2].setPosition(width / 4 - padding, 0);
+    elems[1].setPosition(-width / 4, 0);
+    elems[0].setScale(width / elems[0].getBounds().width);
+
+    container.add(elems);
 
     const safeArea = getSafeArea(scene.layout);
-    walletContainer.setScale(scene.getScaleFactor(metrics, walletContainer));
-    walletContainer.setPosition(
-        getXPos(walletContainer, safeArea, scene.config.listPadding.x),
+    container.setScale(scene.getScaleFactor(metrics, container));
+    container.setPosition(
+        getXPos(container, safeArea, scene.config.listPadding.x),
         getYPos(metrics, getSafeArea(scene.layout)),
     );
 
-    return walletContainer;
+    return container;
 };
