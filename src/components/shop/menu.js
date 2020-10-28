ce030d1bc12c653740fc7534d76f70d6935b432d
Bee Godwin <bee.godwin@bbc.co.uk>
2020-10-28 14:46:23 +0000

    menu can be configured left or right

--- src/components/shop/menu.js
+++ src/components/shop/menu.js
@@ -8,16 +8,33 @@
 import { getSafeArea } from "./shop-layout.js";
 
 export const createMenu = (scene, metrics, config) => {
-
-    const menuContainer = scene.add.container();
-
     const bounds = getSafeArea(scene.layout);
+    const { buttonsRight } = config;
+    
+    const menuContainer = scene.add.container();
+    menuContainer.add(nonButtonRect(scene, bounds, buttonsRight));
+    menuContainer.add(buttonContainer(scene, bounds, !buttonsRight));
 
-    const yOffset = bounds.height / 2 + bounds.y;
-    const halfWidth = bounds.width / 2;
-    
-    menuContainer.add(scene.add.rectangle(-halfWidth / 2, yOffset, halfWidth, bounds.height, 0xff0000, 0.3))
-    menuContainer.add(scene.add.rectangle(halfWidth / 2, yOffset, halfWidth, bounds.height, 0xff00ff, 0.3))
-    
     return menuContainer;
+};
+
+const buttonContainer = (scene, menuBounds, isOnLeft) => {
+    const { x, y, width, height } = getSubContainerPostition(menuBounds, isOnLeft);
+    return scene.add.rectangle(x, y, width, height, 0xff00ff, 0.3);
+};
+
+const nonButtonRect = (scene, menuBounds, isOnLeft) => {
+    const { x, y, width, height } = getSubContainerPostition(menuBounds, isOnLeft);
+    return scene.add.rectangle(x, y, width, height, 0xff0000, 0.3);
+};
+
+const getSubContainerPostition = (menuBounds, isOnLeft) => {
+    const yOffset = menuBounds.height / 2 + menuBounds.y;
+    const halfWidth = menuBounds.width / 2;
+    return {
+        x: isOnLeft ? -halfWidth / 2 : halfWidth / 2,
+        y: yOffset,
+        width: halfWidth,
+        height: menuBounds.height,
+    };
 };
