cfb127ead4e449d404275ac42841724fb110d521
Adam Beswick <adam.beswick@bbc.co.uk>
2020-12-01 14:47:55 +0000

    remove unused import

--- src/core/layout/scrollable-list/scrollable-list.js
+++ src/core/layout/scrollable-list/scrollable-list.js
@@ -5,7 +5,7 @@
  * @license Apache-2.0 Apache-2.0
  */
 import { updatePanelOnFocus, updatePanelOnScroll } from "./scrollable-list-handlers.js";
-import { createGelButton, scaleButton, updateButton, getButtonState } from "./scrollable-list-buttons.js";
+import { createGelButton, scaleButton, updateButton } from "./scrollable-list-buttons.js";
 import * as a11y from "../../accessibility/accessibility-layer.js";
 import { collections } from "../../collections.js";
 import { onScaleChange } from "../../scaler.js";
