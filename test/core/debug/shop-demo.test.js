995f66157a69a5a59f93df5945ecb90d8936a803
Bee Godwin <bee.godwin@bbc.co.uk>
2020-12-07 22:09:13 +0000

    mock out phaser calls on create

--- test/core/debug/shop-demo.test.js
+++ test/core/debug/shop-demo.test.js
@@ -5,12 +5,13 @@
  */
 
 import { ShopDemo, ShopDemoGame } from "../../../src/core/debug/shop-demo.js";
+import { collections } from "../../../src/core/collections.js";
 
 describe("ShopDemo", () => {
     let shopDemo;
 
     beforeEach(() => {
-        const mockButton = { overlays: { set: jest.fn()}, config: { callback: jest.fn() } };
+        const mockButton = { overlays: { set: jest.fn() }, config: { callback: jest.fn() } };
         const mockText = { setOrigin: jest.fn() };
         shopDemo = new ShopDemo();
         shopDemo.add = {
@@ -30,6 +31,8 @@ describe("ShopDemo", () => {
         shopDemo.scene = { key: "shopDemo" };
         shopDemo.setLayout = jest.fn();
         shopDemo.events = { once: jest.fn() };
+        const mockInventory = { get: jest.fn(), getAll: jest.fn() };
+        collections.get = jest.fn().mockReturnValue(mockInventory);
     });
 
     describe("create()", () => {
@@ -47,6 +50,52 @@ describe("ShopDemo", () => {
             const shopCallback = shopDemo.add.gelButton.mock.calls[1][2].callback;
             shopCallback();
             expect(shopDemo.navigation.shop).toHaveBeenCalled();
+        });
+    });
+});
+
+describe("ShopDemoGame", () => {
+    let shopDemoGame;
+    let mockCursors;
+
+    beforeEach(() => {
+        shopDemoGame = new ShopDemoGame();
+
+        shopDemoGame.scene = { key: "shopDemoGame" };
+        shopDemoGame._data = {
+            config: {
+                shopDemoGame: {
+                    assets: {
+                        coin: { key: "foo", scale: 1 },
+                        coinPop: { key: "bar", scale: 1 },
+                        player: { key: "baz", scale: 1 },
+                    },
+                    paneCollections: { manage: "inventory" },
+                    balance: { value: { key: "qux" } },
+                    treeSpawns: [],
+                    player: { spawn: { x: 0, y: 0 }, hat: { scale: 1 } },
+                },
+            },
+        };
+        shopDemoGame.anims = { create: jest.fn(), generateFrameNumbers: jest.fn() };
+        shopDemoGame.setLayout = jest.fn();
+        mockCursors = { space: { isDown: true } };
+        shopDemoGame.input = { keyboard: { createCursorKeys: jest.fn().mockReturnValue(mockCursors) } };
+        const mockCurrency = { qty: 1 };
+        const mockInventory = { get: jest.fn().mockReturnValue(mockCurrency), getAll: jest.fn().mockReturnValue([]) };
+        collections.get = jest.fn().mockReturnValue(mockInventory);
+        shopDemoGame.add = {
+            text: jest.fn(),
+            container: jest.fn().mockReturnValue({ setPosition: jest.fn().mockReturnValue({ add: jest.fn() }) }),
+            sprite: jest.fn().mockReturnValue({ setScale: jest.fn().mockReturnValue({ play: jest.fn() }) }),
+        };
+    });
+
+    describe("create", () => {
+        beforeEach(() => shopDemoGame.create());
+
+        test("creates cursors", () => {
+            expect(shopDemoGame.cursors).toBe(mockCursors);
         });
     });
 });
