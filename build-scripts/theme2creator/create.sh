a3dd189b63a2a4108c636e455c8947243daaf786
Leonie Kenyon <leonie.kenyon@bbc.co.uk>
2021-01-06 10:10:33 +0000

    Adds script to copy theme2 with greyscale images

---
+++ build-scripts/theme2creator/create.sh
@@ -0,0 +1,6 @@
+rm -rf themes/theme2
+cp -R themes/default themes/theme2
+cd themes/theme2
+
+# Convert images to greyscale
+for img in $(find . \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.png"  -or -name "*.gif" \)); do echo -n "Converting $img"; convert $img -intensity Rec709luminance -colorspace gray $img && echo ' [Done]'; done
