9bed76960a73ee7c828bf0ded3443c535d76c446
Leonie Kenyon <leonie.kenyon@bbc.co.uk>
2021-01-06 10:27:19 +0000

    Adds more descriptive comment for greyscale conversion

--- build-scripts/theme2creator/create.sh
+++ build-scripts/theme2creator/create.sh
@@ -4,4 +4,4 @@ cp -R themes/default themes/theme2
 cd themes/theme2
 
 # Convert all images to greyscale
-for img in $(find . \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.png"  -or -name "*.gif" \)); do echo -n "Converting $img"; convert $img -intensity Rec709luminance -colorspace gray $img && echo ' [Done]'; done
+for img in $(find . \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.png"  -or -name "*.gif" \)); do echo -n "Converting to greyscale for theme2 $img"; convert $img -intensity Rec709luminance -colorspace gray $img && echo ' [Done]'; done
