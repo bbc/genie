9f5b4189cf59f6e1d5d0c4857c1529df440e2049
Adam Beswick <adam.beswick@bbc.co.uk>
2021-02-10 14:47:25 +0000

    Add rest of the buy transaction tests

--- test/components/shop/transact.test.js
+++ test/components/shop/transact.test.js
@@ -40,13 +40,14 @@ describe("Shop Transactions", () => {
             qty: 500,
         };
         mockItem = {
-            id: "id",
+            id: "item",
+            price: 50,
         };
         mockShopItem = {
             qty: 1,
         };
         mockInventoryItem = {
-            qty: 0,
+            qty: 5,
         };
         collections.get = jest.fn(collectionName =>
             collectionName === "shop" ? mockShopCollection : mockManageCollection,
@@ -62,11 +63,35 @@ describe("Shop Transactions", () => {
     });
     afterEach(() => jest.clearAllMocks());
 
-    describe("Buy", () => {
-        test("item quantity is reduced by 1 in the shop collection when buying an item", () => {
+    describe("Buying an item", () => {
+        test("item quantity is reduced by 1 in the shop collection", () => {
             const expectedItem = { ...mockItem, qty: mockShopItem.qty - 1 };
             transact.buy(mockScene, mockItem);
             expect(mockShopCollection.set).toHaveBeenCalledWith(expectedItem);
+        });
+
+        test("item quantity is increased by 1 and given purchased state in the inventory collection", () => {
+            const expectedItem = { ...mockItem, qty: mockInventoryItem.qty + 1, state: "purchased" };
+            transact.buy(mockScene, mockItem);
+            expect(mockManageCollection.set).toHaveBeenCalledWith(expectedItem);
+        });
+
+        test("item is added to the inventory collection with qty set to 1 when the item does not exist in the inventory yet", () => {
+            mockInventoryItem = undefined;
+            const expectedItem = { ...mockItem, qty: 1, state: "purchased" };
+            transact.buy(mockScene, mockItem);
+            expect(mockManageCollection.set).toHaveBeenCalledWith(expectedItem);
+        });
+
+        test("balance item has its quantity reduced by the price of the item", () => {
+            const expectedBalanceItem = { ...mockCurrencyItem, qty: mockCurrencyItem.qty - mockItem.price };
+            transact.buy(mockScene, mockItem);
+            expect(mockManageCollection.set).toHaveBeenCalledWith(expectedBalanceItem);
+        });
+
+        test("emits an updatebalance event", () => {
+            transact.buy(mockScene, mockItem);
+            expect(mockScene.events.emit).toHaveBeenCalledWith("updatebalance");
         });
     });
 });
