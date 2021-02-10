7df95c8d818cde7f4a2226add061f168c3cef5da
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-10 14:07:26 +0000

    Update cypress.sh

--- build-scripts/cypress.sh
+++ build-scripts/cypress.sh
@@ -3,6 +3,7 @@ set -e
 
 npm install --force
 #python build-scripts/licensechecker/licensechecker.py
+DEBUG=cypress:*
 node cypress/support/clearReports.js
 npm run cy:headless
 node cypress/support/createReports.js
