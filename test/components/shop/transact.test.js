1e51abb17dbdf3d8ebf6da96703112250d4ea6c8
Adam Beswick <adam.beswick@bbc.co.uk>
2021-02-10 14:13:14 +0000

    first buy test

--- test/components/shop/transact.test.js
+++ test/components/shop/transact.test.js
@@ -11,12 +11,62 @@ import { collections } from "../../../src/core/collections.js";
 jest.mock("../../../src/core/collections.js");
 
 describe("Shop Transactions", () => {
+    let mockScene;
+    let mockCurrencyItem;
+    let mockItem;
+    let mockShopItem;
+    let mockInventoryItem;
+    let mockShopCollection;
+    let mockManageCollection;
+
     beforeEach(() => {
-        // TODO rewrite all these tests
+        mockScene = {
+            config: {
+                balance: {
+                    value: {
+                        key: "currency",
+                    },
+                },
+                paneCollections: {
+                    shop: "shop",
+                    manage: "manage",
+                },
+            },
+            events: {
+                emit: jest.fn(),
+            },
+        };
+        mockCurrencyItem = {
+            qty: 500,
+        };
+        mockItem = {
+            id: "id",
+        };
+        mockShopItem = {
+            qty: 1,
+        };
+        mockInventoryItem = {
+            qty: 0,
+        };
+        collections.get = jest.fn(collectionName =>
+            collectionName === "shop" ? mockShopCollection : mockManageCollection,
+        );
+        mockShopCollection = {
+            get: jest.fn(() => mockShopItem),
+            set: jest.fn(),
+        };
+        mockManageCollection = {
+            get: jest.fn(itemId => (itemId === "currency" ? mockCurrencyItem : mockInventoryItem)),
+            set: jest.fn(),
+        };
     });
     afterEach(() => jest.clearAllMocks());
 
-    test("test", () => {
-        expect(1).toBe(1);
+    describe("Buy", () => {
+        test("item quantity is reduced by 1 in the shop collection when buying an item", () => {
+            const expectedItem = { ...mockItem, qty: mockShopItem.qty - 1 };
+            transact.buy(mockScene, mockItem);
+            expect(mockShopCollection.set).toHaveBeenCalledWith(expectedItem);
+        });
     });
 });
