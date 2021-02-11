251a25cba8afe8d628367632c802d590e6c0b831
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-11 14:51:29 +0000

    debug enabled

--- build-scripts/cypress.sh
+++ build-scripts/cypress.sh
@@ -4,5 +4,5 @@ set -e
 npm install
 #python build-scripts/licensechecker/licensechecker.py
 node cypress/support/clearReports.js
-npm run cy:headless
+DEBUG=cypress:* npm run cy:headless
 node cypress/support/createReports.js
