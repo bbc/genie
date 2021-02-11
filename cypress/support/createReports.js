1a67556c73ac7f430c4b997a507e11bd80c4ad70
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-11 17:29:32 +0000

    report dir

--- cypress/support/createReports.js
+++ cypress/support/createReports.js
@@ -1,10 +1,16 @@
 const { merge } = require("mochawesome-merge");
 const generator = require('mochawesome-report-generator')
 
+const options = {
+    files: ['./output/reports/*.json'],
+    reportDir: "./output/reports",
+    reportFilename: "index.html"
+}
+
 const mergeReport = async () => {
-    const jsonReport = await merge();
+    const jsonReport = await merge(options);
     jsonReport;
-    await generator.create(jsonReport);
+    await generator.create(jsonReport, options);
 }
 
 mergeReport();
