8467abf5065fc260d91543dfe4287971e61618d7
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-12 14:00:18 +0000

    exit code handling?

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -8,5 +8,14 @@ npm run test
 npm run build
 cp -r themes output/themes
 cp -r debug output/debug
-npm run cy:headless || exit
-node cypress/support/createReports.js
+npm run cy:headless
+if [ $? -eq 0 ]
+    then
+        echo "The script ran ok"
+        node cypress/support/createReports.js
+        exit 0
+    else
+        echo "The script failed" >&2
+        node cypress/support/createReports.js
+        exit 1
+fi
