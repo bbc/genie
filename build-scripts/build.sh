45f963321d6ccf13d6f7bf5d354a72072b30f521
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-12 15:54:02 +0000

    Update build.sh

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 npm install --force
 #python build-scripts/licensechecker/licensechecker.py
 npm run test
@@ -6,14 +7,18 @@ npm run test
 npm run build
 cp -r themes output/themes
 cp -r debug output/debug
+
+set +e
 npm run cy:headless
 if [ $? -eq 0 ]
     then
         echo "The script ran ok"
         node cypress/support/createReports.js
+        #Set a flag for S3 to publish all? Not needed, defaults to all?
         exit 0
     else
         echo "The script failed" >&2
         node cypress/support/createReports.js
+        #set a flag for S3 to only publish reports?
         exit 1
 fi
