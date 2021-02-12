643341098f79fb18bd376bb2b36015cc8ac23bc1
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-12 16:52:37 +0000

    Update build.sh

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -14,11 +14,10 @@ if [ $? -eq 0 ]
     then
         echo "The script ran ok"
         node cypress/support/createReports.js
-        #Set a flag for S3 to publish all? Not needed, defaults to all?
         exit 0
     else
         echo "The script failed" >&2
         node cypress/support/createReports.js
-        #set a flag for S3 to only publish reports?
+        $UPLOADTOS3 --noLatest
         exit 1
 fi
