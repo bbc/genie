0c0c7d41046bd49c960450f03f86af362a26cb69
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-17 16:39:32 +0000

    build script refactor
    -Build script now runs both theme1 and theme2 tests
    -Handling of both these scenarios in the build file
    -refactor how data is passed to screenshots
    -Scenario: Theme1 fails. Still runs Theme2 but commits to nolatest regardless of Theme2 output
    -Scenario: Theme1 passes, Theme2 fails, commits to nolatest
    -Scenario: Theme1 passes, Theme2 passes, commits to latest

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
-set -e
+
+postTheme2Actions()
+{
+     if [ $? -eq 0 ]
+            then
+                node cypress/support/createReports.js
+                zip -r output/reports/screenshots.zip cypress/screenshots
+                exit 0
+            else
+                echo "The Cypress tests failed - Uploading report to S3 with no latest" >&2
+                node cypress/support/createReports.js
+                zip -r output/reports/screenshots.zip cypress/screenshots
+                $UPLOADTOS3 nolatest
+                exit 1
+        fi
+}
+
+postTheme1Actions()
+{
+   if [ $? -eq 0 ]
+            then
+                echo "The Cypress tests failed - Uploading report to S3 with no latest" >&2
+                node cypress/support/createReports.js
+                zip -r output/reports/screenshots.zip cypress/screenshots
+                $UPLOADTOS3 nolatest
+                exit 1
+            else
+                echo "The Cypress tests failed - Uploading report to S3 with no latest" >&2
+                node cypress/support/createReports.js
+                zip -r output/reports/screenshots.zip cypress/screenshots
+                $UPLOADTOS3 nolatest
+                exit 1
+        fi  
+}
+
+set -e 
 source /etc/profile
 nvm install 12.19.0
 node -v
@@ -15,13 +50,10 @@ set +e
 npm run start:pack & npm run cy:local
 if [ $? -eq 0 ]
     then
-        node cypress/support/createReports.js
-        zip -r output/reports/screenshots.zip cypress/screenshots
-        exit 0
+        npm run cy:local-theme2
+        postTheme2Actions
     else
-        echo "The Cypress tests failed - Uploading report to S3 with no latest" >&2
-        node cypress/support/createReports.js
-        zip -r output/reports/screenshots.zip cypress/screenshots
-        $UPLOADTOS3 nolatest
-        exit 1
+        npm run cy:local-theme2
+        postTheme1Actions
 fi
+
