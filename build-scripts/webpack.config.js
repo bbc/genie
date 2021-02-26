29a0c9bf1f4ee533b55204c02fdb81a6f1e6f6f0
Alex pb <alex.parry-brown@bbc.co.uk>
2021-02-26 11:50:25 +0000

    remove port chnages

--- build-scripts/webpack.config.js
+++ build-scripts/webpack.config.js
@@ -8,10 +8,6 @@ const dynamicallyExposeGlobals = require("../dev/scripts/dynamicExpose.js");
 
 const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
-const portFinderSync = require("portfinder-sync");
-const basePort = 9001
-const freePort = portFinderSync.getPort(basePort)
-
 
 module.exports = env => {
     const development = env && env.development;
@@ -63,7 +59,7 @@ module.exports = env => {
             writeToDisk: true,
             useLocalIp: true,
             host: "0.0.0.0",
-            port: freePort,
+            port: 9001,
             historyApiFallback: {
                 index: "node_modules/genie/dev/index.main.html",
             },
--- cypress/support/functions.js
+++ cypress/support/functions.js
@@ -5,8 +5,6 @@
  */
 import { appendToken } from "../support/appendToken.js";
 
-import webpack from "../../build-scripts/webpack.config.js"
-
 let localTesting = "http://localhost:9001/?debug=true";
 let localTheme2 = "http://localhost:9001/?debug=true&theme=theme2";
 let testTheme2 =
@@ -15,7 +13,6 @@ let test =
     "https://www.test.bbc.co.uk/games/embed/genie?versionOverride=latest&viewNonPublished=true&cageEnv=test&debug=true";
 export const getUrl = () => {
 
-    cy.log(webpack({}))
     if (Cypress.env("LOCAL_DEV") == "true") {
         if (Cypress.env("THEME") == "theme_2") {
             cy.log("theme 2 dev");
