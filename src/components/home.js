f49cc25e3d878f2d1114153375aaf39af8b0b3f0
alex-s2 <alexandra.sneddon@bbc.co.uk>
2021-03-03 12:21:08 +0000

    Consumables

--- src/components/home.js
+++ src/components/home.js
@@ -13,9 +13,6 @@ import { isDebug } from "../core/debug/debug-mode.js";
 import { gmi } from "../core/gmi/gmi.js";
 
 export class Home extends Screen {
-    itemUsed(data) {
-        console.log("item used!!", data);
-    }
     create() {
         const achievements = gmi.achievements.get().length ? ["achievements"] : [];
         const debug = isDebug() ? ["debug"] : [];
@@ -27,13 +24,6 @@ export class Home extends Screen {
             channel: buttonsChannel(this),
             name: "play",
             callback: this.navigation.next,
-        });
-        eventBus.subscribe({
-            channel: "shop",
-            name: "used",
-            callback: data => {
-                this.itemUsed(data);
-            },
         });
     }
 }
