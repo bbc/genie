f26089f98c5831a94829b2099697c20ca6bfb4a8
AnthonyGoodchild <anthony.goodchild@bbc.co.uk>
2021-03-04 09:24:07 +0000

    CGPROD-3095 Scrollable-list button disabled state
    Set button.off for `disabled` button states, determined by `disabled` prop
    in the shop button config.

--- src/components/shop/scrollable-list/scrollable-list-buttons.js
+++ src/components/shop/scrollable-list/scrollable-list-buttons.js
@@ -48,6 +48,8 @@ export const createListButton = (scene, item, title, action) => {
     gelButton.item = item;
 
     const properties = item.state && scene.config.states[item.state] ? scene.config.states[item.state].properties : {};
+    const disabled = item.state && scene.config.states[item.state] ? scene.config.states[item.state].disabled : false;
+    disabled && gelButton.off(Phaser.Input.Events.POINTER_UP);
     Object.assign(gelButton.sprite, properties);
 
     scaleButton(gelButton, scene.layout, scene.config.listPadding);
