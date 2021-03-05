e973c434aec3adf5f77afd769e472b70090a0cb4
Adam Beswick <adam.beswick@bbc.co.uk>
2021-03-05 12:21:33 +0000

    Start updating titles to new gel spec

--- src/core/titles.js
+++ src/core/titles.js
@@ -4,29 +4,43 @@
  * @license Apache-2.0
  */
 import fp from "../../lib/lodash/fp/fp.js";
-import { updateStyleOnFontLoad } from "./layout/text-elem.js";
+import { getMetrics } from "./scaler.js";
 
-const styleDefaults = {
-    fontSize: "24px",
+const textStyle = style => ({
+    fontSize: "36px",
     fontFamily: "ReithSans",
     align: "center",
+    ...style,
+});
+
+const scaleTitleImage = (gameObject, area) => {
+    const safeHeight = getMetrics().isMobile ? area.height / 2 : area.height;
+    const safeWidth = area.right * 2;
+    const aspectRatio = gameObject.width / gameObject.height;
+    const height = Phaser.Math.Clamp(Math.max(gameObject.height, safeHeight), 0, safeHeight);
+    const width = Math.min(aspectRatio * height, safeWidth);
+    gameObject.setDisplaySize(width, width / aspectRatio);
 };
 
-const makeElements = makerFns => conf => makerFns[conf.type]({ x: conf.xOffset, y: conf.yOffset }, conf).setOrigin(0.5);
+const createTitle = scene => {
+    const safeAreaFn = scene.layout.getTitleArea;
+    const image = scene.add.image(0, 0, scene.config.title?.key);
+    const template = fp.template(scene.config.title?.text);
+    const textString = fp.startCase(template(scene.transientData?.[scene.scene.key]));
+    const text = scene.add.text(0, 0, textString, textStyle(scene.config.title?.style)).setOrigin(0.5, 0.5);
 
-export const createTitles = scene => {
-    const image = (pos, conf) => scene.add.image(pos.x, pos.y, `${scene.assetPrefix}.${conf.key}`);
-
-    const text = (pos, conf) => {
-        const template = fp.template(conf.value);
-        const text = fp.startCase(template(scene.transientData?.[scene.scene.key]));
-        const textStyle = { ...styleDefaults, ...conf.styles };
-        const textSprite = scene.add.text(pos.x, pos.y, text, textStyle);
-        textSprite.defaultStyle = textStyle;
-        updateStyleOnFontLoad(textSprite);
-        return textSprite;
+    return {
+        resize: () => {
+            const safeArea = safeAreaFn();
+            image.y = safeArea.centerY;
+            text.y = safeArea.centerY;
+            scaleTitleImage(image, safeArea);
+        },
     };
-
-    const configs = scene.config.titles || [];
-    return configs.map(makeElements({ image, text }));
 };
+const createSubtitle = scene => {};
+
+export const createTitles = scene => ({
+    title: createTitle(scene),
+    subtitle: createSubtitle(scene),
+});
