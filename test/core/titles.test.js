2afddc1fc40320e60fea0df8fde73197504f76ae
Adam Beswick <adam.beswick@bbc.co.uk>
2021-03-08 14:57:28 +0000

    titles tests

--- test/core/titles.test.js
+++ test/core/titles.test.js
@@ -5,29 +5,150 @@
  */
 import { createTitles } from "../../src/core/titles.js";
 import * as text from "../../src/core/layout/text-elem.js";
+import * as metrics from "../../src/core/scaler.js";
+
+jest.mock("../../src/core/layout/text-elem.js");
+jest.mock("../../src/core/scaler.js");
 
 describe("Titles", () => {
     let mockScene;
-    let mockImage;
+    let mockTitleBackdrop;
+    let mockSubtitleBackdrop;
+    let mockSubtitleIcon;
+    let mockButtonPad;
     let mockText;
+    let mockTitleArea;
 
     beforeEach(() => {
-        mockImage = { setOrigin: jest.fn() };
-        mockText = { setOrigin: jest.fn() };
+        mockButtonPad = 10;
+        metrics.getMetrics = jest.fn().mockReturnValue({ buttonPad: mockButtonPad });
+        mockTitleArea = { centerY: 50, right: 100 };
+        mockTitleBackdrop = { setOrigin: jest.fn(), destroy: jest.fn() };
+        mockSubtitleBackdrop = { setOrigin: jest.fn(), destroy: jest.fn() };
+        mockSubtitleIcon = { setOrigin: jest.fn(), destroy: jest.fn(), width: 20 };
+        mockText = { setOrigin: jest.fn(), destroy: jest.fn(), width: 50, x: 0 };
         mockScene = {
-            scene: { key: "character-select" },
-            add: { image: jest.fn(() => mockImage), text: jest.fn(() => mockText) },
-            config: {
-                title: {},
-                subtitle: {},
+            layout: { getTitleArea: jest.fn().mockReturnValue(mockTitleArea) },
+            scene: { key: "sceneKey" },
+            add: {
+                image: jest
+                    .fn()
+                    .mockReturnValueOnce(mockTitleBackdrop)
+                    .mockReturnValueOnce(mockSubtitleBackdrop)
+                    .mockReturnValueOnce(mockSubtitleIcon),
+                text: jest.fn(() => mockText),
             },
+            config: {
+                title: {
+                    text: "Title",
+                    backgroundKey: "titleKey",
+                    style: { fontSize: "36px", fontFamily: "ReithSans", align: "center" },
+                },
+                subtitle: {
+                    text: "Subtitle",
+                    backgroundKey: "subtitleKey",
+                    style: { fontSize: "24px", fontFamily: "ReithSans", align: "center" },
+                },
+            },
+            transientData: {},
         };
-        text.updateStyleOnFontLoad = jest.fn();
     });
 
     afterEach(jest.clearAllMocks);
 
-    test("", () => {
-        //
+    describe("Title", () => {
+        test("passes title text to updateStyleOnFontLoad", () => {
+            createTitles(mockScene);
+            expect(text.updateStyleOnFontLoad).toHaveBeenCalledWith(mockText);
+        });
+
+        test("adds title backdrop", () => {
+            createTitles(mockScene);
+            expect(mockScene.add.image).toHaveBeenCalledWith(0, 0, mockScene.config.title.backgroundKey);
+        });
+
+        test("adds title text", () => {
+            createTitles(mockScene);
+            expect(mockScene.add.text).toHaveBeenCalledWith(0, 0, "Title", mockScene.config.title.style);
+            expect(mockText.setOrigin).toHaveBeenCalledWith(0.5, 0.5);
+        });
+
+        test("adds title text when string template is provided", () => {
+            mockScene.config.title.text = "<%= fruit %>";
+            mockScene.transientData[mockScene.scene.key] = { fruit: "banana" };
+            createTitles(mockScene);
+            expect(mockScene.add.text).toHaveBeenCalledWith(0, 0, "Banana", mockScene.config.title.style);
+        });
+
+        test("title text reverts to default style when one is not provided", () => {
+            delete mockScene.config.title.style;
+            const expectedStyle = { fontSize: "36px", fontFamily: "ReithSans", align: "center" };
+            createTitles(mockScene);
+            expect(mockScene.add.text).toHaveBeenCalledWith(0, 0, "Title", expectedStyle);
+        });
+
+        test("repositions the backdrop and text when resize is called", () => {
+            const titles = createTitles(mockScene);
+            titles.title.resize();
+            expect(mockText.y).toBe(mockTitleArea.centerY);
+            expect(mockTitleBackdrop.y).toBe(mockTitleArea.centerY);
+        });
+
+        test("destroys backdrop and text when destroy is called", () => {
+            const titles = createTitles(mockScene);
+            titles.title.destroy();
+            expect(mockTitleBackdrop.destroy).toHaveBeenCalled();
+            expect(mockText.destroy).toHaveBeenCalled();
+        });
+    });
+
+    describe("Subtitle", () => {
+        test("adds subtitle backdrop", () => {
+            createTitles(mockScene);
+            expect(mockScene.add.image).toHaveBeenCalledWith(0, 0, mockScene.config.subtitle.backgroundKey);
+        });
+
+        test("adds subtitle text", () => {
+            createTitles(mockScene);
+            expect(mockScene.add.text).toHaveBeenCalledWith(0, 0, "Subtitle", mockScene.config.subtitle.style);
+            expect(mockText.setOrigin).toHaveBeenCalledWith(0.5, 0.5);
+        });
+
+        test("adds subtitle text when string template is provided", () => {
+            mockScene.config.subtitle.text = "<%= fruit %>";
+            mockScene.transientData[mockScene.scene.key] = { fruit: "banana" };
+            createTitles(mockScene);
+            expect(mockScene.add.text).toHaveBeenCalledWith(0, 0, "Banana", mockScene.config.subtitle.style);
+        });
+
+        test("subtitle text reverts to default style when one is not provided", () => {
+            delete mockScene.config.subtitle.style;
+            const expectedStyle = { fontSize: "36px", fontFamily: "ReithSans", align: "center" };
+            createTitles(mockScene);
+            expect(mockScene.add.text).toHaveBeenCalledWith(0, 0, "Subtitle", expectedStyle);
+        });
+
+        test("repositions the backdrop, text and icon when resize is called", () => {
+            const titles = createTitles(mockScene);
+            titles.subtitle.resize();
+            expect(mockText.x).toBe(mockTitleArea.right - mockButtonPad * 2 - mockText.width / 2);
+            expect(mockText.y).toBe(mockTitleArea.centerY);
+            expect(mockSubtitleBackdrop.x).toBe(
+                mockTitleArea.right - (mockSubtitleIcon.width + mockButtonPad * 4.5 + mockText.width) / 2,
+            );
+            expect(mockSubtitleBackdrop.y).toBe(mockTitleArea.centerY);
+            expect(mockSubtitleIcon.x).toBe(
+                mockText.x - mockText.width / 2 - mockSubtitleIcon.width / 2 - mockButtonPad / 2,
+            );
+            expect(mockSubtitleIcon.y).toBe(mockTitleArea.centerY);
+        });
+
+        test("destroys backdrop, text and icon when destroy is called", () => {
+            const titles = createTitles(mockScene);
+            titles.subtitle.destroy();
+            expect(mockSubtitleBackdrop.destroy).toHaveBeenCalled();
+            expect(mockSubtitleIcon.destroy).toHaveBeenCalled();
+            expect(mockText.destroy).toHaveBeenCalled();
+        });
     });
 });
