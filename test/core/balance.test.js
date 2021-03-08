997ea6e20658f428ea90165025b3de1743ea9352
Adam Beswick <adam.beswick@bbc.co.uk>
2021-03-08 12:26:24 +0000

    setBalance test

---
+++ test/core/balance.test.js
@@ -0,0 +1,38 @@
+/**
+ * @copyright BBC 2020
+ * @author BBC Children's D+E
+ * @license Apache-2.0
+ */
+import * as balance from "../../src/components/shop/balance.js";
+import { collections } from "../../src/core/collections.js";
+
+jest.mock("../../src/core/collections.js");
+
+describe("setBalance", () => {
+    let mockScene;
+    let mockManageCollection;
+    let mockBalanceItem;
+
+    beforeEach(() => {
+        mockBalanceItem = { qty: 50 };
+        mockManageCollection = { get: jest.fn().mockReturnValue(mockBalanceItem) };
+        collections.get = jest.fn().mockReturnValue(mockManageCollection);
+        mockScene = {
+            scene: { key: "sceneKey" },
+            transientData: {
+                shop: { config: { balance: "balance", shopCollections: { manage: "manage" } } },
+                sceneKey: { data: "mock" },
+            },
+        };
+    });
+
+    afterEach(jest.clearAllMocks);
+
+    test("sets balance item to transientData", () => {
+        balance.setBalance(mockScene);
+        expect(mockScene.transientData[mockScene.scene.key]).toEqual({
+            ...mockScene.transientData[mockScene.scene.key],
+            balance: mockBalanceItem.qty,
+        });
+    });
+});
