7669e09e6c82aece8c9f629b8f02a25c892896b4
Alex pb <alex.parry-brown@bbc.co.uk>
2021-03-11 09:59:24 +0000

    Forcefully wait for aria-label to update

--- cypress/integration/select_spec.js
+++ cypress/integration/select_spec.js
@@ -72,22 +72,22 @@ describe(`The Select screens ${Cypress.env("THEME")}`, () => {
 
     it("Continuously cycles through select pages on grid view", () => {
         cy.genieClick("#home__debug");
-        cy.genieClick("#debug__debug-select-grid");
+        cy.genieClick("#debug__debug-select-grid").wait(300);
         cy.ariaHidden("#debug-select-grid__mary", "false");
         cy.ariaHidden("#debug-select-grid__mike", "true");
-        cy.genieClick("#debug-select-grid__next");
+        cy.genieClick("#debug-select-grid__next").wait(300);
         cy.ariaHidden("#debug-select-grid__mary", "true");
         cy.ariaHidden("#debug-select-grid__mike", "false");
-        cy.genieClick("#debug-select-grid__next");
+        cy.genieClick("#debug-select-grid__next").wait(300);
         cy.ariaHidden("#debug-select-grid__mary", "false");
         cy.ariaHidden("#debug-select-grid__mike", "true");
-        cy.genieClick("#debug-select-grid__previous");
+        cy.genieClick("#debug-select-grid__previous").wait(300);
         cy.ariaHidden("#debug-select-grid__mary", "true");
         cy.ariaHidden("#debug-select-grid__mike", "false");
-        cy.genieClick("#debug-select-grid__previous");
+        cy.genieClick("#debug-select-grid__previous").wait(300);
         cy.ariaHidden("#debug-select-grid__mary", "false");
         cy.ariaHidden("#debug-select-grid__mike", "true");
-        cy.genieClick("#debug-select-grid__previous");
+        cy.genieClick("#debug-select-grid__previous").wait(300);
         cy.ariaHidden("#debug-select-grid__mary", "true");
         cy.ariaHidden("#debug-select-grid__mike", "false");
     });
