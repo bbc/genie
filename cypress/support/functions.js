fb381c7755f44cdc2e3c3b30ee18404a32c27ba4
alexparry-brown <37833161+alexparry-brown@users.noreply.github.com>
2021-03-11 16:08:02 +0000

    Cgprod 2808 stats automation (#430)
    -adds 2000ms delay to flakey select screen tests for transition time
    -adds tests around all page view stats
    -adds tests around all user action stats

--- cypress/support/functions.js
+++ cypress/support/functions.js
@@ -11,6 +11,7 @@ let testTheme2 =
     "https://www.test.bbc.co.uk/games/embed/genie?versionOverride=latest&viewNonPublished=true&cageEnv=test&debug=true&theme=theme2";
 let test =
     "https://www.test.bbc.co.uk/games/embed/genie?versionOverride=latest&viewNonPublished=true&cageEnv=test&debug=true";
+
 export const getUrl = () => {
     if (Cypress.env("LOCAL_DEV") == "true") {
         if (Cypress.env("THEME") == "theme_2") {
@@ -27,4 +28,68 @@ export const getUrl = () => {
             return appendToken(test);
         }
     }
+};
+
+export const formatStatConfig = (
+    stat,
+    { actionName, actionType, creationId, format, advertiserId, screenName } = {},
+) => {
+    let bucketString;
+
+    if (actionType && actionName) {
+        stat.creationId = `${actionName}~${actionType}`;
+    }
+
+    if (creationId) {
+        stat.creationId = creationId;
+    }
+
+    if (format) {
+        stat.format = format;
+    }
+
+    if (advertiserId) {
+        stat.advertiserId = advertiserId;
+    }
+
+    if (screenName) {
+        stat.screenName = screenName;
+    }
+
+    const generalPlacement = (generalPlacement, screenName) => {
+        if (screenName === "") {
+            return `${generalPlacement}.page`;
+        } else {
+            return `${generalPlacement}.${screenName}.page`;
+        }
+    };
+
+    const countername = (countername, screenName) => {
+        if (screenName === "") {
+            return `${countername}.page`;
+        } else {
+            return `${countername}.${screenName}.page`;
+        }
+    };
+
+    if (stat.bucket === "testGames") {
+        bucketString = `s=${599452}`;
+    } else if (stat.bucket === "prodGames") {
+        bucketString = `s=${598261}`;
+    }
+
+    const counternameString = countername(stat.counterName, stat.screenName);
+    const statString = `[${stat.campaignId}]-[${stat.creationId}]-[${stat.variant}]-[${
+        stat.format
+    }]-[${generalPlacement(stat.generalPlacement, stat.screenName)}]-[${stat.detailedPlacement}]-[${
+        stat.advertiserId
+    }]-[${stat.url}]`;
+    const newStatData = {
+        bucket: bucketString,
+        stat: statString,
+        counterName: `p=${counternameString}`,
+        event: `echo_event=${stat.event}`,
+    };
+    cy.log(stat.screenName);
+    return newStatData;
 };
