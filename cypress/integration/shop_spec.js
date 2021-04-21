09bea806518931fb01a0bd6f5aea678e359fcf1d
Alex pb <alex.parry-brown@bbc.co.uk>
2021-04-21 12:17:00 +0100

    Cypress shop updates
    -re-enable cypress tests for shop
    -update stats for shop to align with previous work

--- cypress/integration/shop_spec.js
+++ cypress/integration/shop_spec.js
@@ -4,51 +4,49 @@
  * @license Apache-2.0
  */
 
-//import { getUrl } from "../support/functions";
+import { getUrl } from "../support/functions";
 
-/* Temporarily removed until bug fix is introduced */
 
-// describe(`The Genie Shop component ${Cypress.env("THEME")}`, () => {
-//     beforeEach(() => {
-//         cy.visit(getUrl());
-//         cy.get("#home__play", { timeout: 60000 }).should("exist");
-//         if (!Cypress.env("DEV_LOCAL") == "true") {
-//             cy.get(".data-notice").click();
-//         }
-//         cy.genieClick("#home__debug");
-//         cy.genieClick("#debug__debug-shop-demo");
-//         cy.genieClick("#debug-shop-demo__Shop");
-//     });
+describe(`The Genie Shop component ${Cypress.env("THEME")}`, () => {
+    beforeEach(() => {
+        cy.visit(getUrl());
+        cy.get("#home__play", { timeout: 60000 }).should("exist");
+        if (!Cypress.env("DEV_LOCAL") == "true") {
+            cy.get(".data-notice").click();
+        }
+        cy.genieClick("#home__debug");
+        cy.genieClick("#debug__debug-shop-equippables-menu");
+    });
 
-//     it("Successfully purchase and equip an item", () => {
-//         cy.genieClick("#debug-shop-demo-shop__shop_menu_button");
-//         cy.genieClick("#debug-shop-demo-shop__scroll_button_redHat_shop");
-//         cy.genieClick("#debug-shop-demo-shop__tx_buy_button");
-//         cy.genieClick("#debug-shop-demo-shop__back");
-//         cy.genieClick("#debug-shop-demo-shop__manage_menu_button");
-//         cy.genieClick("#debug-shop-demo-shop__scroll_button_redHat_manage");
-//         cy.genieClick("#debug-shop-demo-shop__tx_equip_button");
-//     });
+    it("Successfully purchase and equip an item", () => {
+        cy.genieClick("#debug-shop-equippables-menu__shop_menu_button");
+        cy.genieClick("#debug-shop-equippables-list__scroll_button_ironHat_shop");
+        cy.genieClick("#debug-shop-equippables-confirm__tx_buy_button");
+        cy.genieClick("#debug-shop-equippables-list__back");
+        cy.genieClick("#debug-shop-equippables-menu__manage_menu_button");
+        cy.genieClick("#debug-shop-equippables-list__scroll_button_ironHat_manage");
+        cy.genieClick("#debug-shop-equippables-confirm__tx_equip_button");
+    });
 
-//     it("Can pause the game throughout the shop", () => {
-//         cy.genieClick("#debug-shop-demo-shop__pause");
-//         cy.genieClick("#pause__play");
-//         cy.genieClick("#debug-shop-demo-shop__shop_menu_button");
-//         cy.genieClick("#debug-shop-demo-shop__pause");
-//         cy.genieClick("#pause__play");
-//         cy.genieClick("#debug-shop-demo-shop__back");
-//         cy.genieClick("#debug-shop-demo-shop__manage_menu_button");
-//         cy.genieClick("#debug-shop-demo-shop__pause");
-//         cy.genieClick("#pause__play");
-//     });
+    it("Can pause the game throughout the shop", () => {
+        cy.genieClick("#debug-shop-equippables-menu__pause");
+        cy.genieClick("#pause__play");
+        cy.genieClick("#debug-shop-equippables-menu__shop_menu_button");
+        cy.genieClick("#debug-shop-equippables-list__pause");
+        cy.genieClick("#pause__play");
+        cy.genieClick("#debug-shop-equippables-list__back");
+        cy.genieClick("#debug-shop-equippables-menu__manage_menu_button");
+        cy.genieClick("#debug-shop-equippables-list__pause");
+        cy.genieClick("#pause__play");
+    });
 
-//     it("Returns the user to the previous screen when the back button is selected", () => {
-//         cy.genieClick("#debug-shop-demo-shop__manage_menu_button");
-//         cy.genieClick("#debug-shop-demo-shop__scroll_button_ironHat_manage");
-//         cy.genieClick("#debug-shop-demo-shop__back");
-//         cy.genieClick("#debug-shop-demo-shop__back");
-//         cy.genieClick("#debug-shop-demo-shop__shop_menu_button");
-//         cy.genieClick("#debug-shop-demo-shop__back");
-//         cy.get("#debug-shop-demo-shop__manage_menu_button").should("exist");
-//     });
-// });
+    it("Returns the user to the previous screen when the back button is selected", () => {
+        cy.genieClick("#debug-shop-equippables-menu__manage_menu_button");
+        cy.genieClick("#debug-shop-equippables-list__scroll_button_box_manage");
+        cy.genieClick("#debug-shop-equippables-confirm__back");
+        cy.genieClick("#debug-shop-equippables-list__back");
+        cy.genieClick("#debug-shop-equippables-menu__shop_menu_button");
+        cy.genieClick("#debug-shop-equippables-list__back");
+        cy.get("#debug-shop-equippables-menu__manage_menu_button").should("exist");
+    });
+});
