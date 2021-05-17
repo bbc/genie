8cc479136bea8749dcd1fcb58094de39aea355dc
Leonie Kenyon <leonie.kenyon@bbc.co.uk>
2021-05-17 16:51:48 +0100

    Ensures correct font is used throughout the shop

--- src/components/shop/confirm/confirm-buttons.js
+++ src/components/shop/confirm/confirm-buttons.js
@@ -3,10 +3,11 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
-import { createConfirmButtons } from "../menu-buttons.js";
+import { createConfirmButtons, resizeGelButtons } from "../menu-buttons.js";
 import fp from "../../../../lib/lodash/fp/fp.js";
 import { buy, equip, unequip, use } from "../transact.js";
 import { canAffordItem, isEquippable, itemIsInStock } from "./item-checks.js";
+import { getInnerRectBounds, getSafeArea } from "../shop-layout.js";
 
 const disableActionButton = button => {
     Object.assign(button, { alpha: 0.25, tint: 0xff0000 });
@@ -29,7 +30,19 @@ const actions = {
 
 const canBuyItem = (scene, item) => canAffordItem(scene, item) && itemIsInStock(scene, item);
 
+const getRect = scene => {
+    const bounds = getSafeArea(scene.layout);
+    const innerBounds = getInnerRectBounds(scene);
+    const rect = scene.add.rectangle(innerBounds.x, innerBounds.y, innerBounds.width, innerBounds.height, 0x000000, 0);
+
+    rect.setSize(innerBounds.width, innerBounds.height);
+    rect.setY(bounds.height / 2 + bounds.y);
+
+    return rect;
+};
+
 export const addConfirmButtons = (scene, title, action, item) => {
+    const rect = getRect(scene);
     const confirmButtonCallback = () => handleActionClick(scene, title, action, item);
     const cancelButtonCallback = () => {
         scene._data.addedBy.scene.resume();
@@ -42,6 +55,7 @@ export const addConfirmButtons = (scene, title, action, item) => {
         cancelButtonCallback,
         item,
     );
+    resizeGelButtons({ confirmButtons, rect });
     ((action === "buy" && !canBuyItem(scene, item)) || (action === "equip" && !isEquippable(item))) &&
         disableActionButton(confirmButtons[0]);
     return confirmButtons;
