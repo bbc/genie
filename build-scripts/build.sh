b22623e6e2b6b8b82b546550351216edd8f87a6b
AnthonyGoodchild <anthony.goodchild@bbc.co.uk>
2021-06-01 11:22:25 +0100

    separate nvm and npm install versions

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -35,10 +35,11 @@
 # }
 
 # set -e
-# source /etc/profile
-# nvm install 10.24.1
+source /etc/profile
+nvm install 10.24.1
 # node -v
 npm install --force
+nvm install 13.14.0
 #python build-scripts/licensechecker/licensechecker.py
 npm run jest
 npm run eslint
