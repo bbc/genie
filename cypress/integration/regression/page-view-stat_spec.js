844f0765a53b35251364fc6ae09e1efe1d6ba14b
Alex pb <alex.parry-brown@bbc.co.uk>
2021-06-25 15:23:22 +0100

    Upgrade to Cypress 7.5.0

--- cypress/integration/regression/page-view-stat_spec.js
+++ cypress/integration/regression/page-view-stat_spec.js
@@ -4,13 +4,18 @@
  * @license Apache-2.0
  */
 
-import { statHelper } from "games-automation-stathelper";
+import { statHelper } from "games-stat-helper";
 import { getUrl } from "../../support/functions";
 import { pageViews } from "../../support/statConfig";
 
 describe("Page view stats for Genie Screens", () => {
     beforeEach(() => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.homePage).counterName).as("homePageView");
+        cy.log(statHelper.formatStatConfig(pageViews.homePage).counterName);
+        cy.intercept({
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.homePage).counterName}`,
+            },
+        }).as("homePageView");
         cy.visit(getUrl());
         cy.get("#home__play", { timeout: 60000 }).should("exist");
         if (!Cypress.env("DEV_LOCAL") == "true") {
@@ -23,14 +28,24 @@ describe("Page view stats for Genie Screens", () => {
     });
 
     it("Fires a page view stat for the character select screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.selectScreenPage).counterName).as("selectScreenView");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.selectScreenPage).counterName}`,
+            },
+        }).as("selectScreenView");
         cy.genieClick("#home__play");
         cy.genieClick("#narrative__skip");
         cy.wait("@selectScreenView");
     });
 
     it("Fires a page view stat for the results screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.resultsScreenPage).counterName).as("resultsScreenView");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.resultsScreenPage).counterName}`,
+            },
+        }).as("resultsScreenView");
         cy.genieClick("#home__play");
         cy.genieClick("#narrative__skip");
         cy.genieClick("#character-select__mary");
@@ -40,7 +55,12 @@ describe("Page view stats for Genie Screens", () => {
     });
 
     it("Fires a page view stat for the narrative screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.narrativePage).counterName).as("narrativeScreen");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.narrativePage).counterName}`,
+            },
+        }).as("narrativeScreen");
         cy.genieClick("#home__play");
         cy.genieClick("#narrative__skip");
         cy.genieClick("#character-select__mary");
@@ -50,14 +70,24 @@ describe("Page view stats for Genie Screens", () => {
     });
 
     it("Fires a page view stat for the shop screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.shopPage).counterName).as("menuScreen");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.shopPage).counterName}`,
+            },
+        }).as("menuScreen");
         cy.genieClick("#home__debug");
         cy.genieClick("#debug__debug-shop-equippables-menu");
         cy.wait("@menuScreen");
     });
 
     it("Fires a page view stat for the shop store screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.shopStore).counterName).as("buyScreen");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.shopStore).counterName}`,
+            },
+        }).as("buyScreen");
         cy.genieClick("#home__debug");
         cy.genieClick("#debug__debug-shop-equippables-menu");
         cy.genieClick("#debug-shop-equippables-menu__shop_menu_button");
@@ -65,14 +95,24 @@ describe("Page view stats for Genie Screens", () => {
     });
 
     it("Fires a page view stat for the shop management screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.shopManagement).counterName).as("managementScreen");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.shopManagement).counterName}`,
+            },
+        }).as("managementScreen");
         cy.genieClick("#home__debug");
         cy.genieClick("#debug__debug-shop-equippables-menu");
         cy.genieClick("#debug-shop-equippables-menu__manage_menu_button");
         cy.wait("@managementScreen");
     });
     it("Fires a page view stat for the level select screen.", () => {
-        cy.intercept(statHelper.formatStatConfig(pageViews.levelSelectPage).counterName).as("selectScreenView");
+        cy.intercept({
+            method: "GET",
+            query: {
+                p: `${statHelper.formatStatConfig(pageViews.levelSelectPage).counterName}`,
+            },
+        }).as("selectScreenView");
         cy.genieClick("#home__play");
         cy.genieClick("#narrative__skip");
         cy.genieClick("#character-select__mary");
