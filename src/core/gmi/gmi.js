c952f2e86ea8072e98345f06a39147d74ed5dcd4
Nick Tipping <nick@bigredmonster.com>
2022-01-06 01:58:28 +0000

    Works but default page is not yet present

--- src/core/gmi/gmi.js
+++ src/core/gmi/gmi.js
@@ -3,56 +3,38 @@
  * @author BBC Children's D+E
  * @license Apache-2.0
  */
+import fp from "../../../lib/lodash/fp/fp.js";
 
-const dedupeGlobalSettings = customSettings => {
-	return customSettings.filter(customSettings => {
-		return !(customSettings.key === "audio" || customSettings.key === "motion");
-	});
-};
-
-const addExtraGlobalSettings = (customSettingsObject, settingsConfig) => {
-	const extraGlobalSettings = dedupeGlobalSettings(customSettingsObject.settings);
-	return settingsConfig.pages[0].settings.concat(extraGlobalSettings);
-};
-
-const getDefaultGlobals = () => {
-	return {
-		pages: [
-			{
-				title: "Global Settings",
-				settings: [
-					{
-						key: "audio",
-						type: "toggle",
-						title: "Audio",
-						description: "Turn off/on sound and music",
-					},
-					{
-						key: "motion",
-						type: "toggle",
-						title: "Motion FX",
-						description: "Turn off/on motion effects",
-					},
-				],
-			},
-		],
-	};
-};
+const globalSettings = [
+	{
+		key: "audio",
+		type: "toggle",
+		title: "Audio",
+		description: "Turn off/on sound and music",
+	},
+	{
+		key: "motion",
+		type: "toggle",
+		title: "Motion FX",
+		description: "Turn off/on motion effects",
+	},
+];
 
 export let gmi = {};
 
-export const setGmi = (customSettings, windowObj) => {
-	const settingsConfig = getDefaultGlobals();
+const parsePage = page =>
+	page.title === "Global Settings"
+		? { ...page, ...{ settings: fp.unionBy("key", globalSettings, page.settings) } }
+		: page;
 
-	if (customSettings && customSettings.pages) {
-		customSettings.pages.forEach(customSettingsObject => {
-			if (customSettingsObject.title === "Global Settings") {
-				settingsConfig.pages[0].settings = addExtraGlobalSettings(customSettingsObject, settingsConfig);
-			} else {
-				settingsConfig.pages = settingsConfig.pages.concat([customSettingsObject]);
-			}
-		});
-	}
+export const setGmi = (customSettings = {}, windowObj) => {
+
+	const customGlobalPage = customSettings.pages?.find(page => page.title === "Global Settings")
+
+	//map isn't needed as we only merge
+	const pages = customSettings.pages?.map(parsePage);
+
+	const settingsConfig = { pages };
 
 	gmi = windowObj.getGMI({ settingsConfig });
 };
