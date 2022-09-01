f6694d1f95b6edb15948cb1dda19ce881622e4c3
Alexandra Sneddon <alexandra.sneddon@bbc.co.uk>
2022-09-01 08:48:06 +0100

    Initial commit

---
+++ themes/texture-compression/compress-textures.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read p; do
+  TexturePacker --format phaser --sheet $p.png --data $p.json $p
+  TexturePacker --format phaser --texture-format pvr3 --opt PVRTCI_4BPP_RGBA --sheet $p.pvr --data $p-pvr.json $p
+  TexturePacker --format phaser --texture-format pvr3 --opt ETC2_RGBA --sheet $p-ETC2.pvr --data $p-ETC2.json $p
+  TexturePacker --format phaser --texture-format pvr3 --opt DXT5/BC3 --sheet $p-DXT5.pvr --data $p-DXT5.json $p
+done <textures-to-compress.txt
---
+++ themes/texture-compression/readme.md
@@ -0,0 +1,10 @@
+# Automatic Compression of Textures with TexturePacker
+
+## Prerequisites: 
+TexturePacker CLI Tool must be installed
+
+## Usage:
+
+Edit the "textures-to-compress.txt" file with a list of the directories you'd like to compress in the theme. 
+
+Run "sh compress-textures.sh" in this directory and it will generate 3 sprite sheets with associated phaser json files.
