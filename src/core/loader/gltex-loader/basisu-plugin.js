5f96c4adb54204da42d554922f38bd8a5f8d9e6f
Nick Tipping <nick.tipping@bbc.co.uk>
2023-06-23 10:42:30 +0100

    Store

--- src/core/loader/gltex-loader/basisu-plugin.js
+++ src/core/loader/gltex-loader/basisu-plugin.js
@@ -8,6 +8,34 @@
  */
 import { BasisUFile } from "./basisu-file.js";
 
+function basisInit() {
+	console.log("basisInit", timeLeft());
+	const check = BasisLoader.loadTranscoder(
+		`./basis_transcoder.js`,
+		`./basis_transcoder.wasm`
+	);
+	check.then(function () {
+		console.log(
+			"%c BasisLoader, promise solved " + timeLeft(),
+			"background: #222; color: #bada55; padding: 0.3rem"
+		);
+		initPixi();
+		eruda.init();
+	});
+}
+
+/**
+ * Loads the transcoder source code for use in {@link PIXI.BasisLoader.TranscoderWorker}.
+ * @private
+ * @param jsURL - URL to the javascript basis transcoder
+ * @param wasmURL - URL to the wasm basis transcoder
+ */
+BasisLoader.loadTranscoder = function (jsURL, wasmURL) {
+	return BasisLoader.TranscoderWorker.loadTranscoder(jsURL, wasmURL);
+};
+
+
+
 export class BasisUPlugin extends Phaser.Plugins.BasePlugin {
     constructor(pluginManager) {
         super(pluginManager);
