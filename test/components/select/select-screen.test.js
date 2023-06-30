6f1ae32c33131dac301c45c6fa6baa08ef29f8ab
Judit Lehoczki <lehoczki.judit@gmail.com>
2023-06-30 15:33:41 +0100

    [CGPROD-3889] rename test

--- test/components/select/select-screen.test.js
+++ test/components/select/select-screen.test.js
@@ -338,7 +338,7 @@ describe("Select Screen", () => {
 			eventBus.subscribe.mock.calls[0][0].callback();
 			expect(selectScreen.transientData["test-select"].choice.title).toBe("Title 1");
 		});
-		test("saves choice to local storage", () => {
+		test("saves choice to collection", () => {
 			mockChoices = [{ id: "id1", title: "Title 1" }];
 			selectScreen.create();
 
