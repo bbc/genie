9852fe2e76b0d386d43e064f211a7760dd339df4
Judit Lehoczki <lehoczki.judit@gmail.com>
2023-08-03 11:04:04 +0100

    [CGPROD-3889] use merged catalogue with getAll() in setUnique and getUnique

--- src/core/collections.js
+++ src/core/collections.js
@@ -60,7 +60,7 @@ export const initCollection = screen => key => {
 	};
 
 	const setUnique = ({ id, key, value }) => {
-		catalogue.forEach(item => {
+		getAll().forEach(item => {
 			const isNewlyUnique = item.id === id && item[key] !== value;
 			const shouldBeReset = item.id !== id && item.hasOwnProperty(key) && item[key] === value;
 
@@ -74,7 +74,7 @@ export const initCollection = screen => key => {
 		});
 	};
 
-	const getUnique = ({ key, value }) => catalogue.find(item => item[key] === value);
+	const getUnique = ({ key, value }) => getAll().find(item => item[key] === value);
 
 	const collection = {
 		config,
