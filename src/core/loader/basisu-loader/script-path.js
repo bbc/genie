7560977a9cccf19c77a010c529318c13d041f4a4
Nick Tipping <nick.tipping@bbc.co.uk>
2023-08-24 23:34:05 +0100

    Use full path for Worker based on gmi

--- src/core/loader/basisu-loader/script-path.js
+++ src/core/loader/basisu-loader/script-path.js
@@ -4,4 +4,6 @@
  * @license Apache-2.0
  */
 //TODO what happens when this is minified?
-export const SCRIPT_PATH = "./lib/basisu"; // import.meta.url.split("/").slice(0,-1).join("/");
+import { gmi } from "../../gmi/gmi.js";
+
+export const SCRIPT_PATH = `${gmi.info.gameDir}lib/basisu`; // import.meta.url.split("/").slice(0,-1).join("/");
