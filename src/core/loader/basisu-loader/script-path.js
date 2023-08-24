e5b4d07cd99568cd37f8df63d9f300ddd8eeb812
Nick Tipping <nick.tipping@bbc.co.uk>
2023-08-24 20:00:17 +0100

    Make script path relative

--- src/core/loader/basisu-loader/script-path.js
+++ src/core/loader/basisu-loader/script-path.js
@@ -4,4 +4,4 @@
  * @license Apache-2.0
  */
 //TODO what happens when this is minified?
-export const SCRIPT_PATH = "/lib/basisu/"; // import.meta.url.split("/").slice(0,-1).join("/");
+export const SCRIPT_PATH = "lib/basisu/"; // import.meta.url.split("/").slice(0,-1).join("/");
